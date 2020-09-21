438f35fe945e19d2a38500c8a7a5126069884011
paul <yewhonwa@gmail.com>
2020-09-21 15:38:13 +0800

    Updated Parameters Retriever

--- Database.jl
+++ Database.jl
@@ -1,36 +1,104 @@
+include("CSVParser.jl")
+import Combinatorics
 
-function lookup(component::String, method="None")
-    methods = ["ogSAFT", "PCSAFT", "sPCSAFT", "SAFTVRSW", "softSAFT", "SAFTVRMie"]
-    found_methods = Array{String, 1}()
-    found_methods_row = Array{Int32, 1}()
-
-    for i in 1:length(methods)
-        count = 1
-        f = CSV.Rows("database/data_" * methods[i] * ".csv"; header=3)
-        for row in f
-            if row.Compound == component
-                push!(found_methods, methods[i]) # this can be changed to a Dictionary later
-                push!(found_methods_row, count)
-                break
-            end
-            count+=1
+function methods_return(selected_method)
+    if selected_method == "None"
+        return filter(x-> isdir(joinpath("database",x)), readdir("database"))
+    else
+        if selected_method in filter(x-> isdir(joinpath("database",x)), readdir("database"))
+            return [selected_method]
+        else
+            error("Database for selected method " * selected_method * " not found.")
         end
     end
+end
 
-    !isempty(found_methods) ? println("Found methods for " * component * " in " * join(found_methods, ", ") * ".") : throw(ErrorException("Parameters for " * component * " do not exist for any of the current methods."))
+function search_database_like(components::Array{String, 1}, selected_method="None", user_input::AbstractString="None")
+    methods = methods_return(selected_method)
+    found_methods = Dict(component => Dict{String, Int64}() for component in components)
+    for component in components
+        for method in methods
+            filepath = joinpath("database", method, "data_" * method * "_like" * ".csv")
+            if isfile(filepath)
+                found_lines = find_match(filepath, component, "species"; header_row=3)
+                if length(found_lines) > 1
+                    println("The species " * component * " is not unique in database for method " * method * ". Selecting most recent entry.")
+                elseif length(found_lines) == 0
+                    if length(found_lines) == 0
+                        if selected_method != "None"
+                            error("The species " * component * " cannot be found in database for method " * method * ".")
+                        end
+                    end
+                end
+                if length(found_lines) != 0
+                    found_methods[component][method] = found_lines[end]
+                end
+            end
+        end
+    end
+    return found_methods
+end
 
-    if method == "none"
-        # just use a method for now if none is provided
-        method = found_methods[1]
-        row = found_methods_row[1]
+function search_database_unlike(components::Array{String, 1}, selected_method="None", user_input::AbstractString="None")
+    methods = methods_return(selected_method)
+    pairs = [Set(i) for i in collect(Combinatorics.combinations(components, 2))]
+    found_methods = Dict(pair => Dict{String, Int64}() for pair in pairs)
+    for pair in pairs
+        for method in methods
+            filepath = joinpath("database", method, "data_" * method * "_unlike" * ".csv")
+            if isfile(filepath)
+                components = [i for i in pair]
+                found_lines = find_match_pair(filepath, components[1], components[2], "species1", "species2"; header_row=3)
+                if length(found_lines) > 1
+                    println("The pair (" * join([i for i in pair], ", ") * ") is not unique in database for method " * method * ". Selecting most recent entry.")
+                end
+                if length(found_lines) != 0
+                    found_methods[pair][method] = found_lines[end]
+                end
+            end
+        end
+    end
+    return found_methods
+end
+
+function retrieve_parameters(components::Array{String, 1}, selected_method, user_input::AbstractString="None")
+    method = selected_method
+    available_methods_like = search_database_like(components, method)
+    available_methods_unlike = search_database_unlike(components, method)
+    #= avaliable_methods_assoc = search_database_assoc(components) =#
+
+    filepath_like = joinpath("database", method, "data_" * method * "_like" * ".csv")
+    filepath_unlike = joinpath("database", method, "data_" * method * "_unlike" * ".csv")
+    filepath_assoc = joinpath("database", method, "data_" * method * "_assoc" * ".csv")
+
+    header_like = read_line(filepath_like, 3)
+    header_unlike = read_line(filepath_like, 3)
+    header_assoc = read_line(filepath_like, 3)
+
+    if haskey(available_methods_like[components[1]], method)
+        #= parameters1_like = Dict(zip(header_like, read_line(filepath_like, available_methods_like[components[1]][method]))) =#
+        parameters1_like = Dict(zip(header_like, [tryparse(Float64, i) for i in read_line(filepath_like, available_methods_like[components[1]][method])]))
     else
-        row = method in found_methods ? found_methods_row[findfirst(isequal(method), found_methods)] : throw(ErrorException("Parameters for " * component * " do not exist in method " * method * ". Try using another."))
+        parameters1_like = Dict()
+    end
+    if haskey(available_methods_like[components[2]], method)
+        #= parameters2_like = Dict(zip(header_like, read_line(filepath_like, available_methods_like[components[2]][method]))) =#
+        parameters2_like = Dict(zip(header_like, [tryparse(Float64, i) for i in read_line(filepath_like, available_methods_like[components[2]][method])]))
+    else
+        parameters1_like = Dict()
+    end
+    if haskey(available_methods_unlike, method)
+        #= parameters_unlike = Dict(zip(header_unlike, read_line(filepath_unlike, available_methods_unlike[Set(components)][method]))) =#
+        parameters_unlike = Dict(zip(header_like, [tryparse(Float64, i) for i in read_line(filepath_unlike, available_methods_unlike[Set(components)][method])]))
+    else
+        parameters_unlike = Dict()
     end
 
-    println("Using method: " * found_methods[1])
-
-
-    dF = CSV.read("database/data_" * method * ".csv"; header=3, datarow=row+3, limit=1)
-
-    return method, dF
+    return (parameters1_like, parameters2_like, parameters_unlike)
 end
+
+#### Functions to try ####
+
+# search_database_like(["methanol", "cyclohexane"])  
+# search_database_unlike(["methanol", "cyclohexane"])  
+# like1, like2, unlike = retrieve_parameters(["methanol", "cyclohexane"], "PCSAFT") 
