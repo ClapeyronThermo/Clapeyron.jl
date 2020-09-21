2fcf3f89360785c7cf23bb09282959ec75b3e6bb
paul <yewhonwa@gmail.com>
2020-09-22 04:21:58 +0800

    Fully functioning parameter retrieving system

---
+++ CSVParser.jl
@@ -0,0 +1,99 @@
+#### CSV Reader ####
+# These are temporary methods to interpret CSV files
+# We will probably revert to using the DelimitedFiles module and use actual CSV
+# But for now, we are using semicolon delimitetd DSV
+
+function read_line(filename::AbstractString, selected_line::Int64)
+    # Function to read a line from a CSV
+    # Returns an array of strings
+    # Parse to floats later with parse(Float64, string)
+    if !isfile(filename)
+        error("File does not exist")
+    end
+    open(filename) do file
+        linecount = 1
+        for line in eachline(file)
+            if linecount == selected_line
+                return tryparse_fallback(split(line, ';'))
+            end
+            linecount += 1
+        end
+        error("File only contains " * string(linecount-1) * " lines")
+    end
+end
+
+function find_matches(filename::AbstractString, match_string::String, selected_col::Int64 = 1)
+    # Function to search for a string in the specified column in a CSV
+    if !isfile(filename)
+        error("File does not exist")
+    end
+    found_lines = Array{Int64, 1}()
+    open(filename) do file
+        linecount = 1
+        for line in eachline(file)
+            if match_string == split(line, ';'; limit = selected_col+1)[selected_col]
+                push!(found_lines, linecount)
+            end
+            linecount += 1
+        end
+        return found_lines
+    end
+end
+
+function find_matches(filename::AbstractString, match_string::String, selected_col::String; header_row::Int64 = 1)
+    if !isfile(filename)
+        error("File does not exist")
+    end
+    selected_col_numbers = findall(isequal(selected_col), read_line(filename, header_row))
+    if length(selected_col_numbers) == 0
+        error("Header " * selected_col * " does not exist.")
+    elseif length(selected_col_numbers) > 1
+        error("Header " * selected_col * " is not unique")
+    end
+    return find_matches(filename, match_string, selected_col_numbers[1])
+end
+
+function find_matches_pair(filename::AbstractString, match_string1::String, match_string2::String,selected_col1::Int64 = 1, selected_col2::Int64 = 2)
+    # Function to search for pairs of strings in two specified columns
+    if !isfile(filename)
+        error("File does not exist")
+    end
+    found_lines = Array{Int64, 1}()
+    match_set = Set([match_string1, match_string2])
+    open(filename) do file
+        linecount = 1
+        for line in eachline(file)
+            row_entries = split(line, ';')
+            if match_set == Set([row_entries[selected_col1], row_entries[selected_col2]])
+                push!(found_lines, linecount)
+            end
+            linecount += 1
+        end
+        return found_lines
+    end
+end
+
+function find_matches_pair(filename::AbstractString, match_string1::String, match_string2::String, selected_col1::String, selected_col2::String; header_row::Int64 = 1)
+    if !isfile(filename)
+        error("File does not exist")
+    end
+    selected_col_numbers1 = findall(isequal(selected_col1), read_line(filename, header_row))
+    selected_col_numbers2 = findall(isequal(selected_col2), read_line(filename, header_row))
+    if length(selected_col_numbers1) == 0
+        error("Header " * selected_col1 * " does not exist.")
+    elseif length(selected_col_numbers1) > 1
+        error("Header " * selected_col1 * " is not unique")
+    end
+    if length(selected_col_numbers2) == 0
+        error("Header " * selected_col2 * " does not exist.")
+    elseif length(selected_col_numbers2) > 1
+        error("Header " * selected_col2 * " is not unique")
+    end
+    return find_matches_pair(filename, match_string1, match_string2, selected_col_numbers1[1], selected_col_numbers2[1])
+end
+
+function tryparse_fallback(list)
+    parsed = [tryparse(Float64, i) for i in list]
+    return [parsed[i] == nothing ? list[i] : parsed[i] for i in 1:length(list)]
+end
+
--- Database.jl
+++ Database.jl
@@ -1,35 +1,151 @@
-function lookup(component::String, method="None")
-    methods = ["ogSAFT", "PCSAFT", "sPCSAFT", "SAFTVRSW", "softSAFT", "SAFTVRMie"]
-    found_methods = Array{String, 1}()
-    found_methods_row = Array{Int32, 1}()
+include("CSVParser.jl")
+import Combinatorics
 
-    for i in 1:length(methods)
-        count = 1
-        f = CSV.Rows("database/data_" * methods[i] * ".csv"; header=3)
-        for row in f
-            if row.Compound == component
-                push!(found_methods, methods[i]) # this can be changed to a Dictionary later
-                push!(found_methods_row, count)
-                break
-            end
-            count+=1
+function methods_return(selected_method)
+    if selected_method == "None"
+        return filter(x-> isdir(joinpath("database",x)), readdir("database"))
+    else
+        if selected_method in filter(x-> isdir(joinpath("database",x)), readdir("database"))
+            return [selected_method]
+        else
+            error("Database for selected method " * selected_method * " not found.")
         end
     end
+end
 
-    !isempty(found_methods) ? println("Found methods for " * component * " in " * join(found_methods, ", ") * ".") : throw(ErrorException("Parameters for " * component * " do not exist for any of the current methods."))
-
-    if method == "none"
-        # just use a method for now if none is provided
-        method = found_methods[1]
-        row = found_methods_row[1]
-    else
-        row = method in found_methods ? found_methods_row[findfirst(isequal(method), found_methods)] : throw(ErrorException("Parameters for " * component * " do not exist in method " * method * ". Try using another."))
+function search_database_like(components::Array{String, 1}, selected_method="None", user_input::AbstractString="None")
+    methods = methods_return(selected_method)
+    found_methods = Dict(Set([component]) => Dict{String, Int64}() for component in components)
+    for component in components
+        for method in methods
+            filepath = joinpath("database", method, "data_" * method * "_like" * ".csv")
+            if isfile(filepath)
+                found_lines = find_matches(filepath, component, "species"; header_row=3)
+                if length(found_lines) > 1
+                    println("The species " * component * " is not unique in database for method " * method * ". Selecting most recent entry.")
+                elseif length(found_lines) == 0
+                    if length(found_lines) == 0
+                        if selected_method != "None"
+                            error("The species " * component * " cannot be found in database for method " * method * ".")
+                        end
+                    end
+                end
+                if length(found_lines) != 0
+                    found_methods[Set([component])][method] = found_lines[end]
+                end
+            end
+        end
     end
+    return found_methods
+end
 
-    println("Using method: " * found_methods[1])
+function search_database_unlike(components::Array{String, 1}, selected_method="None", user_input::AbstractString="None")
+    methods = methods_return(selected_method)
+    pairs = [Set(i) for i in collect(Combinatorics.combinations(components, 2))]
+    found_methods = Dict{Set{String}, Dict{String, Int64}}() 
+    for pair in pairs
+        for method in methods
+            filepath = joinpath("database", method, "data_" * method * "_unlike" * ".csv")
+            if isfile(filepath)
+                components = [i for i in pair]
+                found_lines = find_matches_pair(filepath, components[1], components[2], "species1", "species2"; header_row=3)
+                if length(found_lines) > 1
+                    println("The pair (" * join([i for i in pair], ", ") * ") is not unique in database for method " * method * ". Selecting most recent entry.")
+                end
+                if length(found_lines) != 0
+                    found_methods[pair] = Dict()
+                    found_methods[pair][method] = found_lines[end]
+                end
+            end
+        end
+    end
+    return found_methods
+end
 
+function search_database_assoc(components::Array{String, 1}, selected_method="None", user_input::AbstractString="None")
+    methods = methods_return(selected_method)
+    pairs = vcat([Set([i]) for i in components], [Set(i) for i in collect(Combinatorics.combinations(components, 2))])
+    found_methods = Dict{Set{String}, Dict{String, Array{Int64,1}}}() 
+    for pair in pairs
+        for method in methods
+            filepath = joinpath("database", method, "data_" * method * "_assoc" * ".csv")
+            if isfile(filepath)
+                if length(pair) == 1
+                    component = [i for i in pair]
+                    components = [component component]
+                else
+                    components = [i for i in pair]
+                end
+                found_lines = find_matches_pair(filepath, components[1], components[2], "species1", "species2"; header_row=3)
+                if length(found_lines) > 0
+                    found_methods[pair] = Dict()
+                    found_methods[pair][method] = found_lines
+                end
+            end
+        end
+    end
+    return found_methods
+end
+    
+function retrieve_parameters_assoc(components::Array{String, 1}, selected_method, user_input::AbstractString="None")
+    filepath = joinpath("database", selected_method, "data_" * selected_method * "_assoc" * ".csv")
+    header = read_line(filepath, 3)
+    found_method = search_database_assoc(components, selected_method)
+    found_parameters = Dict{Set{String}, Dict{String, Dict{Set{String}, Dict{String, Any}}}}() 
+    if !isempty(found_method)
+        for pair in keys(found_method)
+            found_parameters[pair] = Dict("Assoc" => Dict())
+            for line_number in found_method[pair][selected_method]
+                retrieved = Dict(zip(header, read_line(filepath, line_number)))
+                assoc_pair = Set([retrieved["site1"], retrieved["site2"]])
+                found_parameters[pair]["Assoc"][assoc_pair] = Dict()
+                found_parameters[pair]["Assoc"][assoc_pair] = retrieved 
+            end
+        end
+    end
+    return found_parameters
+end
 
-    dF = CSV.read("database/data_" * method * ".csv"; header=3, datarow=row+3, limit=1)
+function retrieve_parameters_like(components::Array{String, 1}, selected_method, user_input::AbstractString="None")
+    filepath = joinpath("database", selected_method, "data_" * selected_method * "_like" * ".csv")
+    header = read_line(filepath, 3)
+    found_method = search_database_like(components, selected_method)
+    found_parameters = Dict{Set{String}, Dict{String, Any}}() 
+    for component in keys(found_method)
+        found_parameters[component] = Dict(zip(header, read_line(filepath, found_method[component][selected_method])))
+    end
+    return found_parameters
+end
 
-    return method, dF
+function retrieve_parameters_unlike(components::Array{String, 1}, selected_method, user_input::AbstractString="None")
+    filepath = joinpath("database", selected_method, "data_" * selected_method * "_unlike" * ".csv")
+    header = read_line(filepath, 3)
+    found_method = search_database_unlike(components, selected_method)
+    found_parameters = Dict{Set{String}, Dict{String, Any}}() 
+    for pair in keys(found_method)
+        found_parameters[pair] = Dict(zip(header, read_line(filepath, found_method[pair][selected_method])))
+    end
+    return found_parameters
+end
+
+function retrieve_parameters(components::Array{String, 1}, selected_method, user_input::AbstractString="None")
+    parameters_like = retrieve_parameters_like(components, selected_method)
+    parameters_unlike = retrieve_parameters_unlike(components, selected_method)
+    parameters_assoc = retrieve_parameters_assoc(components, selected_method)
+
+    pairs = union(Set(keys(parameters_like)), Set(keys(parameters_unlike)), Set(keys(parameters_assoc)))
+    parameters = Dict{Set{String}, Any}()
+    for pair in pairs
+        parameters[pair] = Dict()
+        if haskey(parameters_like, pair)
+            merge!(parameters[pair], parameters_like[pair])
+        end
+        if haskey(parameters_unlike, pair)
+            merge!(parameters[pair], parameters_unlike[pair])
+        end
+        if haskey(parameters_assoc, pair)
+            merge!(parameters[pair], parameters_assoc[pair])
+        end
+    end
+    return parameters
 end
