4113e62506cdae2b33b848e6b422603dbd6a3f39
paul <yewhonwa@gmail.com>
2020-09-26 23:10:20 +0800

    PCSAFT working

--- src/methods/import_parameters.jl
+++ src/methods/import_parameters.jl
@@ -1,11 +1,12 @@
-function create_PCSAFTParams(raw_params)
+function create_PCSAFTParams(raw_params; combiningrule_ϵ = "Berth")
     pure_params_dict, pair_params_dict, assoc_params_dict =
         filterparams(raw_params, ["m", "sigma", "epsilon", "n_H", "n_e"];
                      pair_params = ["k"], assoc_params = ["epsilon_assoc", "bond_vol"])
     segment = pure_params_dict["m"]
-    sigma = pure_params_dict["sigma"] # If multiplication by a factor is needed, do it here
+    sigma = pure_params_dict["sigma"]
+    map!(x->x*1E-10, values(sigma))
     # We can also add binary parameters using push!(sigma, Set([x1, x2]) => value) later
-    # using a combining rule.
+    # using a combining rule. 
     epsilon = pure_params_dict["epsilon"]
     epsilon_assoc = assoc_params_dict["epsilon_assoc"]
     bond_vol = assoc_params_dict["bond_vol"]
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,13 +5,21 @@ method = "PCSAFT"
 model = system(["butane"], method)
 
 p = 1e5
-T = 300
+T = 500
 z = create_z(model, [1.0])
 
 a = JuliaSAFT.eos(model, z, p, T)
+println(a)
+
 #= (T_c, p_c, v_c) = get_Pcrit(model) =#
-#= cp = get_isobaric_heat_capacity(model, z, p, T) =#
-#= println(cp) =#
+cp = get_isobaric_heat_capacity(model, z, p, T)
+println(cp)
+
+
+μ = get_chemical_potential(model, z, p, T)
+println(μ)
+g = get_Gibbs_free_energy(model, z, p, T)
+println(g)
 #= println(T_c) =#
 #= temperature  = range(0.65*T_c,T_c,length=100) =#
 #= (P_sat,v_l,v_v) = Psat(EoS,model,temperature) =#
