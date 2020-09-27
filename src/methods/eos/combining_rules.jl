b08d2906ae291dfb8167d9a64edd3dae7bdb0ec2
paul <yewhonwa@gmail.com>
2020-09-28 01:01:51 +0800

    Added combining rules for PCSAFT

--- src/methods/eos/combining_rules.jl
+++ src/methods/eos/combining_rules.jl
@@ -1,11 +1,32 @@
-function combining_σ(σ, rules="Lorentz-Berthelot")
-    if rules = "Lorentz-Berthelot"
-        return sum([σ[i] for i in keys(σ)])/length(σ)
+function combining_sigma(sigma::Dict; rules="Lorentz-Berthelot")
+    components = [collect(i)[1] for i in keys(sigma)]
+    pairs = [Set(i) for i in collect(combinations(components, 2))]
+    combined_sigmas = Dict{Set{String},Float64}()
+    for pair in pairs 
+        if rules == "Lorentz-Berthelot"
+            combined_sigma = (sum(sigma[Set([i])] for i in pair))/2
+            push!(combined_sigmas, pair => combined_sigma) 
+        end
     end
+    return combined_sigmas
 end
 
-function combining_ϵ(ϵ, rules="Lorentz-Berthelot")
-    if rules = "Lorentz-Berthelot"
-        return prod([ϵ[i] for i in keys(ϵ)])^(1/length(ϵ))
+function combining_epsilon(epsilon::Dict, k::Dict; rules_k="Hudsen-McCoubrey", rules_no_k="Lorentz-Berthelot")
+    components = [collect(i)[1] for i in keys(epsilon)]
+    pairs = [Set(i) for i in collect(combinations(components, 2))]
+    combined_epsilons = Dict{Set{String},Float64}()
+    for pair in pairs 
+        if haskey(k, pair)
+            if rules_k == "Hudsen-McCoubrey"
+                combined_epsilon = (1-k[pair])*sqrt(prod(epsilon[Set([i])] for i in pair))
+                push!(combined_epsilons, pair => combined_epsilon) 
+            end
+        else
+            if rules_no_k == "Lorentz-Berthelot"
+                combined_epsilon = sqrt(prod(epsilon[Set([i])] for i in pair))
+                push!(combined_epsilons, pair => combined_epsilon) 
+            end
+        end
     end
+    return combined_epsilons
 end
--- src/methods/import_parameters.jl
+++ src/methods/import_parameters.jl
@@ -2,12 +2,17 @@ function create_PCSAFTParams(raw_params; combiningrule_ϵ = "Berth")
     pure_params_dict, pair_params_dict, assoc_params_dict =
         filterparams(raw_params, ["m", "sigma", "epsilon", "n_H", "n_e"];
                      pair_params = ["k"], assoc_params = ["epsilon_assoc", "bond_vol"])
+
     segment = pure_params_dict["m"]
+
     sigma = pure_params_dict["sigma"]
     map!(x->x*1E-10, values(sigma))
-    # We can also add binary parameters using push!(sigma, Set([x1, x2]) => value) later
-    # using a combining rule.
+    merge!(sigma, combining_sigma(sigma))
+
     epsilon = pure_params_dict["epsilon"]
+    k = pair_params_dict["k"]
+    merge!(epsilon, combining_epsilon(epsilon, k))
+
     epsilon_assoc = assoc_params_dict["epsilon_assoc"]
     bond_vol = assoc_params_dict["bond_vol"]
     n_sites = Dict()
@@ -16,7 +21,7 @@ function create_PCSAFTParams(raw_params; combiningrule_ϵ = "Berth")
         n_sites[i][Set(["e"])] = pure_params_dict["n_e"][i]
         n_sites[i][Set(["H"])] = pure_params_dict["n_H"][i]
     end
-    k = pair_params_dict["k"]
+
     return PCSAFTParams(segment, sigma, epsilon, epsilon_assoc, bond_vol, n_sites, k)
 end
 
