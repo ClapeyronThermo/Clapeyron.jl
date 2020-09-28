ce6163f81ba7fc605426b4b0b2624969510d0930
paul <yewhonwa@gmail.com>
2020-09-28 16:16:33 +0800

    Added support for custom databases, variants, and redirects in database lookup.

--- src/JuliaSAFT.jl
+++ src/JuliaSAFT.jl
@@ -27,17 +27,19 @@ export system
 export get_volume, get_Psat, get_Pcrit, get_enthalpy_vap, get_pressure, get_entropy, get_chemical_potential, get_internal_energy, get_enthalpy, get_Gibbs_free_energy, get_Helmholtz_free_energy, get_isochoric_heat_capacity, get_isobaric_heat_capacity, get_thermal_compressibility, get_isentropic_compressibility, get_speed_of_sould, get_isobaric_expansitivity, get_Joule_themson_coefficient
 
 
-function system(components::Array{String,1}, method::String)
+function system(components::Array{String,1}, method::String; kwargs...)
+    # possible kwargs... are filepaths for
+    # customdatabase_like, customdatabase_unlike, customdatabase_assoc
     raw_params = retrieveparams(components, method)
     set_components = [Set([components[i]]) for i in 1:length(components)]
     if method == "PCSAFT"
-        model = PCSAFT(set_components, create_PCSAFTParams(raw_params))
+        model = PCSAFT(set_components, create_PCSAFTParams(raw_params; kwargs...))
     elseif method == "SAFTVRMie"
-        model = SAFTVRMie(set_components, create_SAFTVRMieParams(raw_params))
+        model = SAFTVRMie(set_components, create_SAFTVRMieParams(raw_params; kwargs...))
     elseif method == "ogSAFT"
-        model = ogSAFT(set_components, create_ogSAFTParams(raw_params))
+        model = ogSAFT(set_components, create_ogSAFTParams(raw_params; kwargs...))
     elseif method == "sPCSAFT"
-        model = sPCSAFT(set_components, create_sPCSAFTParams(raw_params))
+        model = sPCSAFT(set_components, create_sPCSAFTParams(raw_params; kwargs...))
     end
     return model
 end
