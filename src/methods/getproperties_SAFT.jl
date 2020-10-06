917e85eecf647f2d9da347ceea71dc379ff448ad
Pierre Walker <pierre.walker17@imperial.ac.uk>
2020-10-06 17:43:13 +0100

    Pxy solver has been added and some of paper has been written
    Pxy solver has been coded and benchmarked; more examples to come in the notebooks.

--- src/methods/getproperties_SAFT.jl
+++ src/methods/getproperties_SAFT.jl
@@ -120,32 +120,103 @@ function Obj_Crit(model::SAFT, F, T_c, v_c)
     F[1] = d2f(v_c)
     F[2] = d3f(v_c)
 end
+## Mixture saturation solver
+function get_sat_mix_Tx(model, T, x)
+    components = model.components
+    y0    = 10 .*x[1,:]./(1 .+x[1,:].*(10-1))
+    y0    = y0 ./sum(y0[i] for i in 1:length(components))
+    X     = create_z(model,x[1,:])
+    Y0    = create_z(model,y0)
+    v0    = [log10(π/6*N_A*sum(X[i]*model.params.segment[i]*model.params.sigma[i]^3 for i in components)/0.45),
+             log10(π/6*N_A*sum(Y0[i]*model.params.segment[i]*model.params.sigma[i]^3 for i in components)/1e-4)]
+    append!(v0,y0[1:end-1])
+    v_l   = []
+    v_v   = []
+    y     = deepcopy(x)
+    P_sat = []
+    for i in 1:size(x)[1]
+        f! = (F,z) -> Obj_Sat_mix_Tx(model, F, T, 10^z[1], 10^z[2], x[i,:], z[3:end])
+        j! = (J,z) -> Jac_Sat_mix_Tx(model, J, T, 10^z[1], 10^z[2], x[i,:], z[3:end])
+        r  =nlsolve(f!,j!,v0)
+        append!(v_l,10^r.zero[1])
+        append!(v_v,10^r.zero[2])
+        append!(P_sat,get_pressure(model,v_l[i],T,x[i,:]))
+        y[i,1:end-1] = r.zero[3:end]
+        y[i,end] = 1-sum(r.zero[3:end])
+        v0 = r.zero
+    end
+    return (P_sat, v_l, v_v, y)
+end
+
+function Obj_Sat_mix_Tx(model, F, T, v_l, v_v, x, y)
+    components = model.components
+    append!(y,1-sum(y[i] for i in 1:(length(components)-1)))
+
+    fun(z) = eos(model, create_z(model, z[1:end-1]), z[end], T)
+    df(z)  = ForwardDiff.gradient(fun,z)
+    X = deepcopy(x)
+    Y = deepcopy(y)
+    df_l = df(append!(X,v_l))
+    df_v = df(append!(Y,v_v))
+    for i in 1:length(components)
+        F[i] = (df_l[i]-df_v[i])/R̄/model.params.epsilon[components[i]]
+    end
+    F[end] = (df_l[end]-df_v[end])*model.params.sigma[components[1]]^3*N_A/R̄/model.params.epsilon[components[1]]
+end
+
+function Jac_Sat_mix_Tx(model, J, T, v_l, v_v, x, y)
+    components = model.components
+    append!(y,1-sum(y[i] for i in 1:(length(components)-1)))
+
+    fun(z) = eos(model, create_z(model, z[1:end-1]), z[end], T)
+    df(z)  = ForwardDiff.gradient(fun,z)
+    d2f(z) = ForwardDiff.hessian(fun,z)
+    X = deepcopy(x)
+    Y = deepcopy(y)
+    d2f_l = d2f(append!(X,v_l))
+    d2f_v = d2f(append!(Y,v_v))
+    for i in 1:(length(components))
+        J[i,1] =  log(10)*v_l*d2f_l[i,end]/R̄/model.params.epsilon[components[i]]
+        J[i,2] = -log(10)*v_v*d2f_v[i,end]/R̄/model.params.epsilon[components[i]]
+    end
+    J[end,1] = log(10)*v_l*d2f_l[end,end]*model.params.sigma[components[1]]^3*N_A/R̄/model.params.epsilon[components[1]]
+    J[end,2] =-log(10)*v_v*d2f_v[end,end]*model.params.sigma[components[1]]^3*N_A/R̄/model.params.epsilon[components[1]]
+
+    for j in 1:(length(components)-1)
+        J[end,j+2] = (d2f_v[end,end-1]-d2f_v[end,j])*model.params.sigma[components[1]]^3*N_A/R̄/model.params.epsilon[components[1]]
+    end
+
+    for i in 1:(length(components))
+        for j in 1:(length(components)-1)
+                J[i,j+2]= -(d2f_v[i,j]-d2f_v[i,end-1])/R̄/model.params.epsilon[components[i]]
+        end
+    end
+end
+
 
 ## Mixture critical point solver
-# function get_crit_mix(model::SAFT,T; units = false, output=[u"K", u"Pa", u"m^3"])
+# function get_crit_mix(model::SAFT,x_c)
 #     components = model.components
-#     f! = (F,x) -> Obj_Crit(model, F, 10^x[1],x[2:end])
-#     # j! = (J,x) -> Jac_Crit(J,eos,model,x[1]*model.params.epsilon[(1, 1)],10^x[2])
-#     x0 = [log10(π/6*N_A*model.params.segment[components[1]]*model.params.sigma[components[1]]^3/0.3),
-#           ones()]
+#     z  = create_z(model,x_c)
+#     f! = (F,x) -> Obj_Crit_mix(model, F, 10^x[2], x[1]*prod(model.params.epsilon[i]^z[i] for i in components), x_c)
+#     x0 = [1.5,log10(π/6*N_A*sum(z[i]*model.params.segment[i]*model.params.sigma[i]^3 for i in components)/0.15)]
 #     r  = nlsolve(f!,x0)
-#     T_c = r.zero[1]*model.params.epsilon[components[1]]
+#     T_c = r.zero[1]*prod(model.params.epsilon[i]^z[i] for i in components)
 #     v_c = 10^r.zero[2]
-#     p_c = get_pressure(model, v_c, T_c)
-#     if units
-#         return (uconvert(output[1], T_c*u"K"), uconvert(output[2], p_c*u"Pa"), uconvert(output[2], v_c*u"m^3"))
-#     else
-#         return (T_c, p_c, v_c)
-#     end
+#     p_c = get_pressure(model, v_c, T_c, x_c)
+#     return (T_c, p_c, v_c)
 # end
-#
-# function Obj_Crit_mix(model::SAFT, F, v_c,x_c)
-#     fun(x)  = eos(model, create_z(model, [1]), x[1], T_c)
+# #
+# function Obj_Crit_mix(model::SAFT, F, v_c,T_c,x_c)
+#     fun(x)  = eos(model, create_z(model, [x[1],1-x[1]]), v_c, T_c)
 #     df(x)   = ForwardDiff.derivative(fun,x)
 #     d2f(x)  = ForwardDiff.derivative(df,x)
 #     d3f(x)  = ForwardDiff.derivative(d2f,x)
-#     F[1] = d2f(v_c)
-#     F[2] = d3f(v_c)
+#     F[1] = d2f(x_c[1])
+#     F[2] = d3f(x_c[1])
+#     println(F)
+#     println(v_c)
+#     println(T_c)
 # end
 ## Derivative properties
 function get_pressure(model::SAFT, v, T, z=[1.]; phase = "unknown")
