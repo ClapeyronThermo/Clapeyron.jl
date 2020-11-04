ea85126464244df3e21559464ed77fe66df97376
Pierre Walker <pierre.walker17@imperial.ac.uk>
2020-11-04 13:46:02 +0000

    SAFTgammaMie calculations for pure have been added
    After consolodating the methods codes, all eos now use the same code but initial guesses are calculated in a separate function. SAFTgammaMie calculations are now functional; example notebooks to follow!

--- src/OpenSAFT.jl
+++ src/OpenSAFT.jl
@@ -38,9 +38,9 @@ export eos,ideal
 
 using Unitful
 
-include("methods/getproperties_SAFT.jl")
+include("methods/getproperties.jl")
 include("methods/getproperties_SAFT_Unitful.jl")
-include("methods/getproperties_Cubic.jl")
+include("methods/initial_guess_properties.jl")
 
 export get_volume, get_sat_pure, get_sat_mix_Tx, get_crit_pure, get_enthalpy_vap, get_pressure, get_entropy, get_chemical_potential, get_internal_energy, get_enthalpy, get_Gibbs_free_energy, get_Helmholtz_free_energy, get_isochoric_heat_capacity, get_isobaric_heat_capacity, get_isothermal_compressibility, get_isentropic_compressibility, get_speed_of_sound, get_isobaric_expansivity, get_Joule_Thomson_coefficient, get_second_virial_coeff
 end # module
