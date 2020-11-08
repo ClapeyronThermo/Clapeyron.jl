d3a7786cc5917abec63c6b8f67cfc7359c2107ee
longemen3000 <longemen3000@gmail.com>
2020-11-08 01:47:59 -0300

    add vsctor string constructors for each SAFT eos

--- src/models/model_structs.jl
+++ src/models/model_structs.jl
@@ -11,9 +11,21 @@ struct SAFTVRMie <: SAFTVRMieFamily;
     params::SAFTVRMieParams
 end
 
+function SAFTVRMie(components::T1) where T1 <: AbstractVector{T2} where T2 <: AbstractString
+    set_components = [Set([components[i]]) for i in 1:length(components)]
+    raw_params = retrieveparams(components, "SAFTVRMie"; kwargs...)
+    model = SAFTVRMie(set_components, create_SAFTVRMieParams(raw_params))
+end
+
 struct PCSAFT <: PCSAFTFamily
     components
     params::PCSAFTParams
+end
+
+function PCSAFT(components::T1) where T1 <: AbstractVector{T2} where T2 <: AbstractString
+    set_components = [Set([components[i]]) for i in 1:length(components)]
+    raw_params = retrieveparams(components, "PCSAFT"; kwargs...)
+    model = PCSAFT(set_components, create_PCSAFTParams(raw_params))
 end
 
 struct sPCSAFT <: sPCSAFTFamily
@@ -21,7 +33,19 @@ struct sPCSAFT <: sPCSAFTFamily
     params::sPCSAFTParams
 end
 
+function sPCSAFT(components::T1) where T1 <: AbstractVector{T2} where T2 <: AbstractString
+    set_components = [Set([components[i]]) for i in 1:length(components)]
+    raw_params = retrieveparams(components, "sPCSAFT"; kwargs...)
+    model = sPCSAFT(set_components, create_sPCSAFTParams(raw_params))
+end
+
 struct ogSAFT <: ogSAFTFamily
     components
     params::ogSAFTParams
+end
+
+function ogSAFT(components::T1) where T1 <: AbstractVector{T2} where T2 <: AbstractString
+    set_components = [Set([components[i]]) for i in 1:length(components)]
+    raw_params = retrieveparams(components, "ogSAFT"; kwargs...)
+    model = ogSAFT(set_components, create_ogSAFTParams(raw_params))
 end
