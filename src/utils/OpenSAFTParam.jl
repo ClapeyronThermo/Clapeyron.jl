1ab6ff09a57ae2146dedf7173912b1e6f81616f6
ypaul21 <yewhonwa@gmail.com>
2021-01-14 05:22:11 +0800

    Started writing some tests for the database

--- src/utils/OpenSAFTParam.jl
+++ src/utils/OpenSAFTParam.jl
@@ -13,9 +13,6 @@ end
 function SingleParam(x::SingleParam{T}) where T
     return SingleParam(x.name, deepcopy(x.values), deepcopy(x.ismissingvalues), x.components, x.allcomponentsites, x.sourcecsvs, deepcopy(x.sources))
 end
-function SingleParam(x::SingleParam{T}, v::Array{T,1}) where T
-    return SingleParam(x.name, v, deepcopy(x.ismissingvalues), x.components, x.allcomponentsites, x.sourcecsvs, deepcopy(x.sources))
-end
 
 struct PairParam{T} <: OpenSAFTParam
     name::String
@@ -35,17 +32,8 @@ end
 function PairParam(x::PairParam{T}) where T
     return PairParam(x.name, deepcopy(x.values), deepcopy(x.ismissingvalues), x.components, x.allcomponentsites, x.sourcecsvs, deepcopy(x.sources))
 end
-function PairParam(x::PairParam{T}, v::Array{T,2}) where T
-    return PairParam(x.name, v, deepcopy(x.ismissingvalues), x.components, x.allcomponentsites, x.sourcecsvs, deepcopy(x.sources))
-end
 function PairParam(x::SingleParam{T}) where T
     return PairParam(x.name, convertsingletopair(x.values), convert(Array{Bool},.!(convertsingletopair(convert(Array{Bool},.!(x.ismissingvalues))))), x.components, x.allcomponentsites, x.sourcecsvs, deepcopy(x.sources))
-end
-function PairParam(x::SingleParam{T}, v::Array{T,1}) where T
-    return PairParam(x.name, convertsingletopair(v), convert(Array{Bool},.!(convertsingletopair(convert(Array{Bool},.!(x.ismissingvalues))))), x.components, x.allcomponentsites, x.sourcecsvs, deepcopy(x.sources))
-end
-function PairParam(x::SingleParam{T}, v::Array{T,2}) where T
-    return PairParam(x.name, v, convert(Array{Bool},.!(convertsingletopair(convert(Array{Bool},.!(x.ismissingvalues))))), x.components, x.allcomponentsites, x.sourcecsvs, deepcopy(x.sources))
 end
 
 struct AssocParam{T} <: OpenSAFTParam
@@ -60,9 +48,6 @@ end
 
 function AssocParam(x::AssocParam{T}) where T
     return PairParam{T}(x.name, deepcopy(x.values), deepcopy(x.ismissingvalues), x.components, x.allcomponentsites, x.sourcecsvs, deepcopy(x.sources))
-end
-function AssocParam(x::AssocParam{T}, v::Array{Array{T,2},2}) where T
-    return PairParam{T}(x.name, v, deepcopy(x.ismissingvalues), x.components, x.allcomponentsites, x.sourcecsvs, deepcopy(x.sources))
 end
 
 struct GCParam <: OpenSAFTParam
