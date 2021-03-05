62475fa428995b5afb81d15335dd10ee1b14f2f7
longemen3000 <longemen3000@gmail.com>
2021-03-05 16:56:03 -0300

    support using passing types to idealmodel constructor

--- src/models/basetools.jl
+++ src/models/basetools.jl
@@ -17,6 +17,42 @@ function getsites(pairs::Dict{String,SingleParam{Int}})
 end
 
 function idealmodelselector(idealmodelstring::String, components::Array{String,1}; verbose::Bool=false)
+    normalisedidealmodelstring = normalisestring(idealmodelstring)
+    if normalisedidealmodelstring == "" 
+        modelsym = :basic
+    else
+        modelsym = Symbol(normalisedidealmodelstring)
+    end
+
+    return idealmodelselector(Val{modelsym},components,verbose=verbose)
+end
+
+function idealmodelselector(::Type{Val{:monomer}},components;verbose=false)
+    return MonomerIdeal(components; verbose=verbose)
+end
+
+function idealmodelselector(::Type{Val{:reid}},components;verbose=false)
+    return ReidIdeal(components; verbose=verbose)
+end
+
+function idealmodelselector(::Type{Val{:walker}},components;verbose=false)
+    return WalkerIdeal(components; verbose=verbose)
+end
+
+function idealmodelselector(::Type{Val{:basic}},components;verbose=false)
+    return BasicIdeal(components; verbose=verbose)
+end
+
+function getsites(pairs::Dict{String,SingleParam{Int}})
+    arbitraryparam = first(values(pairs))
+    components = arbitraryparam.components
+    allcomponentsites = arbitraryparam.allcomponentsites
+    sourcecsvs = unique([([x.sourcecsvs for x in values(pairs)]...)...])
+    allcomponentnsites = [[pairs[allcomponentsites[i][j]].values[i] for j ∈ 1:length(allcomponentsites[i])] for i ∈ 1:length(components)]  # or groupsites
+    return SiteParam(components, allcomponentsites, allcomponentnsites, sourcecsvs)
+end
+
+function idealmodelselector(idealmodelstring::String, components::Array{String,1}; verbose::Bool=false)
     #=normalisedidealmodelstring = normalisestring(idealmodelstring)
     if normalisedidealmodelstring == "monomer"
         return MonomerIdeal(components; verbose=verbose)
@@ -44,15 +80,6 @@ function idealmodelselector(::Type{Val{:monomer}},components;verbose=false)
     return MonomerIdeal(components; verbose=verbose)
 end
 
-function idealmodelselector(::Type{Val{:reid}},components;verbose=false)
-    return ReidIdeal(components; verbose=verbose)
+function idealmodelselector(idealmodel::Type{T}, components::Array{String,1}; verbose::Bool=false) where T <: IdealModel
+    return idealmodel(components,verbose=verbose)
 end
-
-function idealmodelselector(::Type{Val{:walker}},components;verbose=false)
-    return WalkerIdeal(components; verbose=verbose)
-end
-
-function idealmodelselector(::Type{Val{:basic}},components;verbose=false)
-    return BasicIdeal(components; verbose=verbose)
-end
-
