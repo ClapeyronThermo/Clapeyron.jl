6eb9f21c7e52c8dc9524249293aab472e307f8d9
longemen3000 <longemen3000@gmail.com>
2021-03-06 16:26:10 -0300

    is_phase functions work on syms

--- src/utils/misc.jl
+++ src/utils/misc.jl
@@ -1,12 +1,15 @@
 # extract all sites
-const LIQUID_STR = ("liquid","LIQUID","L","l")
-is_liquid(str::String) = str in LIQUID_STR
+const LIQUID_STR = (:liquid,:LIQUID,:L,:l)
+is_liquid(sym::Symbol) = sym in LIQUID_STR
+is_liquid(str::String) = is_liquid(Symbol(str)) 
 
-const VAPOUR_STR = ("vapor","VAPOR","VAPOUR","vapour","g","G","v","V")
-is_vapour(str::String) = str in VAPOUR_STR
+const VAPOUR_STR = (:vapor,:VAPOR,:VAPOUR,:vapour,:g,:G,:v,:V,:gas,:GAS)
+is_vapour(sym::Symbol) = sym in VAPOUR_STR
+is_vapour(str::String) = is_vapour(Symbol(str))
 
-const SUPERCRITICAL_STR = ("sc","SC","supercritical","SUPERCRITICAL")
-is_supercritical(str::String) = str in SUPERCRITICAL_STR
+const SUPERCRITICAL_STR = (:sc,:SC,:supercritical,:SUPERCRITICAL)
+is_supercritical(sym::Symbol) = sym in SUPERCRITICAL_STR
+is_supercritical(str::String) = is_vapour(Symbol(str))
 
 function ∑(iterator) #not collecting is faster
     return reduce(Base.add_sum,iterator,init=0.0)
