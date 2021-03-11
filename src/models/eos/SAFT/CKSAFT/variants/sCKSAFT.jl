a1fd03a778b91875059838abe393f25ead924922
longemen3000 <longemen3000@gmail.com>
2021-03-11 02:58:20 -0300

    typo on sCKSAFT

--- src/models/eos/SAFT/CKSAFT/variants/sCKSAFT.jl
+++ src/models/eos/SAFT/CKSAFT/variants/sCKSAFT.jl
@@ -1,14 +1,21 @@
+struct sCKSAFTParam <: EoSParam
+    segment::SingleParam{Float64}
+    sigma::PairParam{Float64}
+    epsilon::PairParam{Float64}
+    epsilon_assoc::AssocParam{Float64}
+    bondvol::AssocParam{Float64}
+end
+
 
 abstract type sCKSAFTModel <: CKSAFTModel end
 
 
-@newmodel sCKSAFT sCKSAFTModel CKSAFTParam
+@newmodel sCKSAFT sCKSAFTModel sCKSAFTParam
 
 export sCKSAFT
 function sCKSAFT(components::Array{String,1}; idealmodel=BasicIdeal, userlocations::Array{String,1}=String[], verbose=false)
-    params = getparams(components, ["SAFT/CKSAFT","properties/molarmass.csv"]; userlocations=userlocations, verbose=verbose)
+    params = getparams(components, ["SAFT/sCKSAFT","SAFT/CKSAFT","properties/molarmass.csv"]; userlocations=userlocations, verbose=verbose)
     segment = params["m"]
-    c = params["c"]
     k = params["k"]
     sigma = params["vol"]
     sigma.values .*= 6*0.74048/N_A/1e6/π
@@ -19,8 +26,8 @@ function sCKSAFT(components::Array{String,1}; idealmodel=BasicIdeal, userlocatio
     bondvol = params["bond_vol"]
     sites = SiteParam(Dict("e" => params["n_e"], "H" => params["n_H"]))
 
-    packagedparams = CKSAFTParam(segment, sigma, epsilon,c, epsilon_assoc, bondvol)
-    references = ["TODO sCKSAFT", "sTODO CKSAFT"]
+    packagedparams = sCKSAFTParam(segment, sigma, epsilon, epsilon_assoc, bondvol)
+    references = ["TODO sCKSAFT", "TODO sCKSAFT"]
 
     return sCKSAFT(packagedparams, sites, idealmodel; references=references, verbose=verbose)
 end
