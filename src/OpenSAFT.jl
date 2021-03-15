b086b8477362bf3df65ed857ee780c5332718953
ypaul21 <yewhonwa@gmail.com>
2021-03-16 04:42:15 +0800

    SAFTVRSW ported with small errors, but I'm stopping here for now. May have to come back later to fix.

--- src/OpenSAFT.jl
+++ src/OpenSAFT.jl
@@ -30,6 +30,7 @@ include("models/eos/ideal/WalkerIdeal.jl")
 include("models/eos/SAFT/PCSAFT/PCSAFT.jl")
 include("models/eos/SAFT/PCSAFT/variants/sPCSAFT.jl")
 
+include("models/eos/SAFT/SAFTVRSW/SAFTVRSW.jl")
 include("models/eos/SAFT/SAFTVRMie/SAFTVRMie.jl")
 include("models/eos/SAFT/SAFTVRMie/variants/SAFTVRQMie.jl")
 include("models/eos/SAFT/SAFTgammaMie/SAFTgammaMie.jl")
