7c581fd798abf1e45044a8d745c0f4906da352ea
longemen3000 <longemen3000@gmail.com>
2021-04-05 14:03:22 -0400

    test methods shuld work now

--- test/test_methods.jl
+++ test/test_methods.jl
@@ -1,24 +1,24 @@
 using OpenSAFT, Test
 
 @testset "SAFT methods single phase" begin
-    test_system = system("ethanol", "PCSAFT")
-    @test_broken get_volume(test_system, 1E5, 298)[1] ≈ 5.9069454773905654e-5 rtol = 0.01 #returns incorrect value
-    @test get_sat_pure(test_system, 298)[1][1] ≈ 7904.2246894075415 rtol = 0.01
-    @test get_enthalpy_vap(test_system, 298)[1] ≈ 41720.97771100548 rtol = 0.01
-    @test_broken get_crit_pure(test_system)[1][1] ≈ 1 rtol = 1E10 #T_scale not defined
-    @test get_pressure(test_system, 1, 298)[1] ≈ 1 rtol = 1E10
-    @test get_entropy(test_system, 1E5, 298)[1] ≈ 1 rtol = 1E10
-    @test get_chemical_potential(test_system, 1E5, 298)[1] ≈ 1 rtol = 1E10
-    @test get_internal_energy(test_system, 1E5, 298)[1] ≈ 1 rtol = 1E10
-    @test get_enthalpy(test_system, 1E5, 298)[1] ≈ 1 rtol = 1E10
-    @test get_Gibbs_free_energy(test_system, 1E5, 298)[1] ≈ 1 rtol = 1E10
-    @test get_Helmholtz_free_energy(test_system, 1E5, 298)[1] ≈ 1 rtol = 1E10
-    @test get_isochoric_heat_capacity(test_system, 1E5, 298)[1] ≈ 1 rtol = 1E10
-    @test get_isobaric_heat_capacity(test_system, 1E5, 298)[1] ≈ 1 rtol = 1E10
-    @test get_isothermal_compressibility(test_system, 1E5, 298)[1] ≈ 1 rtol = 1E10
-    @test get_isentropic_compressibility(test_system, 1E5, 298)[1] ≈ 1 rtol = 1E10
-    @test_broken get_speed_of_sound(test_system, 1E5, 298)[1] ≈ 1 rtol = 1E10 #requires that the model has Mr
-    @test get_isobaric_expansivity(test_system, 1E5, 298)[1] ≈ 1 rtol = 1E10
-    @test get_Joule_Thomson_coefficient(test_system, 1E5, 298)[1] ≈ 1 rtol = 1E10
-    @test get_second_virial_coeff(test_system, 298)[1] ≈ 1 rtol = 1E10
+    test_system = PCSAFT(["ethanol"])
+    @test OpenSAFT.volume(test_system, 1E5, 298) ≈ 5.9069454773905654e-5 rtol = 0.01 #returns incorrect value
+    @test OpenSAFT.sat_pure(test_system, 298)[1] ≈ 7904.2246894075415 rtol = 0.01
+    @test OpenSAFT.enthalpy_vap(test_system, 298) ≈ 41720.97771100548 rtol = 0.01
+    @test_broken OpenSAFT.crit_pure(test_system)[1] ≈ 1 rtol = 1E10 #T_scale not defined
+    @test OpenSAFT.pressure(test_system, 1, 298) ≈ 1 rtol = 1E10
+    @test OpenSAFT.entropy(test_system, 1E5, 298) ≈ 1 rtol = 1E10
+    @test OpenSAFT.chemical_potential(test_system, 1E5, 298)[1] ≈ 1 rtol = 1E10
+    @test OpenSAFT.internal_energy(test_system, 1E5, 298) ≈ 1 rtol = 1E10
+    @test OpenSAFT.enthalpy(test_system, 1E5, 298) ≈ 1 rtol = 1E10
+    @test OpenSAFT.gibbs_free_energy(test_system, 1E5, 298) ≈ 1 rtol = 1E10
+    @test OpenSAFT.helmholtz_free_energy(test_system, 1E5, 298) ≈ 1 rtol = 1E10
+    @test OpenSAFT.isochoric_heat_capacity(test_system, 1E5, 298) ≈ 1 rtol = 1E10
+    @test OpenSAFT.isobaric_heat_capacity(test_system, 1E5, 298) ≈ 1 rtol = 1E10
+    @test OpenSAFT.isothermal_compressibility(test_system, 1E5, 298) ≈ 1 rtol = 1E10
+    @test OpenSAFT.isentropic_compressibility(test_system, 1E5, 298) ≈ 1 rtol = 1E10
+    @test OpenSAFT.speed_of_sound(test_system, 1E5, 298) ≈ 1 rtol = 1E10 #requires that the model has Mr
+    @test OpenSAFT.isobaric_expansivity(test_system, 1E5, 298) ≈ 1 rtol = 1E10
+    @test OpenSAFT.joule_thomson_coefficient(test_system, 1E5, 298) ≈ 1 rtol = 1E10
+    @test OpenSAFT.second_virial_coeff(test_system, 298) ≈ 1 rtol = 1E10
 end
