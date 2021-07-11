7b20bc48ab1e2296dc2f252034e8f251458d8cbd
longemen3000 <longemen3000@gmail.com>
2021-07-11 01:46:38 -0400

    update test to use Clapeyron

--- test/test_database.jl
+++ test/test_database.jl
@@ -1,7 +1,7 @@
 
 @testset "database_lookup" begin
-    # Quick test to show that lookups in the OpenSAFT database works.
-    @test haskey(OpenSAFT.getparams(["water", "methanol"], ["SAFT/PCSAFT"]), "sigma")
+    # Quick test to show that lookups in the Clapeyron database works.
+    @test haskey(Clapeyron.getparams(["water", "methanol"], ["SAFT/PCSAFT"]), "sigma")
 
     # The rest of the test will be conducted with a custom dataset in the test_csvs directory.
     testspecies = ["sp1", "sp2", "sp3", "sp4", "sp5"]
@@ -22,34 +22,34 @@
     filepath_param_gc = ["test_csvs/group_param_test.csv"]
 
     # Check that it detects the right sites.
-    @test OpenSAFT.findsitesincsvs(testspecies, filepath_normal) == [[],
+    @test Clapeyron.findsitesincsvs(testspecies, filepath_normal) == [[],
                                                                      [],
                                                                      ["e", "e2", "H"],
                                                                      ["e", "H"],
                                                                      ["e", "e2", "H"]]
 
     # Check that it throws an error if ignore_missingsingleparams is not set to true.
-    @test_throws ErrorException OpenSAFT.getparams(testspecies; userlocations=filepath_normal)
+    @test_throws ErrorException Clapeyron.getparams(testspecies; userlocations=filepath_normal)
 
-    params = OpenSAFT.getparams(testspecies; userlocations=filepath_normal, ignore_missingsingleparams=true)
+    params = Clapeyron.getparams(testspecies; userlocations=filepath_normal, ignore_missingsingleparams=true)
 
     # Check that all the types are correct.
-    @test typeof(params["intparam"]) <: OpenSAFT.SingleParam{Int}
-    @test typeof(params["doubleparam"]) <: OpenSAFT.SingleParam{Float64}
-    @test typeof(params["boolparam"]) <: OpenSAFT.SingleParam{Bool}
-    @test typeof(params["stringparam"]) <: OpenSAFT.SingleParam{String}
+    @test typeof(params["intparam"]) <: Clapeyron.SingleParam{Int}
+    @test typeof(params["doubleparam"]) <: Clapeyron.SingleParam{Float64}
+    @test typeof(params["boolparam"]) <: Clapeyron.SingleParam{Bool}
+    @test typeof(params["stringparam"]) <: Clapeyron.SingleParam{String}
     # If column has both strings and numbers, they should all be strings.
-    @test typeof(params["mixedparam"]) <: OpenSAFT.SingleParam{String}
+    @test typeof(params["mixedparam"]) <: Clapeyron.SingleParam{String}
     # Contains missing values
-    @test typeof(params["missingparam"]) <: OpenSAFT.SingleParam{Int}
+    @test typeof(params["missingparam"]) <: Clapeyron.SingleParam{Int}
     # All missing values
-    @test typeof(params["emptyparam"]) <: OpenSAFT.SingleParam{Any}
+    @test typeof(params["emptyparam"]) <: Clapeyron.SingleParam{Any}
     # Overwrite Int with Float, and also an upgrade from single to pair param
-    @test typeof(params["overwriteparam"]) <: OpenSAFT.PairParam{Float64}
+    @test typeof(params["overwriteparam"]) <: Clapeyron.PairParam{Float64}
     # Overwrite String with Int
-    @test typeof(params["overwritestringparam"]) <: OpenSAFT.SingleParam{String}
+    @test typeof(params["overwritestringparam"]) <: Clapeyron.SingleParam{String}
     # Overwrite Int with String
-    @test typeof(params["overwriteassocparam"]) <: OpenSAFT.AssocParam{String}
+    @test typeof(params["overwriteassocparam"]) <: Clapeyron.AssocParam{String}
 
     # Check that values of "sp1" and "sp3" has been correctly overwritten.
     # "sp1" was overwritten from the same file
@@ -85,12 +85,12 @@
 
 
     # Clashing headers between association and non-association parameters are not allowed
-    @test_throws ErrorException OpenSAFT.getparams(testspecies; userlocations=filepath_clashingheaders)
+    @test_throws ErrorException Clapeyron.getparams(testspecies; userlocations=filepath_clashingheaders)
 
     # If parameter is not tagged as asymmetrical, having non-missing values across the diagonal will throw an error
-    @test_throws ErrorException OpenSAFT.getparams(testspecies; userlocations=filepath_asymmetry, ignore_missingsingleparams=true)
+    @test_throws ErrorException Clapeyron.getparams(testspecies; userlocations=filepath_asymmetry, ignore_missingsingleparams=true)
 
-    asymmetricparams = OpenSAFT.getparams(testspecies; userlocations=filepath_asymmetry, asymmetricparams=["asymmetricpair", "asymmetricassoc"], ignore_missingsingleparams=true)
+    asymmetricparams = Clapeyron.getparams(testspecies; userlocations=filepath_asymmetry, asymmetricparams=["asymmetricpair", "asymmetricassoc"], ignore_missingsingleparams=true)
     asymmetricparams["asymmetricpair"].values == [0.06  0.04  0.0  0.0   0.0
                                                   0.05  0.0   0.0  0.0   0.0
                                                   0.0   0.0   0.0  0.02  0.0
@@ -106,7 +106,7 @@
     # Also, since a non-missing value exists on the diagonal of "asymmetricpair",
     # and the diagonal contains missing values, it should throw an error
     # without ignore_missingsingleparams set to true.
-    @test_throws ErrorException OpenSAFT.getparams(testspecies; userlocations=filepath_asymmetry, asymmetricparams=["asymmetricpair", "asymmetricassoc"])
+    @test_throws ErrorException Clapeyron.getparams(testspecies; userlocations=filepath_asymmetry, asymmetricparams=["asymmetricpair", "asymmetricassoc"])
 
     # GC test
     components_gc = buildspecies(["test1", "test2", ("test3", ["grp1" => 2, "grp2" => 2, "grp3" => 3])]; usergrouplocations=filepath_gc)
--- test/test_methods.jl
+++ test/test_methods.jl
@@ -1,23 +1,23 @@
 
 @testset "SAFT methods single phase" begin
     system = PCSAFT(["ethanol"])
-    @test OpenSAFT.volume(system, 1E5, 298) ≈ 5.9069454773905654e-5 rtol = 0.01 #returns incorrect value
-    @test OpenSAFT.sat_pure(system, 298)[1] ≈ 7904.2246894075415 rtol = 0.01
-    # @test OpenSAFT.enthalpy_vap(system, 298) ≈ 41720.97771100548 rtol = 0.01
-    @test OpenSAFT.crit_pure(system)[1] ≈ 1 rtol = 1E10 #T_scale not defined
-    @test OpenSAFT.pressure(system, 1, 298) ≈ 1 rtol = 1E10
-    @test OpenSAFT.entropy(system, 1E5, 298) ≈ 1 rtol = 1E10
-    @test OpenSAFT.chemical_potential(system, 1E5, 298)[1] ≈ 1 rtol = 1E10
-    @test OpenSAFT.internal_energy(system, 1E5, 298) ≈ 1 rtol = 1E10
-    @test OpenSAFT.enthalpy(system, 1E5, 298) ≈ 1 rtol = 1E10
-    @test OpenSAFT.gibbs_free_energy(system, 1E5, 298) ≈ 1 rtol = 1E10
-    @test OpenSAFT.helmholtz_free_energy(system, 1E5, 298) ≈ 1 rtol = 1E10
-    @test OpenSAFT.isochoric_heat_capacity(system, 1E5, 298) ≈ 1 rtol = 1E10
-    @test OpenSAFT.isobaric_heat_capacity(system, 1E5, 298) ≈ 1 rtol = 1E10
-    @test OpenSAFT.isothermal_compressibility(system, 1E5, 298) ≈ 1 rtol = 1E10
-    @test OpenSAFT.isentropic_compressibility(system, 1E5, 298) ≈ 1 rtol = 1E10
-    @test OpenSAFT.speed_of_sound(system, 1E5, 298) ≈ 1 rtol = 1E10 #requires that the model has Mr
-    @test OpenSAFT.isobaric_expansivity(system, 1E5, 298) ≈ 1 rtol = 1E10
-    @test OpenSAFT.joule_thomson_coefficient(system, 1E5, 298) ≈ 1 rtol = 1E10
-    @test OpenSAFT.second_virial_coefficient(system, 298) ≈ 1 rtol = 1E10
+    @test Clapeyron.volume(system, 1E5, 298) ≈ 5.9069454773905654e-5 rtol = 0.01 #returns incorrect value
+    @test Clapeyron.sat_pure(system, 298)[1] ≈ 7904.2246894075415 rtol = 0.01
+    # @test Clapeyron.enthalpy_vap(system, 298) ≈ 41720.97771100548 rtol = 0.01
+    @test Clapeyron.crit_pure(system)[1] ≈ 1 rtol = 1E10 #T_scale not defined
+    @test Clapeyron.pressure(system, 1, 298) ≈ 1 rtol = 1E10
+    @test Clapeyron.entropy(system, 1E5, 298) ≈ 1 rtol = 1E10
+    @test Clapeyron.chemical_potential(system, 1E5, 298)[1] ≈ 1 rtol = 1E10
+    @test Clapeyron.internal_energy(system, 1E5, 298) ≈ 1 rtol = 1E10
+    @test Clapeyron.enthalpy(system, 1E5, 298) ≈ 1 rtol = 1E10
+    @test Clapeyron.gibbs_free_energy(system, 1E5, 298) ≈ 1 rtol = 1E10
+    @test Clapeyron.helmholtz_free_energy(system, 1E5, 298) ≈ 1 rtol = 1E10
+    @test Clapeyron.isochoric_heat_capacity(system, 1E5, 298) ≈ 1 rtol = 1E10
+    @test Clapeyron.isobaric_heat_capacity(system, 1E5, 298) ≈ 1 rtol = 1E10
+    @test Clapeyron.isothermal_compressibility(system, 1E5, 298) ≈ 1 rtol = 1E10
+    @test Clapeyron.isentropic_compressibility(system, 1E5, 298) ≈ 1 rtol = 1E10
+    @test Clapeyron.speed_of_sound(system, 1E5, 298) ≈ 1 rtol = 1E10 #requires that the model has Mr
+    @test Clapeyron.isobaric_expansivity(system, 1E5, 298) ≈ 1 rtol = 1E10
+    @test Clapeyron.joule_thomson_coefficient(system, 1E5, 298) ≈ 1 rtol = 1E10
+    @test Clapeyron.second_virial_coefficient(system, 298) ≈ 1 rtol = 1E10
 end
