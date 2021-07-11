5a61d171c313faf138dacb1ed4be15270ddf5dc5
longemen3000 <longemen3000@gmail.com>
2021-07-11 00:30:37 -0400

    changes on make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -4,8 +4,9 @@ using Documenter,Clapeyron
 makedocs(sitename = "Clapeyron.jl",
 format = Documenter.HTML(
     # Use clean URLs, unless built as a "local" build
-    canonical = "https://juliadocs.github.io/Clapeyron.jl/",
+    canonical = "https://ypaul21.github.io/Clapeyron.jl/",
     assets = ["assets/logo.ico"],
+    repo="github.com/ypaul21/Clapeyron.jl"
 ),
     authors = "Pierre J. Walker, Hon Wa Yew and Andrés Riedemann.",
     pages = [
@@ -14,10 +15,10 @@ format = Documenter.HTML(
         "User guide" => Any["Basic Usage"=>"user_guide/basic_usage.md",
                             "Custom Databases"=>"user_guide/custom_dtb.md",
                             "Custom Methods"=>"user_guide/custom_methods.md",
-                            "Custom Models"=>"user_guide/custom_model.md"],
+                            "Custom Models"=>"user_guide/custom_eos.md"],
         "To-do list" => "to-do_list.md"])
 
         deploydocs(;
     repo="github.com/ypaul21/Clapeyron.jl.git",
-    devbranch = "master",
+    devbranch = "development",
 )
