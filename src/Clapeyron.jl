e049315207e8d6b8cf8e4d784d7831fed63375d8
longemen3000 <longemen3000@gmail.com>
2021-08-05 03:10:44 -0400

    remove not used dependencies

--- src/Clapeyron.jl
+++ src/Clapeyron.jl
@@ -2,18 +2,14 @@ module Clapeyron
 using StaticArrays
 using LinearAlgebra
 using NLopt, NLSolvers,Roots
-#using NLSolve
 using  DiffResults, ForwardDiff
 include("solvers/Solvers.jl")
 using .Solvers
-using Combinatorics
-using NamedArrays
+using Unitful
 import LogExpFunctions
-using DataStructures: DefaultDict
 include("constants.jl")
 include("models/basetools.jl")
 include("utils/ClapeyronParam.jl")
-
 include("utils/macros.jl")
 using CSV, Tables
 include("utils/database.jl")
@@ -60,50 +56,7 @@ include("models/eos/EmpiricHelmholtz/GERG2008/GERG2008.jl")
 
 include("models/eos/SPUNG/SPUNG.jl")
 
-# include("models/param_structs.jl")
-# include("models/ideal_param_structs.jl")
-# include("models/model_structs.jl")
-# include("models/eos/combining_rules.jl")
-# include("models/import_params.jl")
-# include("models/import_ideal_params.jl")
-
-# include("models/eos/Ideal/Basic.jl")
-# include("models/eos/Ideal/Monomer.jl")
-# include("models/eos/Ideal/Walker.jl")
-# include("models/eos/Ideal/Reid.jl")
-
-# include("models/eos/SAFT/PCSAFT.jl")
-# include("models/eos/SAFT/CKSAFT.jl")
-# include("models/eos/SAFT/sCKSAFT.jl")
-# include("models/eos/SAFT/BACKSAFT.jl")
-# include("models/eos/SAFT/sPCSAFT.jl")
-# include("models/eos/SAFT/SAFTVRMie.jl")
-# include("models/eos/SAFT/SAFTVRQMie.jl")
-# include("models/eos/SAFT/SAFTVRSW.jl")
-# include("models/eos/SAFT/ogSAFT.jl")
-# include("models/eos/SAFT/LJSAFT.jl")
-# include("models/eos/SAFT/softSAFT.jl")
-# include("models/eos/SAFT/SAFTgammaMie.jl")
-
-# include("models/eos/Cubic/vdW.jl")
-# include("models/eos/Cubic/RK.jl")
-# include("models/eos/Cubic/SRK.jl")
-# include("models/eos/Cubic/PR.jl")
-# include("models/eos/Cubic/CPA.jl")
-
-# include("models/eos/eos.jl")
-
-# include("models/system.jl")
-# include("models/system2.jl")
-
-# export system,System
-# export eos,ideal
-
-using Unitful
 
 include("methods/methods.jl")
 
-# export get_volume, get_sat_pure, get_bubble_pressure, get_crit_pure, get_enthalpy_vap, get_pressure, get_entropy, get_chemical_potential, get_internal_energy, get_enthalpy, get_Gibbs_free_energy, get_Helmholtz_free_energy, get_isochoric_heat_capacity, get_isobaric_heat_capacity, get_isothermal_compressibility, get_isentropic_compressibility, get_speed_of_sound, get_isobaric_expansivity, get_Joule_Thomson_coefficient, get_second_virial_coeff
-# export eos
-# export create_z
 end # module
