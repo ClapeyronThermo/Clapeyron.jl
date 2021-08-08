71ae3b3d79c4804854c6ffc813392a2305deba77
longemen3000 <longemen3000@gmail.com>
2021-08-08 03:02:28 -0400

    make molecular_weight work on GC models

--- src/methods/methods.jl
+++ src/methods/methods.jl
@@ -1,5 +1,24 @@
-molecular_weight(model::EoSModel,z = @SVector [1.]) = 0.001*mapreduce(*,+,mw(model),z)
 mw(model::EoSModel) = paramvals(model.params.Mw)
+
+function group_molecular_weight(groups::GroupParam,mw,z = @SVector [1.])
+    n = groups.n_flattenedgroups
+    res = zero(first(z))
+    Σz = sum(z)
+    @inbounds for i in 1:length(groups.components)
+        ni = n[i]
+        gi = groups.i_groups[i]
+        mwi = zero(res)
+        for idx in 1:length(gi)
+            j = gi[idx]
+            mwi += mw[j]*ni[j]
+        end
+        res +=z[i]*mwi
+    end
+    return 0.001*res/Σz
+end
+
+
+comp_molecular_weight(mw,z = @SVector [1.]) = 0.001*mapreduce(*,+,mw,z)
 
 include("initial_guess.jl")
 include("differentials.jl")
