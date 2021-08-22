ec3fc470cede11e16287624cb0b926a58fa9ab9c
longemen3000 <longemen3000@gmail.com>
2021-08-22 02:34:13 -0400

    various fixes on SPUNG, IAPWS95, PropaneRef, RK and PR, the default value of NLSolve is now using CholeskyNewton

--- src/methods/property_solvers/singlecomponent.jl
+++ src/methods/property_solvers/singlecomponent.jl
@@ -175,7 +175,8 @@ function crit_pure(model::EoSModel,x0=nothing)
 end
 
 function obj_crit(model::EoSModel, F, T_c, V_c)
-    ∂²A∂V², ∂³A∂V³ = ∂²³f(model, V_c, T_c, SA[1.0])
+    _1 = one(T_c+V_c)
+    ∂²A∂V², ∂³A∂V³ = ∂²³f(model, V_c, T_c, SA[_1])
     F[1] = -∂²A∂V²
     F[2] = -∂³A∂V³
     return F
