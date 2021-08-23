7d869c56814fa8b6db934c8ea6b86c974c4f4d02
longemen3000 <longemen3000@gmail.com>
2021-08-23 04:15:40 -0400

    fix test errors

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using Clapeyron, Test, Unitful
 
 @testset "All tests" begin
     include("test_database.jl")
+    include("test_differentials.jl")
     include("test_models.jl")
     include("test_methods.jl")
 end
