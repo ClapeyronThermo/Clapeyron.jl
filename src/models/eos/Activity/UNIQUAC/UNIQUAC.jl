6104c55093e8d22c224ca4496ee6ae4ae5d169d0
pw0908 <pierre.walker17@imperial.ac.uk>
2021-08-26 10:42:15 +0100

    Adding UNIQUAC

---
+++ src/models/eos/Activity/UNIQUAC/UNIQUAC.jl
@@ -0,0 +1,74 @@
+struct UNIQUACParam <: EoSParam
+    a::PairParam{Float64}
+    r::SingleParam{Float64}
+    q::SingleParam{Float64}
+    Mw::SingleParam{Float64}
+end
+
+abstract type UNIQUACModel <: ActivityModel end
+
+struct UNIQUAC{c<:EoSModel} <: UNIQUACModel
+    components::Array{String,1}
+    icomponents::UnitRange{Int}
+    params::UNIQUACParam
+    puremodel::Vector{c}
+    absolutetolerance::Float64
+    references::Array{String,1}
+end
+
+has_sites(::Type{<:UNIQUACModel}) = false
+has_groups(::Type{<:UNIQUACModel}) = false
+built_by_macro(::Type{<:UNIQUACModel}) = false
+
+function Base.show(io::IO, mime::MIME"text/plain", model::UNIQUAC)
+    return eosshow(io, mime, model)
+end
+
+function Base.show(io::IO, model::UNIQUAC)
+    return eosshow(io, model)
+end
+
+Base.length(model::UNIQUAC) = Base.length(model.icomponents)
+
+molecular_weight(model::UNIQUAC,z=SA[1.0]) = comp_molecular_weight(mw(model),z)
+
+export UNIQUAC
+
+function UNIQUAC(components::Vector{String}; puremodel=PR,
+    userlocations=String[], 
+     verbose=false)
+    params = getparams(components, ["Activity/UNIQUAC/UNIQUAC_like.csv", "properties/molarmass.csv","Activity/UNIQUAC/UNIQUAC_unlike.csv"]; userlocations=userlocations, asymmetricparams=["a"], ignore_missing_singleparams=true, verbose=verbose)
+    a  = params["a"]
+    r  = params["r"]
+    q  = params["q"]
+    Mw  = params["Mw"]
+    icomponents = 1:length(components)
+    
+    init_puremodel = [puremodel([components[i]]) for i in icomponents]
+    packagedparams = UNIQUACParam(a,r,q,Mw)
+    references = String[]
+    model = UNIQUAC(components,icomponents,packagedparams,init_puremodel,1e-12,references)
+    return model
+end
+
+function activity_coefficient(model::UNIQUACModel,p,T,z)
+    a = model.params.a.values
+    q = model.params.q.values
+    r = model.params.r.values
+
+    x = z ./ sum(z)
+
+    Φ =  x.*r/sum(x[i]*r[i] for i ∈ @comps)
+    θ = x.*q/sum(x[i]*q[i] for i ∈ @comps)
+    τ = @. exp(-a/T)
+    lnγ_comb = @. log(Φ/x)+(1-Φ/x)-5*q*(log(Φ/θ)+(1-Φ/θ))
+    lnγ_res  = q.*(1 .-log.(sum(θ[i]*τ[i,:] for i ∈ @comps)) .-sum(θ[i]*τ[:,i]/sum(θ[j]*τ[j,i] for j ∈ @comps) for i ∈ @comps))
+    return exp.(lnγ_comb+lnγ_res)
+end
+
+function excess_gibbs_free_energy(model::UNIQUACModel,p,T,z)
+    lnγ = log.(activity_coefficient(model,p,T,z))
+    return sum(z[i]*R̄*T*lnγ[i] for i ∈ @comps)
+end
+
+is_splittable(::UNIQUAC) = true
