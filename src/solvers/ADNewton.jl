158f08c96d61ffc069365da9a2e9e45b9dacbd91
longemen3000 <longemen3000@gmail.com>
2021-08-30 18:44:52 -0400

    rr_vle_vapor_fraction converges, adding tests

--- src/solvers/ADNewton.jl
+++ src/solvers/ADNewton.jl
@@ -35,12 +35,28 @@ end
     Performs newton root finding (via Roots.jl), but the derivatives are calculated with ForwardDiff.jl
     kwargs are passed to `Roots.find_zero`
 """
-function ad_newton(f,x0;kwargs...)
-    function f0(x)
-        fx,∂f∂x = f∂f(f,x)
+function ad_newton(f::F,x0;xatol=nothing, xrtol=nothing, maxevals = 100) where F
+    function f0(_x)
+        fx,∂f∂x = f∂f(f,_x)
         return fx,fx/∂f∂x
     end
-    return Roots.find_zero(f0,x0,Roots.Newton();kwargs...)
+    x = float(x0)
+    T = typeof(x)
+    atol = xatol !== nothing ? xatol : oneunit(T) * (eps(one(T)))^(4/5)
+    rtol = xrtol !== nothing ? xrtol : eps(one(T))^(4/5)
+    xo = T(Inf)
+    for i in 1:maxevals
+        fx, Δx = f0(x)
+        iszero(fx) && return x
+
+        x -= Δx
+
+        if isapprox(x, xo, atol=atol, rtol=rtol)
+            return x
+        end
+        xo = x
+    end
+    return zero(x0)/zero(x0)
 end
 
 
