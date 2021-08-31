5b77a544691db269b06e478810d9a3b5f071ec95
pw0908 <pierre.walker17@imperial.ac.uk>
2021-08-31 10:02:16 +0100

    Fix cubics
    No clue how I made that mistake...

--- src/models/eos/cubic/translation/Peneloux.jl
+++ src/models/eos/cubic/translation/Peneloux.jl
@@ -18,6 +18,6 @@ end
 function translation(model::CubicModel,V,T,z,translation_model::PenelouxTranslation)
     Tc = model.params.Tc.values
     Pc = model.params.Pc.values
-    Vc = model.params.Vc.values
+    Vc = translation_model.params.Vc.values
     return @. -0.252*R̄*Tc/Pc*(1.5448*Pc*Vc/(R̄*Tc)-0.4024)
 end
--- src/models/eos/cubic/translation/Rackett.jl
+++ src/models/eos/cubic/translation/Rackett.jl
@@ -18,6 +18,6 @@ end
 function translation(model::CubicModel,V,T,z,translation_model::RackettTranslation)
     Tc = model.params.Tc.values
     Pc = model.params.Pc.values
-    Vc = model.params.Vc.values
+    Vc = translation_model.params.Vc.values
     return @. 0.40768*R̄*Tc/Pc*(0.29441-Pc*Vc/(R̄*Tc))
 end
