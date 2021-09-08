6f5c3432c38f99650a2724d99970137a4b7e5bb6
longemen3000 <longemen3000@gmail.com>
2021-09-08 14:14:07 -0300

    optimize PCSAFT, part 2

--- src/models/eos/SAFT/PCSAFT/PCSAFT.jl
+++ src/models/eos/SAFT/PCSAFT/PCSAFT.jl
@@ -38,28 +38,80 @@ function a_hc(model::PCSAFTModel, V, T, z)
     Σz = sum(z)
     m = model.params.segment.values
     m̄ = dot(z, m)/Σz
-    return m̄*@f(a_hs) - ∑(z[i]*(m[i]-1)*log(@f(g_hs,i,i)) for i ∈ @comps)/Σz
+    ϵᵢᵢ = model.params.epsilon.diagvalues
+    σᵢᵢ = model.params.sigma.diagvalues
+    ζ0,ζ1,ζ2,ζ3 = @f(ζ0123)
+    #constants for g_hs
+    c1 = 1/(1-ζ3)
+    c2 = 3ζ2/(1-ζ3)^2
+    c3 = 2ζ2^2/(1-ζ3)^3
+
+    a_hs = 1/ζ0 * (3ζ1*ζ2/(1-ζ3) + ζ2^3/(ζ3*(1-ζ3)^2) + (ζ2^3/ζ3^2-ζ0)*log(1-ζ3))
+    res = zero(a_hs)
+    for i ∈ @comps
+        dᵢ = σᵢᵢ[i] * (1 - 0.12exp(-3ϵᵢᵢ[i]/T))
+        di,dj = dᵢ,dᵢ
+        g_hsᵢᵢ = c1 + di*dj/(di+dj)*c2 + (di*dj/(di+dj))^2*c3
+        res += z[i]*(m[i]-1)*log(g_hsᵢᵢ)
+    end
+    #return  m̄*@f(a_hs) - ∑(z[i]*(m[i]-1)*log(@f(g_hs,i,i)) for i ∈ @comps)/Σz
+    return m̄*a_hs - res/sum(z)
 end
 
 function a_disp(model::PCSAFTModel, V, T, z)
     Σz = sum(z)
     m = model.params.segment.values
     m̄ = dot(z, m)/Σz
-    return -2*π*N_A*Σz/V*@f(I,1)*@f(m2ϵσ3, 1) - π*m̄*N_A*Σz/V*@f(C1)*@f(I,2)*@f(m2ϵσ3,2)
+    m2ϵσ3₁,m2ϵσ3₂ = @f(m2ϵσ3)
+    return -2*π*N_A*Σz/V*@f(I,1)*m2ϵσ3₁ - π*m̄*N_A*Σz/V*@f(C1)*@f(I,2)*m2ϵσ3₂
 end
 
 function d(model::PCSAFTModel, V, T, z, i)
-    ϵii = model.params.epsilon.diagvalues[i]
-    σii = model.params.sigma.diagvalues[i]
-    return σii * (1 - 0.12exp(-3ϵii/T))
+    ϵᵢᵢ = model.params.epsilon.diagvalues[i]
+    σᵢᵢ = model.params.sigma.diagvalues[i]
+    return σᵢᵢ * (1 - 0.12exp(-3ϵᵢᵢ/T))
 end
 
 function ζ(model::PCSAFTModel, V, T, z, n)
-    ∑z = ∑(z)
-    x = z * (one(∑z)/∑z)
     m = model.params.segment.values
-    res = N_A*∑z*π/6/V * ∑((x[i]*m[i]*@f(d,i)^n for i ∈ @comps))
+    ϵᵢᵢ = model.params.epsilon.diagvalues
+    σᵢᵢ = model.params.sigma.diagvalues
+    res = zero(V+T+first(z))
+    for i ∈ @comps
+        dᵢ = σᵢᵢ[i] * (1 - 0.12exp(-3ϵᵢᵢ[i]/T))
+        res += z[i]*m[i]*dᵢ^n
+    end
+    res *= N_A*π/6/V
+    return res
 end
+
+function ζ0123(model::PCSAFTModel, V, T, z)
+    m = model.params.segment.values
+    ϵᵢᵢ = model.params.epsilon.diagvalues
+    σᵢᵢ = model.params.sigma.diagvalues
+    ζ0 = zero(V+T+first(z))
+    ζ1 = ζ0
+    ζ2 = ζ0
+    ζ3 = ζ0
+    for i ∈ @comps
+        dᵢ = σᵢᵢ[i] * (1 - 0.12exp(-3ϵᵢᵢ[i]/T))
+        zᵢmᵢ = z[i]*m[i]
+        d1 = dᵢ
+        d2 = d1*d1
+        d3 = d2*d1
+        ζ0 += zᵢmᵢ
+        ζ1 += zᵢmᵢ*d1
+        ζ2 += zᵢmᵢ*d2
+        ζ3 += zᵢmᵢ*d3
+    end
+    NV = N_A*π/6/V
+    ζ0 *= NV
+    ζ1 *= NV
+    ζ2 *= NV
+    ζ3 *= NV
+    return ζ0,ζ1,ζ2,ζ3
+end
+
 
 function g_hs(model::PCSAFTModel, V, T, z, i, j)
     di = @f(d,i)
@@ -69,14 +121,12 @@ function g_hs(model::PCSAFTModel, V, T, z, i, j)
     return 1/(1-ζ3) + di*dj/(di+dj)*3ζ2/(1-ζ3)^2 + (di*dj/(di+dj))^2*2ζ2^2/(1-ζ3)^3
 end
 
+#=
 function a_hs(model::PCSAFTModel, V, T, z)
-    ζ0 = @f(ζ,0)
-    ζ1 = @f(ζ,1)
-    ζ2 = @f(ζ,2)
-    ζ3 = @f(ζ,3)
+    ζ0,ζ1,ζ2,ζ3 = @f(ζ0123)
     return 1/ζ0 * (3ζ1*ζ2/(1-ζ3) + ζ2^3/(ζ3*(1-ζ3)^2) + (ζ2^3/ζ3^2-ζ0)*log(1-ζ3))
 end
-
+=#
 function C1(model::PCSAFTModel, V, T, z)
     m = model.params.segment.values
     m̄ = dot(z, m)/sum(z)
@@ -84,11 +134,25 @@ function C1(model::PCSAFTModel, V, T, z)
     return (1 + m̄*(8η-2η^2)/(1-η)^4 + (1-m̄)*(20η-27η^2+12η^3-2η^4)/((1-η)*(2-η))^2)^-1
 end
 
-function m2ϵσ3(model::PCSAFTModel, V, T, z, n = 1)
+function m2ϵσ3(model::PCSAFTModel, V, T, z)
     m = model.params.segment.values
     σ = model.params.sigma.values
     ϵ = model.params.epsilon.values
-    return ∑(z[i]*z[j]*m[i]*m[j] * (ϵ[i,j]*(1)/T)^n * σ[i,j]^3 for i ∈ @comps, j ∈ @comps)/(sum(z)^2)
+    m2ϵσ3₂ = zero(V+T+first(z))
+    m2ϵσ3₁ = m2ϵσ3₂
+    @inbounds for i ∈ @comps
+        for j ∈ @comps
+            constant = z[i]*z[j]*m[i]*m[j] * σ[i,j]^3
+            exp1 = (ϵ[i,j]/T)
+            exp2 = exp1*exp1
+            m2ϵσ3₁ += constant*exp1
+            m2ϵσ3₂ += constant*exp2
+        end
+    end
+    Σz = sum(z)
+    k = (1/Σz)^2
+    return k*m2ϵσ3₁,k*m2ϵσ3₂
+    #return ∑(z[i]*z[j]*m[i]*m[j] * (ϵ[i,j]*(1)/T)^n * σ[i,j]^3 for i ∈ @comps, j ∈ @comps)/(sum(z)^2)
 end
 
 function I(model::PCSAFTModel, V, T, z, n)
@@ -97,10 +161,10 @@ function I(model::PCSAFTModel, V, T, z, n)
     σ = model.params.sigma.values
     η = @f(ζ,3)
     if n == 1
-        corr = PCSAFT_consts.corr1
+        corr = PCSAFTconsts.corr1
     elseif n == 2
        
-        corr = PCSAFT_consts.corr2
+        corr = PCSAFTconsts.corr2
     end
 
     res = zero(η)
@@ -158,26 +222,6 @@ function Δ(model::PCSAFTModel, V, T, z, i, j, a, b)
 end
 
 const PCSAFTconsts = (
-    corr1 =
-    [0.9105631445 -0.3084016918 -0.0906148351;
-    0.6361281449 0.1860531159 0.4527842806;
-    2.6861347891 -2.5030047259 0.5962700728;
-    -26.547362491 21.419793629 -1.7241829131;
-    97.759208784 -65.255885330 -4.1302112531;
-    -159.59154087 83.318680481 13.776631870;
-    91.297774084 -33.746922930 -8.6728470368],
-
-    corr2 =
-    [0.7240946941 -0.5755498075 0.0976883116;
-    2.2382791861 0.6995095521 -0.2557574982;
-    -4.0025849485 3.8925673390 -9.1558561530;
-    -21.003576815 -17.215471648 20.642075974;
-    26.855641363 192.67226447 -38.804430052;
-    206.55133841 -161.82646165 93.626774077;
-    -355.60235612 -165.20769346 -29.666905585]
-)
-
-const PCSAFT_consts = (
     corr1 =
     [(0.9105631445,-0.3084016918, -0.0906148351),
     (0.6361281449, 0.1860531159, 0.4527842806),
