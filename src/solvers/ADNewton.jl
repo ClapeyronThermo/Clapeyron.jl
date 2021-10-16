1fc8d43b0418de9196e0bcd1d962678724c997c2
longemen3000 <longemen3000@gmail.com>
2021-10-16 02:56:37 -0300

    move halley to Solvers module, fix VRQ

--- src/solvers/ADNewton.jl
+++ src/solvers/ADNewton.jl
@@ -26,27 +26,28 @@ function f∂f∂2f(f,x::T) where T
 end
 
 """
-    ad_newton(f,x0;kwargs...)
+    ad_newton(f,x0;atol = oneunit(T) * (eps(one(T)))^(4/5),rtol = )
 
     Performs newton root finding (via Roots.jl), but the derivatives are calculated with ForwardDiff.jl
     kwargs are passed to `Roots.find_zero`
 """
-function ad_newton(f::F,x0;xatol=nothing, xrtol=nothing, maxevals = 100) where F
-    function f0(_x)
-        fx,∂f∂x = f∂f(f,_x)
-        return fx,fx/∂f∂x
-    end
-    x = float(x0)
-    T = typeof(x)
-    atol = xatol !== nothing ? xatol : oneunit(T) * (eps(one(T)))^(4/5)
-    rtol = xrtol !== nothing ? xrtol : eps(one(T))^(4/5)
+function ad_newton(f::F,x0::T;
+    rtol= (eps(one(T)))^(4/5),
+    atol=rtol*oneunit(T),
+    max_iters = 100) where {F,T}
+    return newton(Base.Fix1(f∂f,f),x0;rtol,atol,max_iters)
+end
+
+function newton(f::F,x0::T;
+    rtol= (eps(one(T)))^(4/5),
+    atol=rtol*oneunit(T),
+    max_iters = 100) where {F,T} 
     xo = T(Inf)
-    for i in 1:maxevals
-        fx, Δx = f0(x)
+    for _ in 1:max_iters
+        fx, gx = f(x)
+        Δx = fx/gx
         iszero(fx) && return x
-
         x -= Δx
-
         if isapprox(x, xo, atol=atol, rtol=rtol)
             return x
         end
@@ -55,4 +56,19 @@ function ad_newton(f::F,x0;xatol=nothing, xrtol=nothing, maxevals = 100) where F
     return zero(x0)/zero(x0)
 end
 
+function halley(fgh::F,x0::T;
+    rtol= (eps(one(T)))^(4/5),
+    atol=rtol*oneunit(T),
+    max_iters = 100) where {F,T}
+    for _ in 1:max_iters
+        ff,gg,hh = fgh(x0)
+        ff < eps(x0) && return x0
+        d = ff/gg/(1-ff*hh/(2*gg^2))
+        if isapprox(x0, x0-d, atol=atol, rtol=rtol)
+            return x0
+        end
+        x0=x0-d
+    end
+    return zero(x0)/zero(x0)
+end
 
