4d18fd72aa681e6ffd86371a5465bef88555fe31
longemen3000 <longemen3000@gmail.com>
2021-11-12 04:23:36 -0300

    fix errors caused by change of base and others

--- src/models/eos/SAFT/equations.jl
+++ src/models/eos/SAFT/equations.jl
@@ -22,7 +22,7 @@ function issite(i,a,ij,ab)
     ia1 = (i1,a1)
     ia2 = (i2,a2)
     return (ia == ia1) | (ia == ia2)
-end 
+end
 
 function X!(output,input,pack_indices,delta,modelsites,ρ,z)
     
@@ -30,45 +30,19 @@ function X!(output,input,pack_indices,delta,modelsites,ρ,z)
     _1 = one(_0)
     Xinput = PackedVofV(pack_indices,input)
     Xoutput = PackedVofV(pack_indices,output)
-    #=
-    n = modelsites.n_flattenedsites
-    sitesⱼ = modelsites.i_flattenedsites
-    for i ∈ 1:length(modelsites.components)
-        sitesᵢ = modelsites.i_sites[i]
+    n = modelsites.n_sites
+    for i ∈ 1:length(modelsites.components) #for i ∈ comps
+        sitesᵢ = 1:length(n[i]) #sites are normalized, with independent indices for each component
         iszero(length(sitesᵢ)) && continue
-        idxi = sitesⱼ[i]
-        for a ∈ sitesᵢ
+        for a ∈ sitesᵢ #for a ∈ sites(comps(i))
             ∑X = _0
-            for (idx,ij,ab) in indices(delta)
-                if issite(i,a,ij,ab)
-                    j = complement_index(i,ij)
-                    b = complement_index(a,ab)
-                    idxj = sitesⱼ[j]
-                    _b =  idxj[b]
-                    nj = n[j]
-                    njb = nj[b]
-                    ∑X += ρ*njb*z[j]*Xoutput[j][_b]*delta.values[idx]   
-                end
-            end
-            _a = idxi[a]
-            rhs = _1/(_1 +∑X)
-            Xoutput[i][_a] = rhs
-        end
-    end=#
-        n = modelsites.n_sites
-    for i ∈ 1:length(modelsites.components)
-        sitesᵢ = modelsites.i_sites[i]
-        iszero(length(sitesᵢ)) && continue
-        for a ∈ sitesᵢ
-            ∑X = _0
-            for (idx,ij,ab) in indices(delta)
+            for (idx,ij,ab) in indices(delta) #iterating for all sites
                 if issite(i,a,ij,ab)
                     j = complement_index(i,ij)
                     b = complement_index(a,ab)
                     nj = n[j]
                     njb = nj[b]
                     ∑X += ρ*njb*z[j]*Xinput[j][b]*delta.values[idx]
-                    
                 end
             end
             rhs = _1/(_1 +∑X)
@@ -112,38 +86,27 @@ function X_exact1(model,V,T,z,data=nothing)
     else
         _Δ = @f(Δ,i,j,a,b,data)
     end
+    
     ρ = N_A/V
     zi = z[i]
     zj = z[j]
-    ni = model.sites.n_flattenedsites[i]
+    ni = model.sites.n_sites[i]
     na = ni[a]
-    nj = model.sites.n_flattenedsites[j]
+    nj = model.sites.n_sites[j]
     nb = nj[b]
     ρ = N_A/V
     kia = na*zi*ρ*_Δ
     kjb = nb*zj*ρ*_Δ
     #kia*x*x + x(kjb-kia+1) - 1 = 0
-    a = kia
-    b = 1 -kia + kjb
-    c = -1
-    xia = -2*c/(b + sqrt(b*b - 4*a*c))
+    _a = kia
+    _b = 1 -kia + kjb
+    _c = -1
+    xia = -2*_c/(_b + sqrt(_b*_b - 4*_a*_c))
     xjb = 1/(1+kia*xia)
-    if (i,a) < (j,b)
-        return (xia,xjb)
-    else
-        return (xjb,xia)
-    end 
+    return ifelse((i,a) < (j,b),(xia,xjb),(xjb,xia))
 end
 
-function a_assoc(model::Union{SAFTModel,CPAModel}, V, T, z)
-    _0 = zero(V+T+first(z))
-    nn = length(model.params.bondvol.values.values)
-    iszero(nn) && return _0
-    X_ = @f(X)
-    return @f(_a_assoc,X_)
-end
-
-function a_assoc(model::Union{SAFTModel,CPAModel}, V, T, z,data)
+function a_assoc(model::Union{SAFTModel,CPAModel}, V, T, z,data=nothing)
     _0 = zero(V+T+first(z))
     nn = length(model.params.bondvol.values.values)
     iszero(nn) && return _0
@@ -153,20 +116,16 @@ end
 
 function _a_assoc(model::Union{SAFTModel,CPAModel}, V, T, z,X_)
     _0 = zero(first(X_.v))
-    n = model.sites.n_flattenedsites
-    _idx = model.sites.i_flattenedsites
+    n = model.sites.n_sites
     res = _0
     resᵢₐ = _0
     for i ∈ @comps
-        sitesᵢ = @sites(i)
-        _i = _idx[i] 
-        iszero(length(sitesᵢ)) && continue
         ni = n[i]
+        iszero(length(ni)) && continue        
         Xᵢ = X_[i]
         resᵢₐ = _0
-        for a ∈ sitesᵢ
-            _a = _i[a]
-            Xᵢₐ = Xᵢ[_a]
+        for (a,nᵢₐ) ∈ pairs(ni)
+            Xᵢₐ = Xᵢ[a]
             nᵢₐ = ni[a]
             resᵢₐ +=  nᵢₐ* (log(Xᵢₐ) - Xᵢₐ/2 + 0.5)
         end
@@ -176,6 +135,11 @@ function _a_assoc(model::Union{SAFTModel,CPAModel}, V, T, z,X_)
 
 end
 
+function show_param(param)
+    for i in fieldnames(typeof(param))
+        display(getfield(param,i))
+    end
+end
 
 #res =  ∑(z[i]*∑(n[i][a] * (log(X_[i][a]) - X_[i][a]/2 + 0.5) for a ∈ @sites(i)) for i ∈ @comps)/sum(z)
 
