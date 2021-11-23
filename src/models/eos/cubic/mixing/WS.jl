ece9a3589c1cca4d22f3bfc400e1d995f92c799c
longemen3000 <longemen3000@gmail.com>
2021-11-23 17:39:53 -0300

    fix WS

--- src/models/eos/cubic/mixing/WS.jl
+++ src/models/eos/cubic/mixing/WS.jl
@@ -15,16 +15,17 @@ function WSRule(components::Vector{String}; activity = Wilson, userlocations::Ve
 end
 
 WS_λ(::PRModel) = 0.6232252401402305 #1/(2*√(2))*log((2+√(2))/(2-√(2)))
-WS_λ(::RKModel) = 0.6931471805599453 #log(2)
+WS_λ(::RKModel) = 0.6931471805599453#log(2)
 
 function mixing_rule(model::Union{RKModel,PRModel},V,T,z,mixing_model::WSRuleModel,α,a,b,c)
     λ = WS_λ(model)
     n = sum(z)
     invn = (one(n)/n)
-    Σab = sum(z[i]*a[i,i]*α[i]/b[i,i]/(R̄*T) for i ∈ @comps)*invn
-    num = Σab
-    den = 1 - (Σab-excess_gibbs_free_energy(mixing_model.activity,1e5,T,z)/(n*R̄*T)/λ)
-    c̄ = dot(z,c)/n
+    Σab = sum(z[i]*a[i,i]*α[i]/b[i,i]/(n*R̄*T) for i ∈ @comps)
+    num = sum(z[i]*(b[i,i]-a[i,i]*α[i]/(R̄*T)) for i ∈ @comps)*invn
+    gE = excess_gibbs_free_energy(mixing_model.activity,1e5,T,z)
+    den = 1 - (Σab-gE/(n*R̄*T)/λ)
+    c̄ = dot(z,c)*invn
     b̄  = num/den
     ā  = R̄*T*(b̄-num)
     return ā,b̄,c̄
