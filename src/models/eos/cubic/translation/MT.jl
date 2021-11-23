82c3cf0c146abf4ff1cc2c65f8d88f13740f8427
longemen3000 <longemen3000@gmail.com>
2021-11-23 03:17:38 -0300

    something is wrong with WS

--- src/models/eos/cubic/translation/MT.jl
+++ src/models/eos/cubic/translation/MT.jl
@@ -19,13 +19,10 @@ function translation(model::CubicModel,V,T,z,translation_model::MTTranslation)
     Tc = model.params.Tc.values
     Pc = model.params.Pc.values
     ω  = translation_model.params.acentricfactor.values
-
     Zc = @. 0.289-0.0701*ω-0.0207*ω^2
     β  = @. -10.2447-28.6312*ω
-
     t0 = @. R̄*Tc/Pc*(-0.014471+0.067498*ω-0.084852*ω^2+0.067298*ω^3-0.017366*ω^4)
     tc = @. R̄*Tc/Pc*(0.3074-Zc)
-
     Tr = @. T/Tc
     return @. t0+(tc-t0)*exp(β*abs(1-Tr))
 end
