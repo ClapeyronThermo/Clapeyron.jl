a3c08d8cc72b1843c8d93bd009c76b66828398b3
pw0908 <pierre.walker17@imperial.ac.uk>
2021-12-15 02:03:00 -0800

    add bubble temperature method for activity models

--- src/models/eos/Activity/equations.jl
+++ src/models/eos/Activity/equations.jl
@@ -28,3 +28,34 @@ function bubble_pressure(model::ActivityModel,T,x)
     y     = x.*γ.*p_sat ./ p
     return (p,y)
 end
+
+function bubble_temperature(model::ActivityModel,p,x;T0=nothing)
+    f(z) = Obj_bubble_temperature(model,z,p,x)
+    if T0===nothing
+        pure = model.puremodel
+        sat = saturation_temperature.(pure,p)
+        Ti   = zero(x)
+        for i ∈ 1:length(x)
+            if isnan(sat[i][1])
+                Tc,pc,vc = crit_pure(pure[i])
+                g(x) = p-pressure(pure[i],vc,x,[1.])
+                Ti[i] = Roots.find_zero(g,(Tc))
+            else
+                Ti[i] = sat[i][1]
+            end
+        end
+        T = Roots.find_zero(f,(minimum(Ti)*0.9,maximum(Ti)*1.1))
+    else
+        T = Roots.find_zero(f,T0)
+    end
+    p,y = bubble_pressure(model,T,x)
+    return (T,y)
+end
+
+function Obj_bubble_temperature(model::ActivityModel,T,p,x)
+    sat = saturation_pressure.(model.puremodel,T)
+    p_sat = [tup[1] for tup in sat]
+    γ     = activity_coefficient(model,1e-4,T,x)
+    y     = x.*γ.*p_sat ./ p
+    return sum(y)-1
+end
