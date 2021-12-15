db37585f0488421323e38a3b6dae747f33d7945f
pw0908 <pierre.walker17@imperial.ac.uk>
2021-12-15 02:02:36 -0800

    update uniquac

--- src/models/eos/Activity/UNIQUAC/UNIQUAC.jl
+++ src/models/eos/Activity/UNIQUAC/UNIQUAC.jl
@@ -2,6 +2,7 @@ struct UNIQUACParam <: EoSParam
     a::PairParam{Float64}
     r::SingleParam{Float64}
     q::SingleParam{Float64}
+    q_p::SingleParam{Float64}
     Mw::SingleParam{Float64}
 end
 
@@ -25,11 +26,12 @@ function UNIQUAC(components::Vector{String}; puremodel=PR,
     a  = params["a"]
     r  = params["r"]
     q  = params["q"]
+    q_p = params["q_p"]
     Mw  = params["Mw"]
     icomponents = 1:length(components)
     
     init_puremodel = [puremodel([components[i]]) for i in icomponents]
-    packagedparams = UNIQUACParam(a,r,q,Mw)
+    packagedparams = UNIQUACParam(a,r,q,q_p,Mw)
     references = String[]
     model = UNIQUAC(components,icomponents,packagedparams,init_puremodel,1e-12,references)
     return model
@@ -38,14 +40,16 @@ end
 function activity_coefficient(model::UNIQUACModel,p,T,z)
     a = model.params.a.values
     q = model.params.q.values
+    q_p = model.params.q_p.values
     r = model.params.r.values
 
     x = z ./ sum(z)
 
     Φ =  x.*r/sum(x[i]*r[i] for i ∈ @comps)
     θ = x.*q/sum(x[i]*q[i] for i ∈ @comps)
+    θ_p = x.*q_p/sum(x[i]*q_p[i] for i ∈ @comps)
     τ = @. exp(-a/T)
     lnγ_comb = @. log(Φ/x)+(1-Φ/x)-5*q*(log(Φ/θ)+(1-Φ/θ))
-    lnγ_res  = q.*(1 .-log.(sum(θ[i]*τ[i,:] for i ∈ @comps)) .-sum(θ[i]*τ[:,i]/sum(θ[j]*τ[j,i] for j ∈ @comps) for i ∈ @comps))
+    lnγ_res  = q_p.*(1 .-log.(sum(θ_p[i]*τ[i,:] for i ∈ @comps)) .-sum(θ_p[i]*τ[:,i]/sum(θ_p[j]*τ[j,i] for j ∈ @comps) for i ∈ @comps))
     return exp.(lnγ_comb+lnγ_res)
 end
