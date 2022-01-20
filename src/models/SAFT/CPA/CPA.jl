f2707d9bdd389dafb33303139de0d8709e3231e3
pw0908 <pierre.walker17@imperial.ac.uk>
2022-01-20 15:50:52 -0800

    Fix CPA

--- src/models/SAFT/CPA/CPA.jl
+++ src/models/SAFT/CPA/CPA.jl
@@ -79,7 +79,7 @@ T_scale(model::CPAModel,z=SA[1.0]) = T_scale(model.cubicmodel,z)
 p_scale(model::CPAModel,z=SA[1.0]) = p_scale(model.cubicmodel,z)
 
 function x0_crit_pure(model::CPAModel)
-    lb_v = lb_volume(model,z)
+    lb_v = lb_volume(model)
     if isempty(model.params.epsilon_assoc.values[1,1])
         [2.0, log10(lb_v/0.3)]
     else
