9326c66e17f092b379d5073e472bda49271b8584
longemen3000 <longemen3000@gmail.com>
2022-02-02 04:53:12 -0300

    more docs

--- src/models/ideal/BasicIdeal.jl
+++ src/models/ideal/BasicIdeal.jl
@@ -20,11 +20,12 @@ None
 
 None
 
-Default Ideal Model. It does not require any parameters. its helmholtz energy is defined as:
+Default Ideal Model. Constant specific heat capacity equal to `5R/2`. it's Helmholtz energy is equal to:
 ```
     a₀ = A₀/nRT =  ∑(xᵢlog(nxᵢ/V)) - 1 - 1.5log(T)
 ```
 """
+BasicIdeal
 
 export BasicIdeal
 function BasicIdeal(components::Array{String,1}; userlocations::Array{String,1}=String[], verbose=false)
