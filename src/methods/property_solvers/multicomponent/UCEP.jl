896d42e5d654cb16f10abf12e1474a1b393eaf48
longemen3000 <longemen3000@gmail.com>
2022-02-10 01:34:22 -0300

    use common LdetM function on UCEP and UCST_mix to speed up compilation

--- src/methods/property_solvers/multicomponent/UCEP.jl
+++ src/methods/property_solvers/multicomponent/UCEP.jl
@@ -33,26 +33,22 @@ function UCEP_mix(model::EoSModel;v0=nothing)
     return (T, p, V_l, V_v, x, y)
 end
 
+
+
 function Obj_UCEP_mix(model::EoSModel,F,x,y,V_l,V_v,T,ts,ps)
     y    = FractionVector(y)
     x    = FractionVector(x)
-    f(z) = eos(model,V_l,T,z)
-    H(z) = ForwardDiff.hessian(f,z)/(R̄*T)
-    L(z) = det(H(z))
-    dL(z) = ForwardDiff.gradient(L,z)
-    M(z) = [H(z)[1:end-1,:];transpose(dL(z))]
-    
     μ_l = VT_chemical_potential(model,V_l,T,x)
     μ_v = VT_chemical_potential(model,V_v,T,y)
     p_l = pressure(model,V_l,T,x)
     p_v = pressure(model,V_v,T,y)
-    
+    L,detM = LdetM(model,V_l,T,x) 
     for i in 1:length(x)
         F[i] = (μ_l[i]-μ_v[i])/(R̄*ts[i])
     end
     F[3] = (p_l-p_v)/ps
-    F[4] = L(x)
-    F[5] = det(M(x))
+    F[4] = L
+    F[5] = detM
     return F
 end
 """
@@ -75,3 +71,4 @@ function x0_UCEP_mix(model::EoSModel)
     v0 = x0_bubble_pressure(model,T0,[x0,1-x0])
     return [x0,y0,v0[1],v0[2],T0]
 end
+
