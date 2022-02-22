4902e76684d3b058229c4fdc89a36a8235d19339
longemen3000 <longemen3000@gmail.com>
2022-02-22 19:40:31 -0300

    minor change to UNIQUAC

--- src/models/Activity/UNIQUAC/UNIQUAC.jl
+++ src/models/Activity/UNIQUAC/UNIQUAC.jl
@@ -36,7 +36,26 @@ function UNIQUAC(components::Vector{String}; puremodel=PR,
     model = UNIQUAC(components,icomponents,packagedparams,init_puremodel,1e-12,references)
     return model
 end
-
+#=
+function lnγ_comb(model::UNIQUACModel,p,T,z)
+    #Φ =  x.*r/sum(x[i]*r[i] for i ∈ @comps)
+    #θ = x.*q/sum(x[i]*q[i] for i ∈ @comps)
+    q = model.params.q.values
+    r = model.params.r.values
+    ∑z = sum(z)
+    Φ_mean = dot(z,q)/∑z
+    θ_mean = dot(z,r)/∑z
+    _0 = zero(p+T+first(z))
+    lnγ = fill(_0,length(model))
+    for i in @comps
+        zi = z[i]
+        Φᵢ = zi*q[i]/Φ_mean
+        θᵢ = zi*r[i]/θ_mean
+        lnγ[i] =  log(Φᵢ/zi)+(1-Φᵢ/zi)-5*q[i]*(log(Φᵢ/θᵢ)+(1-Φᵢ/θᵢ))
+    end
+    return lnγ
+end
+=#
 function activity_coefficient(model::UNIQUACModel,p,T,z)
     a = model.params.a.values
     q = model.params.q.values
@@ -48,8 +67,14 @@ function activity_coefficient(model::UNIQUACModel,p,T,z)
     Φ =  x.*r/sum(x[i]*r[i] for i ∈ @comps)
     θ = x.*q/sum(x[i]*q[i] for i ∈ @comps)
     θ_p = x.*q_p/sum(x[i]*q_p[i] for i ∈ @comps)
-    τ = @. exp(-a/T)
+    τ = @f(Ψ)
     lnγ_comb = @. log(Φ/x)+(1-Φ/x)-5*q*(log(Φ/θ)+(1-Φ/θ))
     lnγ_res  = q_p.*(1 .-log.(sum(θ_p[i]*τ[i,:] for i ∈ @comps)) .-sum(θ_p[i]*τ[:,i]/sum(θ_p[j]*τ[j,i] for j ∈ @comps) for i ∈ @comps))
     return exp.(lnγ_comb+lnγ_res)
+end
+
+function Ψ(model::UNIFACModel,V,T,z)
+    Tinv = 1/T
+    a = model.params.a.values
+    return @. exp(-a*Tinv)
 end
