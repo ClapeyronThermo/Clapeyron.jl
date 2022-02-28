a1ac5191b30800e7922ba26b07aa68b9fedda564
longemen3000 <longemen3000@gmail.com>
2022-02-28 17:54:00 -0300

    correct composition derivative - multicomponent version

--- src/utils/misc.jl
+++ src/utils/misc.jl
@@ -1,6 +1,5 @@
-function correct_composition_derivative(model,V,T)
-    z = SA[1.0]
-    μ = only(Clapeyron.VT_chemical_potential(model,V,T,z))
+function correct_composition_derivative(model,V,T,z=SA[1.0])
+    μ = dot(z,Clapeyron.VT_chemical_potential(model,V,T,z))
     g = VT_gibbs_free_energy(model,V,T,z)
     return abs(μ-g),μ,g
 end
