65fe231f0ee182a90a994fda537775206ebcf963
longemen3000 <longemen3000@gmail.com>
2022-02-28 22:55:29 -0300

    remove extra zero type init for some activities [skip actions]

--- src/models/Activity/UNIFAC/variants/VTPR.jl
+++ src/models/Activity/UNIFAC/variants/VTPR.jl
@@ -58,6 +58,7 @@ Xₖ = (∑xᵢνᵢₖ)/v̄ for i ∈ components
 3. Ahlers, J., & Gmehling, J. (2001). Development of an universal group contribution equation of state. Fluid Phase Equilibria, 191(1–2), 177–188. doi:10.1016/s0378-3812(01)00626-4
 """
 VTPRUNIFAC
+
 function VTPRUNIFAC(components; puremodel=PR,
     userlocations=String[], 
      verbose=false)
