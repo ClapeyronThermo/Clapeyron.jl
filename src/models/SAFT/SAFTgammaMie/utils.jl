5f4963e1bfe285fa995bc9b4c574c824af378840
longemen3000 <longemen3000@gmail.com>
2022-03-22 21:27:14 -0300

    fix SAFTgammaMie (this error was haunting me for days)

--- src/models/SAFT/SAFTgammaMie/utils.jl
+++ src/models/SAFT/SAFTgammaMie/utils.jl
@@ -112,13 +112,13 @@ function gc_to_comp_assoc_idx(param::AssocParam,sites::SiteParam,idxdict)
 end
 
 
-function mix_segment!(groups::GroupParam,s = ones(length(groups.flattenedgroups)))
+function mix_segment!(groups::GroupParam,s = ones(length(groups.flattenedgroups)),segment = s)
     gc = 1:length(groups.flattenedgroups)
     comps = 1:length(groups.components)
     v = groups.n_groups_cache
     for k in 1:length(gc)
         for i in 1:length(comps)
-            v[i][k] = v[i][k]*s[k]
+            v[i][k] = v[i][k]*s[k]*segment[k]
         end
     end
     #SingleParam("mixed segment",groups.flattenedgroups,mixsegment,[false for i ∈ gc],String[],String[])
