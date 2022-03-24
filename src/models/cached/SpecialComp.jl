1691893f4144a5ae4eeaea0a7bdca1075e5f9741
longemen3000 <longemen3000@gmail.com>
2022-03-24 03:40:27 -0300

    add special water (water08) handling

---
+++ src/models/cached/SpecialComp.jl
@@ -0,0 +1,29 @@
+"""
+    SpecialComp(components::Vector{String},defaults=["water"])
+
+Auxiliary model that just stores the index of one component. faster than looking for the component string on each iteration. 
+
+Used mainly for applying specific correlations in presence of a certain compound.
+"""
+struct SpecialComp <: EoSModel
+    components::Vector{String}
+    idx::Int
+    references::Vector{String}
+end
+
+function SpecialComp(components::Vector{String},defaults=["water"])
+    idx = findfirst(in(defaults),components)
+    idx === nothing && (idx = 0)
+    return SpecialComp(components,idx,defaults)
+end
+
+function split_model(model::SpecialComp,subset=nothing)
+    splitted = auto_split_model(model,subset)
+    for i in 1:length(splitted)
+        splitted[i] = SpecialComp(splitted[i].components,defaults)
+    end
+    return splitted
+end
+
+Base.getindex(model::SpecialComp) = model.idx
+
