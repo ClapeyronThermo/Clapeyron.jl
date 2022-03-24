3cbeee39f99894dd30a34c4700b952df0372afd0
longemen3000 <longemen3000@gmail.com>
2022-03-24 04:33:30 -0300

    fix sPCSAFT errors

--- src/models/SAFT/PCSAFT/variants/sPCSAFT.jl
+++ src/models/SAFT/PCSAFT/variants/sPCSAFT.jl
@@ -13,13 +13,14 @@ function sPCSAFT(components;
     
     segment = params["m"]
     k = params["k"]
+    k1 = get(params,"kT",PairParam("kT",components,zeros(n,n)))
     Mw = params["Mw"]
     params["sigma"].values .*= 1E-10
     sigma = sigma_LorentzBerthelot(params["sigma"])
-    epsilon = epsilon_LorentzBerthelot(params["epsilon"], k)
+    epsilon = epsilon_LorentzBerthelot(params["epsilon"])
     epsilon_assoc = params["epsilon_assoc"]
     bondvol = params["bondvol"]
-    packagedparams = PCSAFTParam(Mw, segment, sigma, epsilon, epsilon_assoc, bondvol)
+    packagedparams = PCSAFTParam(Mw, segment, sigma, epsilon,k,k1, epsilon_assoc, bondvol)
     references = ["10.1021/ie020753p"]
 
     model = sPCSAFT(packagedparams, sites, idealmodel; ideal_userlocations, references, verbose, assoc_options)
