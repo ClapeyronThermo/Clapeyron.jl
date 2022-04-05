d58d68742badefc07c7675f097e77e486c993059
longemen3000 <longemen3000@gmail.com>
2022-04-05 04:05:47 -0400

    start of EPPR78

---
+++ src/models/cubic/PR/variants/EPPR78.jl
@@ -0,0 +1,43 @@
+#=
+groups:
+CH3
+CH2
+CH
+C
+CH4
+C2H6
+CH aro
+C aro
+C fused aromatic rings
+CH2 cyclic
+CH cyclic~|~C cyclic
+CO2
+N2
+H2S
+SH
+H2O
+C2H4
+CH2 alkenic~|~CH alkenic
+C alkenic
+CH cycloalkenic~|~C cycloalkenic
+H2
+C2F6
+CF3
+CF2
+CF2 double bond~|~CF double bond
+C2H4F2
+C2H2F4
+CO
+He
+Ar
+SO2
+O2
+NO
+COS
+NH3
+NO2~|~N2O4
+N2O
+C2H2
+HC=-C-
+-C=-C-
+=#
