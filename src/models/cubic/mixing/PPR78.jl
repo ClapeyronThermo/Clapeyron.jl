b9daeddf230588cbee3da914902caa9551ed106a
longemen3000 <longemen3000@gmail.com>
2022-04-07 04:02:28 -0400

    fix EPPR78

--- src/models/cubic/mixing/PPR78.jl
+++ src/models/cubic/mixing/PPR78.jl
@@ -95,12 +95,55 @@ function mixing_rule(model::CubicModel,V,T,z,mixing_model::PPR78Rule,α,a,b,c)
                     end
                 end
             end
-            gᴱ += bi*b[j,j]*z[i]*z[j]*Eij
+            gᴱ += bi*b[j,j]*z[i]*z[j]*Eij #(0.5 * 2)
         end
     end
-    gᴱ = 0.5*gᴱ*invn2/b̄
+    gᴱ = gᴱ*invn2/b̄
     ∑ab = sum(z[i]*a[i,i]*α[i]/b[i,i] for i ∈ @comps)*invn
     ā = b̄*(∑ab-gᴱ)
     return ā,b̄,c̄
 end
 
+#=
+groups:
+["CH3",
+"CH2",
+"CH",
+"C",
+"CH4",
+"C2H6",
+"CH aro",
+"C aro",
+"C fused aromatic rings",
+"CH2 cyclic",
+"CH cyclic~|~C cyclic",
+"CO2",
+"N2",
+"H2S",
+"SH",
+"H2O",
+"C2H4",
+"CH2 alkenic~|~CH alkenic",
+"C alkenic",
+"CH cycloalkenic~|~C cycloalkenic",
+"H2",
+"C2F6",
+"CF3",
+"CF2",
+"CF2 double bond~|~CF double bond",
+"C2H4F2",
+"C2H2F4",
+"CO",
+"He",
+"Ar",
+"SO2",
+"O2",
+"NO",
+"COS",
+"NH3",
+"NO2~|~N2O4",
+"N2O",
+"C2H2",
+"HC=-C-",
+"-C=-C-"]
+=#
