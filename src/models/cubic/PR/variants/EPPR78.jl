302cbdc8412047ece249d6a5face15f34a04e4c9
longemen3000 <longemen3000@gmail.com>
2022-04-08 21:18:54 -0400

    testing ref

--- src/models/cubic/PR/variants/EPPR78.jl
+++ src/models/cubic/PR/variants/EPPR78.jl
@@ -45,7 +45,7 @@ HC=-C-
 """
     EPPR78(components::Vector{String}; idealmodel=BasicIdeal,
     alpha = PR78Alpha,
-    mixing = vdW1fRule,
+    mixing = PPR78Rule,
     activity = nothing,
     translation=NoTranslation,
     userlocations=String[], 
@@ -57,13 +57,14 @@ HC=-C-
 
 Enhanced Predictive Peng Robinson equation of state. it uses the following models:
 
-- Translation Model: `NoTranslation`
-- Alpha Model: `PR78Alpha`
-- Mixing Rule Model: `PPR78Rule`
+- Translation Model: [`NoTranslation`](@ref)
+- Alpha Model: [`PR78Alpha`](@ref)
+- Mixing Rule Model: [`PPR78Rule`](@ref)
 
 ## References
 
-1. Robinson DB, Peng DY. The characterization of the heptanes and heavier fractions for the GPA Peng-Robinson programs. Tulsa: Gas Processors Association; 1978
+1. Jaubert, J.-N., Privat, R., & Mutelet, F. (2010). Predicting the phase equilibria of synthetic petroleum fluids with the PPR78 approach. AIChE Journal. American Institute of Chemical Engineers, 56(12), 3225–3235. doi:10.1002/aic.12232
+2. Jaubert, J.-N., Qian, J.-W., Lasala, S., & Privat, R. (2022). The impressive impact of including enthalpy and heat capacity of mixing data when parameterising equations of state. Application to the development of the E-PPR78 (Enhanced-Predictive-Peng-Robinson-78) model. Fluid Phase Equilibria, (113456), 113456. doi:10.1016/j.fluid.2022.113456
 """
 function EPPR78(components::Vector{String}; idealmodel=BasicIdeal,
     alpha = PR78Alpha,
