1ac5bc1b3f46eacfd01dce02437c46cbf110e4f7
longemen3000 <longemen3000@gmail.com>
2022-04-14 01:33:21 -0400

    more work on SAFT docs, add Mw to remaining models.

--- docs/src/eos/saft.md
+++ docs/src/eos/saft.md
@@ -10,9 +10,16 @@ Pages = ["saft.md"]
 
 # SAFT Models
 
-```
-
-
-
-
+```@docs
+Clapeyron.ogSAFT
+Clapeyron.CKSAFT
+Clapeyron.sCKSAFT
+Clapeyron.PCSAFT
+Clapeyron.sPCSAFT
+Clapeyron.pharmaPCSAFT
+Clapeyron.softSAFT
+Clapeyron.softSAFT2016
+Clapeyron.SAFTVRSW
+Clapeyron.SAFTVRMie
+Clapeyron.SAFTgammaMie
 ```
--- src/models/SAFT/SAFTgammaMie/SAFTgammaMie.jl
+++ src/models/SAFT/SAFTgammaMie/SAFTgammaMie.jl
@@ -29,6 +29,49 @@ struct SAFTgammaMie{I,VR} <: SAFTgammaMieModel
     references::Array{String,1}
 end
 
+"""
+    SAFTVRSWModel <: SAFTModel
+
+    SAFTVRSW(components; 
+    idealmodel=BasicIdeal,
+    userlocations=String[],
+    ideal_userlocations=String[],
+    verbose=false,
+    assoc_options = AssocOptions())
+
+## Input parameters
+- `Mw`: Single Parameter (`Float64`) - Molecular Weight `[g/mol]`
+- `m`: Single Parameter (`Float64`) - Number of segments (no units)
+- `shapefactor`: Single Parameter (`Float64`) - Shape factor for segment (no units)
+- `sigma`: Single Parameter (`Float64`) - Segment Diameter [`A°`]
+- `epsilon`: Single Parameter (`Float64`) - Reduced dispersion energy  `[K]`
+- `lambda_a`: Pair Parameter (`Float64`) - Atractive range parameter (no units)
+- `lambda_r`: Pair Parameter (`Float64`) - Repulsive range parameter (no units)
+- `epsilon_assoc`: Association Parameter (`Float64`) - Reduced association energy `[K]`
+- `bondvol`: Association Parameter (`Float64`) - Association Volume
+
+## Model Parameters
+- `segment`: Single Parameter (`Float64`) - Number of segments (no units)
+- `shapefactor`: Single Parameter (`Float64`) - Shape factor for segment (no units)
+- `sigma`: Pair Parameter (`Float64`) - Mixed segment Diameter `[m]`
+- `lambda_a`: Pair Parameter (`Float64`) - Atractive range parameter (no units)
+- `lambda_r`: Pair Parameter (`Float64`) - Repulsive range parameter (no units)
+- `epsilon`: Pair Parameter (`Float64`) - Mixed reduced dispersion energy`[K]`
+- `epsilon_assoc`: Association Parameter (`Float64`) - Reduced association energy `[K]`
+- `bondvol`: Association Parameter (`Float64`) - Association Volume
+
+## Input models
+- `idealmodel`: Ideal Model
+
+## Description
+
+SAFT-γ-Mie potential
+
+## References
+1. Lafitte, T., Apostolakou, A., Avendaño, C., Galindo, A., Adjiman, C. S., Müller, E. A., & Jackson, G. (2013). Accurate statistical associating fluid theory for chain molecules formed from Mie segments. The Journal of Chemical Physics, 139(15), 154504. doi:10.1063/1.4819786
+2. Dufal, S., Lafitte, T., Haslam, A. J., Galindo, A., Clark, G. N. I., Vega, C., & Jackson, G. (2015). The A in SAFT: developing the contribution of association to the Helmholtz free energy within a Wertheim TPT1 treatment of generic Mie fluids. Molecular Physics, 113(9–10), 948–984. doi:10.1080/00268976.2015.1029027
+"""
+
 
 function SAFTgammaMie(components; 
     idealmodel=BasicIdeal,
