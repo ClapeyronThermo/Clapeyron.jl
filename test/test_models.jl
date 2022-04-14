8bf54ac92499166f15c6592096d65b6650d11b55
pw0908 <pierre.walker17@imperial.ac.uk>
2022-04-14 16:16:01 -0700

    Fix WS and tests

--- test/test_models.jl
+++ test/test_models.jl
@@ -185,7 +185,7 @@ end
 
         @testset "RK w/ WSRule" begin
             system = RK(["methanol","benzene"];mixing = WSRule, activity=Wilson)
-            @test Clapeyron.a_res(system, V, T, z) ≈ -0.5568144490135614 rtol = 1e-6
+            @test Clapeyron.a_res(system, V, T, z) ≈ -0.5729126903890258 rtol = 1e-6
         end
     end
 
@@ -269,7 +269,7 @@ end
 
         @testset "PR w/ WSRule" begin
             system = PR(["methanol","benzene"];mixing = WSRule, activity=Wilson)
-            @test Clapeyron.a_res(system, V, T, z) ≈ -0.6602069628893608 rtol = 1e-6
+            @test Clapeyron.a_res(system, V, T, z) ≈ -0.669085674824878 rtol = 1e-6
         end
     end
     @printline
