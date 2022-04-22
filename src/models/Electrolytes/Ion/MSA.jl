87121d3de4a19692272a95d390c34c0824632af2
longemen3000 <longemen3000@gmail.com>
2022-04-22 18:42:03 -0400

    make MSA a tad bit faster

--- src/models/Electrolytes/Ion/MSA.jl
+++ src/models/Electrolytes/Ion/MSA.jl
@@ -50,16 +50,14 @@ function a_ion(model::ElectrolyteModel, V, T, z,ion::MSAModel)
     σ = model.params.sigma.values
     Z = model.params.charge.values
     ϵ_r = RSP(model,V,T,z,model.RSPmodel)
-
-    x = z ./ sum(z)
+    ∑z = sum(z)
     ρ = N_A*sum(z)/V
-
     Γ = @f(screening_length)
-    Δ = 1-π*ρ/6*sum(x[i]*σ[i]^3 for i ∈ model.iions)
-    Ω = 1+π*ρ/(2*Δ)*sum(x[i]*σ[i]^3/(1+Γ*σ[i]) for i ∈ model.iions)
-    Pn = ρ/Ω*sum(x[i]*σ[i]*Z[i]/(1+Γ*σ[i]) for i ∈ model.iions)
+    Δ = 1-π*ρ/6*sum(z[i]*σ[i]^3 for i ∈ model.iions)/∑z
+    Ω = 1+π*ρ/(2*Δ)*sum(z[i]*σ[i]^3/(1+Γ*σ[i]) for i ∈ model.iions)/∑z
+    Pn = ρ/Ω*sum(z[i]*σ[i]*Z[i]/(1+Γ*σ[i]) for i ∈ model.iions)/∑z
 
-    U_MSA = -e_c^2*V/(4π*ϵ_0*ϵ_r)*(Γ*ρ*sum(x[i]*Z[i]^2/(1+Γ*σ[i]) for i ∈ model.iions)+π/(2Δ)*Ω*Pn^2)
+    U_MSA = -e_c^2*V/(4π*ϵ_0*ϵ_r)*(Γ*ρ*sum(z[i]*Z[i]^2/(1+Γ*σ[i]) for i ∈ model.iions)/∑z + π/(2Δ)*Ω*Pn^2)
     return (U_MSA+Γ^3*k_B*T*V/(3π))/(N_A*k_B*T*sum(z))
 end
 
@@ -67,23 +65,29 @@ function screening_length(model::MSAModel,V,T,z)
     σ = model.params.sigma.values
     Z = model.params.charge.values
     ϵ_r = RSP(model.RSPmodel,V,T,z)
-
-    x = z ./ sum(z)
+    ∑z = sum(z)
+    #x = z ./ sum(z)
     ρ = N_A*sum(z)/V
-    Δ = 1-π*ρ/6*sum(x[i]*σ[i]^3 for i ∈ model.iions)
+    Δ = 1-π*ρ/6*sum(z[i]*σ[i]^3 for i ∈ model.iions)/∑z
 
-    Γold = (4π*e_c^2/(4π*ϵ_0*ϵ_r*k_B*T)*ρ*sum(x[i]*Z[i]^2 for i ∈ model.iions))^(1/2)
-    Γnew = deepcopy(Γold)
-    tol  = 1.
-    i = 1.
-    while tol>1e-8 && i < 100
-        Ω = 1+π*ρ/(2*Δ)*sum(x[i]*σ[i]^3/(1+Γold*σ[i]) for i ∈ model.iions)
-        Pn = ρ/Ω*sum(x[i]*σ[i]*Z[i]/(1+Γold*σ[i]) for i ∈ model.iions)
-        Q = @. (Z-σ^2*Pn*(π/(2Δ)))./(1+Γold*σ)
-        Γnew = sqrt(π*e_c^2*ρ/(4π*ϵ_0*ϵ_r*k_B*T)*sum(x[i]*Q[i]^2 for i ∈ model.iions))
+    Γold = (4π*e_c^2/(4π*ϵ_0*ϵ_r*k_B*T)*ρ*sum(z[i]*Z[i]^2 for i ∈ model.iions)/∑z)^(1/2)
+    _0 = zero(Γold)
+    Γnew = _0
+    tol  = one(_0)
+    iter = 1
+    while tol>1e-8 && iter < 100
+        Ω = 1+π*ρ/(2*Δ)*sum(z[i]*σ[i]^3/(1+Γold*σ[i]) for i ∈ model.iions)/∑z
+        Pn = ρ/Ω*sum(z[i]*σ[i]*Z[i]/(1+Γold*σ[i]) for i ∈ model.iions)/∑z
+        #Q = @. (Z-σ^2*Pn*(π/(2Δ)))./(1+Γold*σ)
+        ∑Q2x = _0
+        for i in ∈ model.iions
+            Qi = (Z[i]-σ[i]^2*Pn*(π/(2Δ)))/(1+Γold*σ[i])
+            ∑Q2x += z[i]*Qi^2
+        end
+        Γnew = sqrt(π*e_c^2*ρ/(4π*ϵ_0*ϵ_r*k_B*T)*∑Q2x/∑z)
         tol = abs(1-Γnew/Γold)
-        Γold = deepcopy(Γnew)
-        i+=1
+        Γold = Γnew
+        iter += 1
     end
     return Γnew
 end
