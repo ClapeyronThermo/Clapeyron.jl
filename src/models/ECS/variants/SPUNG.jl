8803a28b50568740d464f1f3996cc0d3c05c55b2
longemen3000 <longemen3000@gmail.com>
2022-06-13 18:43:50 -0400

    typo

--- src/models/ECS/variants/SPUNG.jl
+++ src/models/ECS/variants/SPUNG.jl
@@ -17,8 +17,6 @@ SPUNG: State Research Program for Utilization of Natural Gas
 
 1. Wilhelmsen, Ø., Skaugen, G., Jørstad, O., & Li, H. (2012). Evaluation of SPUNG* and other equations of state for use in carbon capture and storage modelling. Energy Procedia, 23, 236–245. doi:10.1016/j.egypro.2012.06.024
 """
-const SPUNG = ECS{<:SRK,PropaneRef}
-
 function SPUNG(components::Vector{String},
     refmodel=PropaneRef(),
     shapemodel=SRK(components),
