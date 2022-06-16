f29014781382f7fb88e9db5930d48ac0e3c875bc
pw0908 <pierre.walker17@imperial.ac.uk>
2022-06-15 23:15:11 -0700

    Convert bubble, az, LLE and VLLE to new solver

--- test/test_methods.jl
+++ test/test_methods.jl
@@ -191,7 +191,7 @@ end
         GC.gc()
         @test Clapeyron.VLLE_pressure(system, T)[1] ≈ 54504.079665621306 rtol = 1E-6
         GC.gc()
-        @test Clapeyron.VLLE_temperature(system, p)[1] ≈ 328.2478837563423 rtol = 1E-6
+        @test Clapeyron.VLLE_temperature(system, 54504.079665621306)[1] ≈ 313.1499860368554 rtol = 1E-6
         GC.gc()
         @test Clapeyron.crit_mix(system,z)[1] ≈ 518.0004062881115 rtol = 1E-6
     end
