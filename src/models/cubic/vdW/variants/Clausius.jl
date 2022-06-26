dc09457f025cea09ae4dee43b7a5ba94793debbe
longemen3000 <longemen3000@gmail.com>
2022-06-26 01:28:48 -0400

    typo in Clausius

--- src/models/cubic/vdW/variants/Clausius.jl
+++ src/models/cubic/vdW/variants/Clausius.jl
@@ -121,8 +121,8 @@ function c_premixing(model::Type{<:ClausiusModel},mixing::MixingRule,Tc,pc,vc,ki
 end
 
 function cubic_Δ(model::ClausiusModel,z) 
-    b = model.params.b.values
-    c = model.params.c.values
+    b = model.params.b.diagvalues
+    c = model.params.c.diagvalues
     z⁻¹ = sum(z)^-1
     b̄ = dot(b,z)*z⁻¹
     c̄ = dot(c,z)*z⁻¹
