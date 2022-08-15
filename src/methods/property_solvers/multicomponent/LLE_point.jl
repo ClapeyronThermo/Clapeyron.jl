af12fd612543daf827ee6af2aa15680d8fc2f48d
pw0908 <pw1017@ic.ac.uk>
2022-08-15 17:41:42 +0200

    Add LLE function for binary activity models

--- src/methods/property_solvers/multicomponent/LLE_point.jl
+++ src/methods/property_solvers/multicomponent/LLE_point.jl
@@ -114,3 +114,29 @@ function presents_LLE(model,p,T)
     res = Roots.solve(prob)
     return (res,mixing_gibbs(res))
 end
+
+function LLE(model::ActivityModel,T;v0=nothing)
+    if v0 === nothing
+        v0 = [0.25,0.75]
+    end
+    
+    len = length(v0)
+
+    Fcache = zeros(eltype(v0),len)
+    f!(F,z) = Obj_LLE(model, F, T, z[1], z[2])
+    r  = Solvers.nlsolve(f!,v0,LineSearch(Newton()))
+    sol = Solvers.x_sol(r)
+    x = sol[1]
+    xx = sol[2]
+    return x,xx
+end
+
+function Obj_LLE(model::ActivityModel, F, T, x, xx)
+    x = Fractions.FractionVector(x)
+    xx = Fractions.FractionVector(xx)
+    γₐ = activity_coefficient(model,1e-3,T,x)
+    γᵦ = activity_coefficient(model,1e-3,T,xx)
+
+    F .= γᵦ.*xx-γₐ.*x
+    return F
+end
