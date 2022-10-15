0d38f8016b110a77fab62f613696948d9c1df38a
longemen3000 <longemen3000@gmail.com>
2022-10-15 02:14:10 -0300

    reinstate tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,10 +13,10 @@ end
 
 @testset "All tests" begin
     include("test_database.jl")
-    #include("test_solvers.jl")
-    #include("test_differentials.jl")
-    #include("test_misc.jl")
-    #include("test_models.jl")
-    #include("test_methods_eos.jl")
-    #include("test_methods_api.jl")
+    include("test_solvers.jl")
+    include("test_differentials.jl")
+    include("test_misc.jl")
+    include("test_models.jl")
+    include("test_methods_eos.jl")
+    include("test_methods_api.jl")
 end
