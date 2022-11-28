ec2652067fcf1791b0d57188f0c67fe5b86c619a
pw0908 <pw1017@ic.ac.uk>
2022-11-28 12:19:51 -0800

    Example with all heat capacity and multi-IL

--- IL/test_IL.jl
+++ IL/test_IL.jl
@@ -1,17 +1,22 @@
 using Clapeyron
 
-model = SAFTgammaEMie([],[("BMIMBF4",["BMIM"=>1,"BF4"=>1])],
-                         [("BMIM",["CH3"=>2,"CH2"=>3,"IM"=>1]),("BF4",["BF4"=>1])];
-                         userlocations=["IL_like.csv","IL_assoc.csv"],
-                         rsp_userlocations=["Salt_like.csv"])
-
+model = SAFTgammaEMie([], # Solvent
+                      [("BMIMBF4",["BF4"=>1,"BMIM"=>1]),
+                       ("EMIMBF4",["BF4"=>1,"EMIM"=>1])], 
+                      [("BMIM",["CH3"=>2,"CH2"=>3,"IM"=>1]),
+                       ("EMIM",["CH3"=>2,"CH2"=>1,"IM"=>1]),
+                       ("BF4",["BF4"=>1])];
+                       idealmodel=JobackIdeal,
+                      userlocations=["IL_like.csv","IL_assoc.csv"], # Parameters for the groups
+                      ideal_userlocations=["Ideal.csv"],
+                      rsp_userlocations=["Salt_like.csv"]) # Parameters for the dielectric constant
 toestimate = [
     Dict(
         :param => :epsilon,
         :indices => (3,3),
         :recombine => true,
         :lower => 100.,
-        :upper => 300.,
+        :upper => 800.,
         :guess => 200.
     ),
     Dict(
@@ -49,26 +54,32 @@ toestimate = [
     ),
     Dict(
         :param => :epsilon_assoc,
-        :lower => 1000.,
+        :lower => 700.,
         :upper => 3000.,
         :guess => 2500.
     )
 ]
 
-function density(model::EoSModel,p,T)
-    return mass_density(model,p,T,[1.,1.])
+function density(model::EoSModel,p,T,id)
+    z = ones(length(model.ions)).*1e-30
+    z[1] = 1
+    z[Int(id)+1] = 1
+    return mass_density(model,p,T,z)
 end
 
-function heat_capacity(model::EoSModel,p,T)
-    return isobaric_heat_capacity(model,p,T,[1.,1.])
+function heat_capacity(model::EoSModel,p,T,id)
+    z = ones(length(model.ions)).*1e-30
+    z[1] = 1
+    z[Int(id)+1] = 1
+    return isobaric_heat_capacity(model,p,T,z)
 end
 
-e = Estimation(model,toestimate,["density_EMIMBF4.csv","heat_capacity_EMIMBF4.csv"],[:vrmodel])
+e = Estimation(model,toestimate,["density.csv"],[:vrmodel])
 
 optimize!(e,Clapeyron.Metaheuristics.ECA(options = Clapeyron.Metaheuristics.Options(iterations=100)))
 
 model = e.model
 
-pred = density.(model,e.data[1].inputs[1],e.data[1].inputs[2])
+pred = density.(model,e.data[1].inputs[1],e.data[1].inputs[2],e.data[1].inputs[3])
 
 println(pred)
