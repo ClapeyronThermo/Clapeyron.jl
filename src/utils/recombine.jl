f85044b0039437eba62dd9920f450076889fe417
longemen3000 <longemen3000@gmail.com>
2022-12-06 21:58:41 -0300

    add non_splittable check for recombine!

--- src/utils/recombine.jl
+++ src/utils/recombine.jl
@@ -1,4 +1,9 @@
 function recombine!(model::EoSModel)
+    #normally non-splittable models doesn't have component-dependent parameters, they are "final" in a way.
+    if !is_splittable(model)
+        return model
+    end
+    
     if has_sites(model)
         recombine!(model.sites)
     end
