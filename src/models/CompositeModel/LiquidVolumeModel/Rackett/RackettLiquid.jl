09f0531a50187935d77262b3c3eaeb31208b0985
longemen3000 <longemen3000@gmail.com>
2023-03-24 23:23:18 -0300

    some docs for saturated liquids

--- src/models/CompositeModel/LiquidVolumeModel/Rackett/RackettLiquid.jl
+++ src/models/CompositeModel/LiquidVolumeModel/Rackett/RackettLiquid.jl
@@ -9,16 +9,43 @@ end
 
 @newmodelsimple RackettLiquid RackettLiquidModel RackettLiquidParam
 
+"""
+    RackettLiquid(components::Vector{String};
+    userlocations::Vector{String}=String[], 
+    verbose::Bool=false)
+
+## Input parameters
+
+- `Tc`: Single Parameter (Float64) - Critical Temperature [K]
+- `Pc`: Single Parameter (Float64) - Critical Pressure [Pa]
+- `Vc`: Single Parameter (`Float64`) - Critical Volume `[m³/mol]`
+
+## Model Parameters
+
+- `Tc`: Single Parameter (Float64) - Critical Temperature [K]
+- `Pc`: Single Parameter (Float64) - Critical Pressure [Pa]
+- `Zc`: Single Parameter (Float64) - Critical Compressibility Factor
+
+## Description
+
+Rackett Equation of State for saturated liquids. it is independent of the pressure.
+```
+Tr = T/Tc
+V = (R̄Tc/Pc)Zc^(1+(1-Tr)^(2/7))
+```
+## References
+- Rackett, H. G. (1970). Equation of state for saturated liquids. Journal of Chemical and Engineering Data, 15(4), 514–517. [doi:10.1021/je60047a012](https://doi.org/10.1021/je60047a012)
+"""
 function RackettLiquid(components::Vector{String}; userlocations::Vector{String}=String[], verbose::Bool=false)
     params = getparams(components, ["properties/critical.csv"]; userlocations=userlocations, verbose=verbose)
-    acentricfactor = params["acentricfactor"]
     Tc = params["Tc"]
     Pc = params["Pc"]
     vc = params["Vc"]
     _zc = Pc.values .* vc.values ./ (R̄ .* Tc.values)
     Zc = SingleParam("Critical Compressibility factor",components,_zc) 
     packagedparams = RackettLiquidParam(Tc,Pc,Zc)
-    model = RackettLiquid(packagedparams)
+    references = ["10.1021/je60047a012"]
+    model = RackettLiquid(packagedparams;references,verbose)
     return model
 end
 
@@ -72,6 +99,36 @@ function volume_impl(model::RackettLiquidModel,p,T,z::SingleComp,phase=:unknown,
 end
 
 
+"""
+    YamadaGunnLiquid(components::Vector{String}; 
+                userlocations::Vector{String}=String[], 
+                verbose::Bool=false)
+
+## Input parameters
+
+- `Tc`: Single Parameter (Float64) - Critical Temperature `[K]`
+- `Pc`: Single Parameter (Float64) - Critical Pressure `[Pa]`
+- `Vc`: Single Parameter (`Float64`) - Critical Volume `[m³/mol]`
+- `acentricfactor`: Single Parameter (`Float64`) - Acentric Factor
+
+## Model Parameters
+
+- `Tc`: Single Parameter (Float64) - Critical Temperature `[K]`
+- `Pc`: Single Parameter (Float64) - Critical Pressure `[Pa]`
+- `Zc`: Single Parameter (Float64) - Critical Compressibility Factor
+
+## Description
+
+The Yamada-Gunn equation of state is a modification of the Rackett equation of state that uses a different approach to calculate the compressibility factor `Zc`:
+```
+Tr = T/Tc
+Zc = 0.29056 - 0.08775ω
+V = (R̄Tc/Pc)Zc^(1+(1-Tr)^(2/7))
+```
+## References
+- Rackett, H. G. (1970). Equation of state for saturated liquids. Journal of Chemical and Engineering Data, 15(4), 514–517. [doi:10.1021/je60047a012](https://doi.org/10.1021/je60047a012)
+- Gunn, R. D., & Yamada, T. (1971). A corresponding states correlation of saturated liquid volumes. AIChE Journal. American Institute of Chemical Engineers, 17(6), 1341–1345. [doi:10.1002/aic.690170613](https://doi.org/10.1002/aic.690170613)
+"""
 function YamadaGunnLiquid(components::Vector{String}; userlocations::Vector{String}=String[], verbose::Bool=false)
     params = getparams(components, ["properties/critical.csv"]; userlocations=userlocations, verbose=verbose)
     acentricfactor = params["acentricfactor"]
@@ -80,7 +137,8 @@ function YamadaGunnLiquid(components::Vector{String}; userlocations::Vector{Stri
     _zc = 0.29056 .- 0.08775 .* acentricfactor.values
     Zc = SingleParam("Critical Compressibility factor",components,_zc) 
     packagedparams = RackettLiquidParam(Tc,Pc,Zc)
-    model = RackettLiquid(packagedparams)
+    references =["10.1021/je60047a012","10.1002/aic.690170613"]
+    model = RackettLiquid(packagedparams;references,verbose)
     return model
 end
     
