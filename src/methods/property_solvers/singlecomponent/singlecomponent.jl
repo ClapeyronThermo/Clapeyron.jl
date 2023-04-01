96f88cd0b9cca9819fb85d01436476fe15f19f43
longemen3000 <longemen3000@gmail.com>
2023-04-01 03:38:31 -0300

    typo in export

--- src/methods/property_solvers/singlecomponent/singlecomponent.jl
+++ src/methods/property_solvers/singlecomponent/singlecomponent.jl
@@ -3,5 +3,5 @@
 include("saturation/saturation.jl")
 include("crit_pure.jl")
 
-export saturation_pressure, saturation_liquid_density, saturation_temperature,
+export saturation_pressure, saturation_liquid_density, saturation_temperature
 export crit_pure, enthalpy_vap, acentric_factor
