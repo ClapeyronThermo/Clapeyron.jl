bf4c85975623a84111f804f56c04f0134a057f09
longemen3000 <longemen3000@gmail.com>
2023-04-25 03:12:14 -0400

    non-allocating a_polar for PPCSAFT

--- src/models/SAFT/PCSAFT/variants/PPCSAFT.jl
+++ src/models/SAFT/PCSAFT/variants/PPCSAFT.jl
@@ -4,6 +4,7 @@ struct PPCSAFTParam <: EoSParam
     sigma::PairParam{Float64}
     epsilon::PairParam{Float64}
     dipole::SingleParam{Float64}
+    dipole2::SingleParam{Float64}
     epsilon_assoc::AssocParam{Float64}
     bondvol::AssocParam{Float64}
 end
@@ -13,7 +14,7 @@ abstract type PPCSAFTModel <: PCSAFTModel end
 
 """
     PPCSAFTModel <: SAFTModel
-    PPCSAFT(components; 
+    PPCSAFT(components;
     idealmodel=BasicIdeal,
     userlocations=String[],
     ideal_userlocations=String[],
@@ -28,6 +29,7 @@ abstract type PPCSAFTModel <: PCSAFTModel end
 - `dipole`: Single Parameter (`Float64`) - Dipole moment `[D]`
 - `epsilon_assoc`: Association Parameter (`Float64`) - Reduced association energy `[K]`
 - `bondvol`: Association Parameter (`Float64`) - Association Volume `[m^3]`
+
 ## Model Parameters
 - `Mw`: Single Parameter (`Float64`) - Molecular Weight `[g/mol]`
 - `segment`: Single Parameter (`Float64`) - Number of segments (no units)
@@ -38,8 +40,10 @@ abstract type PPCSAFTModel <: PCSAFTModel end
 - `bondvol`: Association Parameter (`Float64`) - Association Volume
 ## Input models
 - `idealmodel`: Ideal Model
+
 ## Description
 Polar Perturbed-Chain SAFT (PPC-SAFT)
+
 ## References
 1. Gross, J., & Vrabec, J. (2005). An equation-of-state contribution for polar components: Dipolar molecules. AIChE Journal, 52(3), 856-1282. [doi:10.1002/aic.10683](https://doi.org/10.1002/aic.10683)
 """
@@ -52,6 +56,7 @@ function PPCSAFT(components;
     ideal_userlocations=String[],
     verbose=false,
     assoc_options = AssocOptions())
+
     params,sites = getparams(components, ["SAFT/PCSAFT/PPCSAFT/","properties/molarmass.csv"]; userlocations=userlocations, verbose=verbose)
     segment = params["segment"]
     k = get(params,"k",nothing)
@@ -61,18 +66,26 @@ function PPCSAFT(components;
     epsilon = epsilon_LorentzBerthelot(params["epsilon"], k)
     epsilon_assoc = params["epsilon_assoc"]
     bondvol = params["bondvol"]
+
     bondvol,epsilon_assoc = assoc_mix(bondvol,epsilon_assoc,sigma,assoc_options) #combining rules for association
 
     dipole = params["dipole"]
-
-    packagedparams = PPCSAFTParam(Mw,segment,sigma,epsilon,dipole,epsilon_assoc,bondvol)
+    μ,m = dipole,segment
+    dipole2 = SingleParam("Dipole squared",components, μ.^2 ./ m ./ k_B*1e-36*(1e-10*1e-3))
+    packagedparams = PPCSAFTParam(Mw,segment,sigma,epsilon,dipole,dipole2,epsilon_assoc,bondvol)
     references = ["10.1021/ie0003887", "10.1021/ie010954d"]
 
     model = PPCSAFT(packagedparams, sites, idealmodel; ideal_userlocations, references, verbose, assoc_options)
     return model
 end
 
-recombine_impl!(model ::PPCSAFTModel) = recombine_saft!(model)
+
+#
+function recombine_impl!(model ::PPCSAFTModel)
+    μ,m = model.params.dipole,model.params.segment
+    model.params.dipole2 .= μ.^2 ./ m ./ k_B * 1e-36*(1e-10*1e-3)
+    recombine_saft!(model)
+end
 
 function a_res(model ::PPCSAFTModel, V, T, z)
     _data = @f(data)
@@ -80,131 +93,129 @@ function a_res(model ::PPCSAFTModel, V, T, z)
 end
 
 function a_polar(model ::PPCSAFTModel, V, T, z, _data=@f(data))
-    a₂ = @f(a_2,_data)
-    a₃ = @f(a_3,_data)
-    if a₂ == 0.0
-        return 0.0
-    else
-        return a₂^2/(a₂-a₃)
+    dipole = model.params.dipole.values
+    if all(iszero,dipole)
+        return zero(V+T+first(z))
     end
+    a₂ = @f(a_2,_data)
+    iszero(a₂) && return zero(a₂)
+    a₃ = @f(a_3,_data)
+    return a₂^2/(a₂-a₃)
 end
 
 function a_2(model ::PPCSAFTModel, V, T, z, _data=@f(data))
+    _,_,_,_,η,_ = _data
     ∑z = sum(z)
     ρ = N_A*∑z/V
-    x = z./∑z
     _a_2 = zero(T+V+first(z))
+    nc = length(model)
     m = model.params.segment.values
     ϵ = model.params.epsilon.values
     σ = model.params.sigma.values
-    μ = model.params.dipole.values
-    μ̄² = μ.^2 ./m./k_B*1e-36*(1e-10*1e-3)
-    @inbounds for i ∈ @comps
-        _J2_ii = @f(J2,i,i,_data)
-        _a_2 += x[i]^2*μ̄²[i]^2/σ[i,i]^3/T^2*_J2_ii
-        for j ∈ i+1:length(model)
-            _J2_ij = @f(J2,i,j,_data)
-            _a_2 += 2*x[i]*x[j]*μ̄²[i]*μ̄²[j]/σ[i,j]^3/T^2*_J2_ij
+    μ̄² = model.params.dipole2.values
+    @inbounds for i ∈ 1:nc
+        _J2_ii = @f(J2,i,i,η,m,ϵ)
+        zᵢ = z[i]
+        μ̄²ᵢ = μ̄²[i]
+        _a_2 +=zᵢ^2*μ̄²ᵢ^2/σ[i,i]^3*_J2_ii
+        for j ∈ (i+1):nc
+            _J2_ij = @f(J2,i,j,η,m,ϵ)
+            _a_2 += 2*zᵢ*z[j]*μ̄²ᵢ*μ̄²[j]/σ[i,j]^3*_J2_ij
         end
     end
-    _a_2 *= -π*ρ
+    _a_2 *= -π*ρ/(T*T)/(∑z*∑z)
     return _a_2
 end
 
 function a_3(model ::PPCSAFTModel, V, T, z, _data=@f(data))
     ∑z = sum(z)
     ρ = N_A*∑z/V
-    x = z./∑z
+    _,_,_,_,η,_ = _data
     _a_3 = zero(T+V+first(z))
     m = model.params.segment.values
     ϵ = model.params.epsilon.values
     σ = model.params.sigma.values
-    μ = model.params.dipole.values
-    μ̄² = μ.^2 ./m./k_B*1e-36*(1e-10*1e-3)
-    @inbounds for i ∈ @comps
-        _J3_iii = @f(J3,i,i,i,_data)
-        _a_3 += x[i]^3*μ̄²[i]^3/σ[i,i]^3/T^3*_J3_iii
-        for j ∈ i+1:length(model)
-            _J3_iij = @f(J3,i,i,j,_data)
-            _a_3 += 3*x[i]^2*x[j]*μ̄²[i]^2*μ̄²[j]/σ[i,j]^2/σ[i,i]/T^3*_J3_iij
-            _J3_ijj = @f(J3,i,j,j,_data)
-            _a_3 += 3*x[i]*x[j]^2*μ̄²[i]*μ̄²[j]^2/σ[i,j]^2/σ[j,j]/T^3*_J3_ijj
-            for k in j+1:length(model)
-                _J3_ijk = @f(J3,i,j,k,_data)
-                _a_3 += 6*x[i]*x[j]*x[k]*μ̄²[i]*μ̄²[j]*μ̄²[k]/σ[i,j]/σ[i,k]/σ[j,k]/T^3*_J3_ijk
+    μ̄² = model.params.dipole2.values
+    nc = length(model)
+
+    @inbounds for i ∈ 1:nc
+        _J3_iii = @f(J3,i,i,i,η,m)
+        zi,μ̄²i = z[i],μ̄²[i]
+        a_3_i = zi*μ̄²i/σ[i,i]
+        _a_3 += a_3_i^3*_J3_iii
+        for j ∈ i+1:nc
+            zj,μ̄²j = z[j],μ̄²[j]
+            σij⁻¹ = 1/σ[i,j]
+            a_3_iij = zi*μ̄²i*σij⁻¹
+            a_3_ijj = zj*μ̄²j*σij⁻¹
+            a_3_j = zj*μ̄²j/σ[j,j]
+            _J3_iij = @f(J3,i,i,j,η,m)
+            _J3_ijj = @f(J3,i,j,j,η,m)
+            _a_3 += 3*a_3_iij*a_3_ijj*(a_3_i*_J3_iij + a_3_j*_J3_ijj)
+            for k ∈ j+1:nc
+                zk,μ̄²k = z[k],μ̄²[k]
+                _J3_ijk = @f(J3,i,j,k,η,m)
+                _a_3 += 6*zi*zj*zk*μ̄²i*μ̄²j*μ̄²k*σij⁻¹/σ[i,k]/σ[j,k]*_J3_ijk
             end
         end
     end
-    _a_3 *= -4π^2/3*ρ^2
+    _a_3 *= -4π^2/3*ρ^2/(T*T*T)/(∑z*∑z*∑z)
     return _a_3
 end
 
-function J2(model, V, T, z, i, j, _data=@f(data))
-    _,_,_,_,η,_ = _data
-
+function J2(model, V, T, z, i, j, η = @f(ζ,3),m = model.params.segment.values,ϵ = model.params.epsilon.values)
     corr_a = PPCSAFTconsts.corr_a
     corr_b = PPCSAFTconsts.corr_b
-
-    m = model.params.segment.values
-    m̄ = (m[i]*m[j])^(1/2)
-    if m̄>2
-        m̄=2
-    end
-    ϵ = model.params.epsilon.values
-    res = zero(T+V+first(z))
-    @inbounds for k ∈ 1:5
-        ii = k-1 
-        a1,a2,a3 = corr_a[k]
-        aij = a1 + (m̄-1)/m̄*a2 + (m̄-1)/m̄*(m̄-2)/m̄*a3
-
-        b1,b2,b3 = corr_b[k]
-        bij = b1 + (m̄-1)/m̄*b2 + (m̄-1)/m̄*(m̄-2)/m̄*b3
-
-        res +=(aij+bij*ϵ[i,j]/T)*η^ii
-    end
-    return res
+    m̄ = min(sqrt(m[i]*m[j]),2*one(η))
+    ϵT = ϵ[i,j]/T
+    aij = _ppcsaft_corr_poly(corr_a,m̄)
+    bij = _ppcsaft_corr_poly(corr_b,m̄)
+    cij = aij .+ bij .* ϵT
+    return evalpoly(η,cij)
 end
 
-function J3(model, V, T, z, i, j, k, _data=@f(data))
-    _,_,_,_,η,_ = _data
-
+function J3(model, V, T, z, i, j, k, η = @f(ζ,3),m = model.params.segment.values)
     corr_c = PPCSAFTconsts.corr_c
+    m̄ = min(cbrt(m[i]*m[j]*m[k]),2*one(η))
+    cijk = _ppcsaft_corr_poly(corr_c,m̄)
+    return evalpoly(η,cijk)
+end
 
-    m = model.params.segment.values
-    m̄ = (m[i]*m[j]*m[k])^(1/3)
-    if m̄>2
-        m̄=2
-    end
-    res = zero(η)
-    @inbounds for k ∈ 1:5
-        ii = k-1 
-        c1,c2,c3 = corr_c[k]
-        cijk = c1 + (m̄-1)/m̄*c2 + (m̄-1)/m̄*(m̄-2)/m̄*c3
+function _ppcsaft_corr_poly(corr,m̄)
+    m̄1 = 1.
+    m̄2 = (m̄-1)/m̄
+    m̄3 = m̄2*(m̄-2)/m̄
+    m̄i = (m̄1,m̄2,m̄3)
+    return NTuple{5}(dot(m̄i,ci) for ci in corr)
+end
 
-        res +=cijk*η^ii
-    end
-    return res
+#Optimizations for Single Component PPCSAFT
+
+function d(model::PPCSAFT, V, T, z::SingleComp)
+    ϵ = only(model.params.epsilon.values)
+    σ = only(model.params.sigma.values)
+    return SA[σ*(1 - 0.12*exp(-3ϵ/T))]
 end
 
 const PPCSAFTconsts = (
     corr_a =
-    [(0.3043504,0.9534641,-1.161008),
+    ((0.3043504,0.9534641,-1.161008),
     (-0.1358588,-1.8396383,4.5258607),
     (1.4493329,2.013118,0.9751222),
     (0.3556977,-7.3724958,-12.281038),
-    (-2.0653308,8.2374135,5.9397575)],
+    (-2.0653308,8.2374135,5.9397575)),
 
     corr_b =
-    [(0.2187939,-0.5873164,3.4869576),
+    ((0.2187939,-0.5873164,3.4869576),
     (-1.1896431,1.2489132,-14.915974),
     (1.1626889,-0.508528,15.372022),
-    (0,0,0),
-    (0,0,0)],
+    (0.,0.,0.),
+    (0.,0.,0.)),
 
     corr_c =
-    [(-0.0646774,-0.9520876,-0.6260979),
+    ((-0.0646774,-0.9520876,-0.6260979),
     (0.1975882,2.9924258,1.2924686),
     (-0.8087562,-2.3802636,1.6542783),
     (0.6902849,-0.2701261,-3.4396744),
-    (0,0,0)]
+    (0.,0.,0.))
 )
