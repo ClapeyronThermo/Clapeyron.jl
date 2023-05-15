93cbd1f3a8c486542aa5383b4b0bd572440ff876
longemen3000 <longemen3000@gmail.com>
2023-05-15 03:28:18 -0400

    remove precompile statements for now

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,5 +1,4 @@
-using PrecompileTools
-
+#=
 @setup_workload begin
 
     single = ["water"]
@@ -11,17 +10,19 @@ using PrecompileTools
         s4 = SAFTgammaMie(single)
         p1 = PCSAFT(pair)
         p2 = UNIFAC(pair)
+        split_model(p1)
+        split_model(p2)
         for si in (s1,s2,s3,s4)
             saturation_pressure(si,373.15)
-            crit_pure(si)
+            #crit_pure(si)
             pressure(si,0.03,373.15)
             volume(si,100000.0,373.15)
         end
         activity_coefficient(p2,100000,320,[0.5,0.5])
-        bubble_pressure(p1,320.0,[0.5,0.5])
-        bubble_pressure(p2,320.0,[0.5,0.5])
-        bubble_pressure(p1,320.0,Clapeyron.FractionVector(0.5))
-        bubble_pressure(p2,320.0,Clapeyron.FractionVector(0.5))
+        #bubble_pressure(p1,320.0,[0.5,0.5])
+        #bubble_pressure(p2,320.0,[0.5,0.5])
+        #bubble_pressure(p1,320.0,Clapeyron.FractionVector(0.5))
+        #bubble_pressure(p2,320.0,Clapeyron.FractionVector(0.5))
     end
 
-end
+end =#
