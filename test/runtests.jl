25d4d8090137d9cd6dd5f82df52e60b132af91a9
longemen3000 <longemen3000@gmail.com>
2023-06-18 01:06:35 -0400

    use GERG2008 with legacy R in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,6 +11,19 @@ macro printline()  # useful in hunting for where tests get stuck
     printstyled("  ", file, ":", __source__.line, "\n", color=:light_black)
 end
 
+#fix to current tests
+function GERG2008(components::Vector{String};verbose = false)
+    return MultiFluid(components;
+    mixing = AsymmetricMixing,
+    departure = EmpiricDeparture,
+    pure_userlocations = String["@REMOVEDEFAULTS","@DB/Empiric/GERG2008/pures"],
+    mixing_userlocations  = String["@REMOVEDEFAULTS","@DB/Empiric/GERG2008/mixing/GERG2008_mixing_unlike.csv"],
+    departure_userlocations = String["@REMOVEDEFAULTS","@DB/Empiric/GERG2008/departure/GERG2008_departure_unlike.csv"],
+    coolprop_userlocations = false,
+    verbose = verbose,
+    Rgas = Clapeyron.R̄)
+end
+
 @testset "All tests" begin
     include("test_database.jl")
     include("test_solvers.jl")
