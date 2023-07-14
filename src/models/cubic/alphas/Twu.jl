04908c61f1df11630b2963b13f2f808b4e107b83
longemen3000 <longemen3000@gmail.com>
2023-07-14 03:48:36 -0400

    define Twu88 and Twu91 alphas

--- src/models/cubic/alphas/Twu.jl
+++ src/models/cubic/alphas/Twu.jl
@@ -23,7 +23,7 @@ end
 - `N`: Single Parameter
 - `L`: Single Parameter
 ## Description
-Cubic alpha `(α(T))` model. Default for [`VTPR`](@ref) EoS.
+Cubic alpha `(α(T))` model. Default for [`VTPR`](@ref) EoS. Also known as Twu-91 alpha
 ```
 αᵢ = Trᵢ^(N*(M-1))*exp(L*(1-Trᵢ^(N*M))
 Trᵢ = T/Tcᵢ
@@ -33,12 +33,56 @@ Trᵢ = T/Tcᵢ
 """
 TwuAlpha
 
-export TwuAlpha
 function TwuAlpha(components::Vector{String}; userlocations=String[], verbose::Bool=false)
     params = getparams(components, ["alpha/Twu/Twu_like.csv"]; userlocations=userlocations, verbose=verbose)
     M = params["M"]
     N = params["N"]
     L = params["L"]
+    packagedparams = TwuAlphaParam(M,N,L)
+    model = TwuAlpha(packagedparams, verbose=verbose)
+    return model
+end
+
+const Twu91Alpha = TwuAlpha
+
+"""
+    Twu88Alpha::TwuAlpha
+    
+    Twu88Alpha(components::Vector{String};
+    userlocations=String[],
+    verbose::Bool=false)
+## Input Parameters
+- `M`: Single Parameter
+- `N`: Single Parameter (optional)
+- `L`: Single Parameter 
+## Model Parameters
+- `M`: Single Parameter
+- `N`: Single Parameter
+- `L`: Single Parameter
+## Description
+Cubic alpha `(α(T))` model. Also known as Twu-88 alpha.
+```
+αᵢ = Trᵢ^(N*(M-1))*exp(L*(1-Trᵢ^(N*M))
+N = 2
+Trᵢ = T/Tcᵢ
+```
+if `N` is specified, it will be used.
+
+## References
+1. Twu, C. H., Lee, L. L., & Starling, K. E. (1980). Improved analytical representation of argon thermodynamic behavior. Fluid Phase Equilibria, 4(1–2), 35–44. [doi:10.1016/0378-3812(80)80003-3](https://doi.org/10.1016/0378-3812(80)80003-3)
+"""
+function Twu88Alpha(components::Vector{String}; userlocations=String[], verbose::Bool=false)
+    params = getparams(components, ["alpha/Twu/Twu_like.csv"]; userlocations=userlocations, verbose=verbose,ignore_missing_singleparams = ["N"])
+    M = params["M"]
+    N = params["N"]
+    L = params["L"]
+
+    n = length(components)
+    for i in 1:n
+        if N.ismissingvalues[i]
+            N[i] = 2
+        end
+    end
     packagedparams = TwuAlphaParam(M,N,L)
     model = TwuAlpha(packagedparams, verbose=verbose)
     return model
@@ -70,3 +114,5 @@ function α_function(model::CubicModel,V,T,z::SingleComp,alpha_model::TwuAlphaMo
     Tr = T/Tc
     α = Tr^(N*(M-1))*exp(L*(1-Tr^(N*M)))
 end
+
+export TwuAlpha, Twu88Alpha, Twu91Alpha
