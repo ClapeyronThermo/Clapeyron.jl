d6f4da5ac6b9145ecf219133063296149d4a7596
pw0908 <pjwalker@caltech.edu>
2023-08-21 07:54:56 -0700

    Generalise solid solubility calculation

--- src/models/CompositeModel/SolidModel/SolidSolubility.jl
+++ src/models/CompositeModel/SolidModel/SolidSolubility.jl
@@ -14,13 +14,7 @@ default_references(::Type{SolidSolubility}) = String[]
 function chemical_potential(model::SolidSolubilityModel, p, T, z)
     Hfus = model.params.Hfus.values
     Tm = model.params.Tm.values
-    μ = zeros(length(z))
-    for i in @comps
-        if !isnothing(Tm[i])
-            μ[i] = -Hfus[i]/(Rgas()*Tm[i])*(1-T/Tm[i])
-        end
-    end
-    return μ
+    return @. Hfus*T*(1/Tm-1/T)
 end
 
 export SolidSolubility
