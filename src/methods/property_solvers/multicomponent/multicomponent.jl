f8d0653d5e96e638a4983db86b055089c82b4961
pw0908 <pjwalker@caltech.edu>
2023-08-21 07:55:12 -0700

    Add eutectic point solver

--- src/methods/property_solvers/multicomponent/multicomponent.jl
+++ src/methods/property_solvers/multicomponent/multicomponent.jl
@@ -208,13 +208,14 @@ include("UCST_mix.jl")
 include("tp_flash.jl")
 include("krichevskii_parameter.jl")
 include("solids/solid_solubility.jl")
+include("solids/eutectic_point.jl")
 
 export bubble_pressure_fug, bubble_temperature_fug, dew_temperature_fug, dew_pressure_fug
 export bubble_pressure,    dew_pressure,    LLE_pressure,    azeotrope_pressure, VLLE_pressure
 export bubble_temperature, dew_temperature, LLE_temperature, azeotrope_temperature, VLLE_temperature
 export crit_mix, UCEP_mix, UCST_mix
 export krichevskii_parameter
-export solid_solubility
+export solid_solubility, eutectic_point
 #UpperTriangular
 #UnitUpperTriangular
 #LowerTriangular
