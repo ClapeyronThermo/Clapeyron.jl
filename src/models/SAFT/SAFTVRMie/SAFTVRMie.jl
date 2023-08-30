2e11b8fcbf9d9ed7be27da2f2c56bea507c282e6
longemen3000 <longemen3000@gmail.com>
2023-08-30 17:58:27 -0400

    remove patch on VRMie

--- src/models/SAFT/SAFTVRMie/SAFTVRMie.jl
+++ src/models/SAFT/SAFTVRMie/SAFTVRMie.jl
@@ -791,5 +791,5 @@ function d(model::SAFTVRMie, V, T, z::SingleComp)
     σ = model.params.sigma
     λa = model.params.lambda_a
     λr = model.params.lambda_r
-    return d .+ SA[d_vrmie(T,λa[1],λr[1],σ[1],ϵ[1])]
+    return SA[d_vrmie(T,λa[1],λr[1],σ[1],ϵ[1])]
 end
