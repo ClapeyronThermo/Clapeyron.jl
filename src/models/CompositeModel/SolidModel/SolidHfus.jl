d9bceff4a0935c69510fcc6ce82b01b948cee779
pw0908 <pjwalker@caltech.edu>
2023-08-30 18:09:05 -0700

    Restart the PR

---
+++ src/models/CompositeModel/SolidModel/SolidHfus.jl
@@ -0,0 +1,20 @@
+abstract type SolidHfusModel <: EoSModel end
+
+struct SolidHfusParam <: EoSParam
+    Hfus::SingleParam{Float64}
+    Tm::SingleParam{Float64}
+end
+
+@newmodelsimple SolidHfus SolidHfusModel SolidHfusParam
+
+SolidHfus
+default_locations(::Type{SolidHfus}) = String[]
+default_references(::Type{SolidHfus}) = String[]
+
+function chemical_potential(model::SolidHfusModel, p, T, z)
+    Hfus = model.params.Hfus.values
+    Tm = model.params.Tm.values
+    return @. Hfus*T*(1/Tm-1/T)
+end
+
+export SolidHfus
