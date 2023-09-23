fdd1feff387b7349905b5167d0ed14640d061658
longemen3000 <longemen3000@gmail.com>
2023-09-23 18:59:17 -0300

    overload __eval_G_DETPFlash for activity models

--- src/models/Activity/methods/DIfferentialEvolutiontp_flash.jl
+++ src/models/Activity/methods/DIfferentialEvolutiontp_flash.jl
@@ -1,3 +1,4 @@
 function __eval_G_DETPFlash(model::PTFlashWrapper{<:ActivityModel},p,T,xi,vcache,equilibrium)
-
+    throw(error("Activity Model does not support DETPFlash."))
+    #TODO: actually support this?
 end
