3efd2c82887bc1c226b81f7b78725a17f74d744d
longemen3000 <longemen3000@gmail.com>
2023-10-08 01:55:12 -0300

    first commit (Clapeyron skip ci)

---
+++ src/models/SAFT/SAFTVRMie/variants/VRS.jl
@@ -0,0 +1,218 @@
+#TODO: find a better name
+
+struct VRSParam <: EoSParam
+    Mw::SingleParam{Float64}
+    segment::SingleParam{Float64}
+    sigma::PairParam{Float64}
+    lambda_a::PairParam{Float64}
+    lambda_r::PairParam{Float64}
+    epsilon::PairParam{Float64}
+end
+
+abstract type VRSModel <: SAFTModel end
+@newmodel VRS VRSModel VRSParam
+default_references(::Type{VRS}) = ["TODO"]
+default_locations(::Type{VRS}) = ["SAFT/SAFTVRMie", "properties/molarmass.csv"]
+
+function transform_params(::Type{VRS},params)
+    sigma = params["sigma"]
+    sigma.values .*= 1E-10
+    sigma = sigma_LorentzBerthelot(sigma)
+    epsilon = epsilon_HudsenMcCoubrey(params["epsilon"], sigma)
+    lambda_a = lambda_LorentzBerthelot(params["lambda_a"])
+    lambda_r = lambda_LorentzBerthelot(params["lambda_r"])
+    params["sigma"] = sigma
+    params["epsilon"] = epsilon
+    params["lambda_a"] = lambda_a
+    params["lambda_r"] = lambda_r
+    return params
+end
+
+function a_res(model::VRSModel,V,T,z)
+    _data = @f(data)
+    return @f(a_mono,_data) + @f(a_chain,_data)
+end
+
+function ζ3(model::VRSModel, V, T, z,_d=@f(d),m̄ = dot(z,model.params.segment.values))
+    m = model.params.segment
+    ζ3 = zero(V+T+first(z)+one(eltype(model)))
+    for i ∈ 1:length(z)
+        di =_d[i]
+        xS = z[i]*m[i]/m̄
+        ζ3 += xS*di*di*di
+    end
+    c = π/6*N_A*m̄/V
+    ζ3 = c*ζ3
+    return ζ3
+end
+
+function data(model::VRSModel,V,T,z)
+    ∑z = sum(z)
+    m̄ = dot(model.params.segment.values,z)
+    _d = d(model,V,T,z)
+    η = ζ3(model,V,T,z,_d,m̄)
+    ρS = N_A/V*m̄
+    ρ0 = 1 #TODO
+    γ = 4*(1 - ρs/ρ0)
+    α = ρ0ρs - 1
+    Z = Zhs_hall(γ,α)
+    J̄ = J(model,V,T,z,_d,η,Z)
+    return m̄,d,η,ρS,ρ0,Z,J̄
+end
+
+function a_mono(model::VRSModel,V,T,z,_data = @f(data))
+    ahs = @f(a_hs,_data)
+    a1 = @f(a_1,_data)
+    return m̄*(ahs + a1)
+end
+
+function a_hs(model::VRSModel,V,T,z,_data = @f(data))
+    m̄,d,η,ρS,ρ0,Z,J̄ = _data
+    ρ0ρs = ρ0/ρs
+    γ = 4*(1 - ρs/ρ0)
+    α = ρ0ρs - 1
+    S₀ = -0.24 #± 0.04
+    res = -S₀ + 1 + log(ρ0ρs) - 3*log(2*α/3)
+    #=
+    #Zhs(γ) - Zref = evalpoly(γ,(-0.442304,0.1253077,0.1762393,-1.053308,2.818621,-2.921934,1.118413))
+    #integral(0,γ,evalpoly(γ,pol)/(γ - 4)) = integral(polynomial part) + integral(fractional part)
+
+    #evalpoly(γ,pol)/(γ - 4) =
+    1.11841*x^5 + 1.55172*x^4 + 9.02549*x^3 + 35.0487*x^2 + 140.371*x + 561.609 + 2245.99/(x - 4)
+    (wolfram alpha)
+    p_int = (561.609,140.371,35.0487,9.02549,1.55172,1.11841)
+    = evalpoly(x,p_int) +  2245.99/(x - 4)
+    =#
+    p_int = (561.609,140.371,35.0487,9.02549,1.55172,1.11841)
+    ∫0 = -3113.603272131663 #log(abs(x-4))*2245.99 at x = 0, the polynomial integral evaluated at 0 == 0
+    ∫γ = evalpolyint(γ,p_int) + 2245.99*log(abs(γ-4))
+    return res + ∫γ - ∫0
+end
+
+function Zhs_hall(γ,α) #eq 5
+    Zhs = 3/α + evalpoly(γ,(2.557696,0.1253077,0.1762393,-1.053308,2.818621,-2.921934,1.118413))
+end
+
+function Zhs_hall(model::VRSModel,V,T,z)
+    ρS = N_A/V*m̄
+    ρ0 = 1 #TODO
+    γ = 4*(1 - ρs/ρ0)
+    α = ρ0ρs - 1
+    return Zhs_hall(γ,α)
+end
+
+function g_hs_2_n(model::VRSModel,η,d,r)
+    k = K(model,η)
+    n = VRSConsts.r_fcc
+    g = zero(typeof(η))
+    rd = r/d
+    dr = d/r
+    for ri in 2:length(g)
+        ni = n[i]
+        ri = 1
+        g += (dr*(d/ri)*K*ni*/(sqrt(π)*24*η))*exp(-(k*(rd - ri/d))^2)
+    end
+    return g
+end
+
+function r1_d(η) #SA, eq 1
+    ηc = 0.740480489693061
+    η✷ = ηc - η
+    num = evalpoly(η✷,(1.0,-8.0521,18.003))
+    denom = evalpoly(η✷,(1.0,-8.2973,20.546,-13.828,103.95,-582.74,1245.7))
+    return num/denom
+end
+
+function J(model::VRSModel,V,T,z,_d = @f(d),η = @f(ζ3,_d),Z = @f(Zhs_hall))
+    #TODO
+    #=
+    g_hs(1,η) = (z-1)/(4*η)
+    g_hs_1(1,η) + g_hs_2_n(1,η) = (z-1)/(4*η)
+    g_hs_1(1,η) = (z-1)/(4*η) - g_hs_2_n(1,η)
+    J*ghs_1_divJ(1,η) =  (z-1)/(4*η) - g_hs_2_n(1,η)
+    J = 
+    =#
+    r1d = r1_d(η)
+    k₁ = K1(model,η)
+    k₂ = K2(model,η)
+    J̄ = zeros(length(model),V+T+first(z))
+    for i in @comps
+        di = _d[i]
+        ghs_at_1 = g_hs_2_n(model,η,di,di)
+        _k12 = (-k₁*(1-r1d))^2
+        _k24 = (-k₂*(1-r1d))^4
+        ghs_1_divJ = exp(_k12+_k24)
+        J̄[i] = ((Z-1)/(4*η) - ghs_at_1)/ghs_1_divJ
+    end
+    return J̄
+end
+
+function K1(model::VRSModel,η)   #eq 15
+    ηc = 0.740480489693061
+    η✷ = ηc - η
+    return 1.5338/η✷ - 0.37687*exp(-989.6*(η - 0.52)^2) + evalpoly(η✷,(-2.5146,-1.3574,-8.5038))
+end
+
+function K2(model::VRSModel,η)  #eq 16
+    ηc = 0.740480489693061
+    η✷ = ηc - η
+    return 0.80313/η✷ - 1.208*exp(5.6128*η✷) + 67.808*η✷*η✷ - 67.918*η✷*η✷*η✷
+end
+
+function K(model::VRSModel,η)  #eq 17
+    ηc = 0.740480489693061
+    η✷ = ηc - η
+    return 1.9881/η✷ + evalpoly(η✷,(-3.5276,6.9762,-26.205))
+end
+
+function a_1(model::VRSModel,V,T,z,_data = @f(data))
+    m̄,d,η,ρS,ρ0,Z,J̄ = _data
+    m̄inv = 1/m̄
+    a₁ = zero(V+T+first(z)+one(eltype(model)))
+    for i ∈ @comps
+        xsᵢ = z[i]*m[i]*m̄inv
+        dᵢᵢ = d[i]
+        a₁ᵢᵢ = 1 #TODO: make integral here
+        a₁ += a₁ᵢᵢ*xsᵢ*xsᵢ
+        for j ∈ @comps
+            xsⱼ = z[i]*m[i]*m̄inv
+            dᵢⱼ = 0.5*(dᵢᵢ+d[j])
+            a₁ᵢⱼ = 1 #TODO: make integral here
+            a₁ += 2*a₁ᵢⱼ*xsᵢ*xsⱼ
+        end
+    end
+    return a₁/T
+end
+
+function a_chain(model::VRSModel,V,T,z,_data = @f(data))
+    achain = zero(V+T+first(z)+one(eltype(model)))
+    m = model.params.segment.values
+    # eq 8, using linear mixing (from SAFTVRMie)
+    for i in 1:length(model)
+        g_Mie = g_mie(model,V,T,z,i,_data)
+        achain -= z[i]*(log(g_Mie)*(m[i] - 1))
+    end
+    return achain/sum(z)
+end
+
+function g_mie(model::VRSModel,V,T,z,i,_data = @f(data))
+    σ = model.params.sigma[i]
+    λa = model.params.lambda_a[i]
+    λr = model.params.lambda_r[i]
+    
+    V_hs = 1 #TODO: V_hs(σi)
+    V0 = 1 #TODO: V0(σi)
+    y_hs = g_hs(model,V,T,z,_data,σi)*exp(β*V_hs)
+    return y_hs*exp(β*V0)
+end
+
+function d(model::VRSModel,V,T,z)
+
+end
+
+#SA, Table 1
+#note: idx = [14,30,46,56,62] are missing.
+#for purposes of wiping out an MVE, those values are replaced with 1
+VRSConsts = (;
+    r_fcc = [12, 6, 24, 12, 24, 8, 48, 6, 36, 24, 24, 24, 72, 1, 48, 12, 48, 30, 72, 24, 48, 24, 48, 8, 84, 24, 96, 48, 24, 1, 96, 6, 96, 48, 48, 36, 120, 24, 48, 24, 48, 48, 120, 24, 120, 1, 96, 24, 108, 30, 48, 72, 72, 32, 144, 1, 96, 72, 72, 48, 120, 1, 144, 12, 48],
+    )
