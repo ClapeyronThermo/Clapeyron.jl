16370d63f8cfd5bf7974c0da773b269d794c3f8b
pw0908 <pjwalker@caltech.edu>
2023-12-08 16:04:18 -0800

    Update methods

--- src/methods/property_solvers/electrolytes/mT.jl
+++ src/methods/property_solvers/electrolytes/mT.jl
@@ -1,4 +1,4 @@
-function mean_ionic_activity_coefficient(model::ESElectrolyteModel,salts,T,m,zsolvent=[1.])
+function mean_ionic_activity_coefficient_sat(model::ESElectrolyteModel,salts,T,m,zsolvent=[1.])
     isolvent = model.icomponents[model.charge.==0]
     iions = model.icomponents[model.charge.!=0]
     ions = model.components[model.charge.!=0]
@@ -7,14 +7,17 @@ function mean_ionic_activity_coefficient(model::ESElectrolyteModel,salts,T,m,zso
 
     ν = salt_stoichiometry(model,salts)
 
-    z0 = molality_to_composition(model,salts,ones(length(m)).*1e-10,zsolvent)
+    z0 = molality_to_composition(model,salts,ones(length(m)).*1e-20,zsolvent)
     z = molality_to_composition(model,salts,m,zsolvent)
 
     (p0,vl0,vv0,y0) = bubble_pressure(model,T,z0,method)
-    φ0 = VT_fugacity_coefficient(model,vl0,T,z0)[iions]
+    φ0 = fugacity_coefficient(model,vl0,T,z0;phase=:l)[iions]
 
     (p,vl,vv,y) = bubble_pressure(model,T,z,method)
-    φ = VT_fugacity_coefficient(model,vl,T,z)[iions]
+    φ = fugacity_coefficient(model,vl,T,z;phase=:l)[iions]
+
+    println(φ0)
+    println(φ)
 
     γim = φ./φ0.*sum(z[isolvent])/sum(z)
     γsm = (prod(γim'.^ν,dims=2)).^(1 ./sum(ν,dims=2))
@@ -28,7 +31,7 @@ function mean_ionic_activity_coefficient(model::ESElectrolyteModel,salts,p,T,m,z
 
     ν = salt_stoichiometry(model,salts)
 
-    z0 = molality_to_composition(model,salts,ones(length(m)).*1e-10,zsolvent)
+    z0 = molality_to_composition(model,salts,ones(length(m)).*1e-20,zsolvent)
     z = molality_to_composition(model,salts,m,zsolvent)
 
     φ0 = fugacity_coefficient(model,p,T,z0)[iions]
@@ -40,7 +43,7 @@ function mean_ionic_activity_coefficient(model::ESElectrolyteModel,salts,p,T,m,z
     return γsm
 end
 
-function osmotic_coefficient(model::ESElectrolyteModel,salts,T,m,zsolvent=[1.])
+function osmotic_coefficient_sat(model::ESElectrolyteModel,salts,T,m,zsolvent=[1.])
     isolvent = model.icomponents[model.charge.==0]
     iions = model.icomponents[model.charge.!=0]
     ions = model.components[model.charge.!=0]
@@ -49,20 +52,35 @@ function osmotic_coefficient(model::ESElectrolyteModel,salts,T,m,zsolvent=[1.])
 
     ν = salt_stoichiometry(model,salts)
 
-    z0 = molality_to_composition(model,salts,ones(length(m)).*1e-10,zsolvent)
+    z0 = molality_to_composition(model,salts,ones(length(m)).*1e-20,zsolvent)
     z = molality_to_composition(model,salts,m,zsolvent)
 
-    (p0,vl0,vv0,y0) = bubble_pressure(model,T,z0,method)
-    φ0 = VT_fugacity_coefficient(model,vl0,T,z0)[isolvent]
-
     (p,vl,vv,y) = bubble_pressure(model,T,z,method)
-    φ = VT_fugacity_coefficient(model,vl,T,z)[isolvent]
+    φ = fugacity_coefficient(model,p,T,z;phase=:l)[isolvent]
+    φ0 = fugacity_coefficient(model,p,T,z0;phase=:l)[isolvent]
 
     asolv = φ./φ0.*z[isolvent]/sum(z)
-    Mw = model.neutralmodel.params.Mw.values[isolvent].*1e-3
-    # println(sum(ν.*m).*Mw)
-    println(φ./φ0)
+    Mw = mw(model.neutralmodel)[isolvent].*1e-3
+    # println()
     return -1 ./(sum(ν.*m).*Mw).*log.(asolv)
 end
 
-export mean_ionic_activity_coefficient, osmotic_coefficient
+function osmotic_coefficient(model::ESElectrolyteModel,salts,p,T,m,zsolvent=[1.])
+    isolvent = model.icomponents[model.charge.==0]
+
+    ν = salt_stoichiometry(model,salts)
+
+    z0 = molality_to_composition(model,salts,ones(length(m)).*1e-20,zsolvent)
+    z = molality_to_composition(model,salts,m,zsolvent)
+
+    φ0 = fugacity_coefficient(model,p,T,z0;phase=:l)[isolvent]
+
+    φ = fugacity_coefficient(model,p,T,z;phase=:l)[isolvent]
+
+    asolv = φ./φ0.*z[isolvent]/sum(z)
+    Mw = mw(model.neutralmodel)[isolvent].*1e-3
+    # println()
+    return -1 ./(sum(ν.*m).*Mw).*log.(asolv)
+end
+
+export mean_ionic_activity_coefficient, osmotic_coefficient, mean_ionic_activity_coefficient_sat, osmotic_coefficient_sat
