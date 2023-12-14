9ec721d05043ffdf909646b2218c80e15999f0da
longemen3000 <longemen3000@gmail.com>
2023-12-14 18:12:25 -0300

    changes to make the new single phase solvers compatible with the new CompositeModel eq

--- src/methods/property_solvers/singlecomponent/sublimation.jl
+++ src/methods/property_solvers/singlecomponent/sublimation.jl
@@ -1,18 +1,25 @@
 function obj_sublimation_pressure(model::CompositeModel,F,T,vs,vv,p̄,T̄)
+    eos_solid(V) = eos(model.solid,V,T,z)
+    eos_fluid(V) = eos(model.fluid,V,T,z)
+    A_v,Av_v = Solvers.f∂f(eos_fluid,vv)
+    A_s,Av_s =Solvers.f∂f(eos_solid,vs)
+    μv = muladd(-vv,Av_v,A_v)
+    μs = muladd(-vs,Av_s,A_s)
+    ps = - Av_s
+    pv = - Av_v
+    #=
     μs = VT_chemical_potential(model.solid, vs, T)[1]
-    μv = VT_chemical_potential(model.gas, vv, T)[1]
-
+    μv = VT_chemical_potential(model.fluid, vv, T)[1]
     ps = pressure(model.solid, vs, T)
-    pv = pressure(model.gas, vv, T)
-
+    pv = pressure(model.fluid, vv, T) =#
     F[1] = (μs - μv)/R̄/T̄
     F[2] = (ps - pv)/p̄
     return F
 end
 
 function sublimation_pressure(model::CompositeModel,T;v0=x0_sublimation_pressure(model,T))
-    T̄ = T_scale(model.gas)
-    p̄ = p_scale(model.gas)
+    T̄ = T_scale(model.fluid)
+    p̄ = p_scale(model.fluid)
 
     f!(F,x) = obj_sublimation_pressure(model,F,T,exp10(x[1]),exp10(x[2]),p̄,T̄)
 
@@ -22,11 +29,11 @@ function sublimation_pressure(model::CompositeModel,T;v0=x0_sublimation_pressure
 
     vs = exp10(x[1])
     vv = exp10(x[2])
-    return pressure(model.gas, vv, T), vs, vv
+    return pressure(model.fluid, vv, T), vs, vv
 end
 
 function x0_sublimation_pressure(model,T)
     vs0 = log10(lb_volume(model.solid)*6*1.05/π)
-    vv0 = log10(lb_volume(model.gas)*6*1.25/π)
+    vv0 = log10(lb_volume(model.fluid)*6*1.25/π)
     return [vs0,vv0]
 end
--- src/methods/property_solvers/singlecomponent/triple_point.jl
+++ src/methods/property_solvers/singlecomponent/triple_point.jl
@@ -1,11 +1,11 @@
 function obj_triple_point(model::CompositeModel,F,T,vs,vl,vv,p_scale,T_scale)
     μs = VT_chemical_potential(model.solid, vs, T)[1]
-    μl = VT_chemical_potential(model.liquid, vl, T)[1]
-    μv = VT_chemical_potential(model.gas, vv, T)[1]
+    μl = VT_chemical_potential(model.fluid, vl, T)[1]
+    μv = VT_chemical_potential(model.fluid, vv, T)[1]
 
     ps = pressure(model.solid, vs, T)
-    pl = pressure(model.liquid, vl, T)
-    pv = pressure(model.gas, vv, T)
+    pl = pressure(model.fluid, vl, T)
+    pv = pressure(model.fluid, vv, T)
 
     F[1] = (μs - μl)/R̄/T_scale
     F[2] = (μl - μv)/R̄/T_scale
@@ -17,30 +17,31 @@ end
 function x0_triple_point(model::CompositeModel)
     T0 = 0.65
     vs0 = log10(lb_volume(model.solid)*1.02*6/π)
-    vl0 = log10(lb_volume(model.liquid)*1.15*6/π)
-    vv0 = log10(lb_volume(model.gas)*1000)
+    vl0 = log10(lb_volume(model.fluid)*1.15*6/π)
+    vv0 = log10(lb_volume(model.fluid)*1000)
 
     return [T0,vs0,vl0,vv0]
 end
 
 function triple_point(model::CompositeModel;v0 = x0_triple_point(model))
-    if isnothing(model.solid) || isnothing(model.liquid) || isnothing(model.gas)
+    if isnothing(model.solid) || isnothing(model.fluid)
         throw(ArgumentError("triple_point: model must have solid, liquid and gas phases"))
     end
 
-    T̄  = T_scale(model.liquid)
-    p̄  = p_scale(model.liquid)
+    T̄  = T_scale(model.fluid)
+    p̄  = p_scale(model.fluid)
 
-    f!(F,x) = obj_triple_point(model,F,x[1]*model.solid.params.epsilon[1],exp10(x[2]),exp10(x[3]),exp10(x[4]),p̄,T̄)
+    Ts = T_scale(model.solid)
+    f!(F,x) = obj_triple_point(model,F,x[1]*Ts,exp10(x[2]),exp10(x[3]),exp10(x[4]),p̄,T̄)
 
     results = Solvers.nlsolve(f!,v0,TrustRegion(Newton(),Dogleg()))
 
     x = Solvers.x_sol(results)
 
-    Tp = x[1]*model.solid.params.epsilon[1]
+    Tp = x[1]*Ts
     vs = exp10(x[2])
     vl = exp10(x[3])
     vv = exp10(x[4])
-    pp = pressure(model.gas, vv, Tp)
+    pp = pressure(model.fluid, vv, Tp)
     return (Tp,pp,vs,vl,vv)
 end
