f654102cdad9dc5f9de2b28acd4b16d0aa3921ce
longemen3000 <longemen3000@gmail.com>
2023-12-14 18:35:44 -0300

    docs for solid single phase

--- src/methods/property_solvers/singlecomponent/triple_point.jl
+++ src/methods/property_solvers/singlecomponent/triple_point.jl
@@ -22,7 +22,18 @@ function x0_triple_point(model::CompositeModel)
 
     return [T0,vs0,vl0,vv0]
 end
+"""
+    Tt,pt,vs,vl,vv = triple_point(model::CompositeModel;v0 = x0_triple_point(model))
 
+Calculates the triple point of a `CompositeModel` containing solid and fluid phase EoS.
+
+returns:
+- Triple point Temperature [`K`]
+- Triple point Pressure [`Pa`]
+- solid volume at Triple Point [`m³`]
+- liquid volume at Triple Point [`m³`]
+- vapour volume at Triple Point [`m³`]
+"""
 function triple_point(model::CompositeModel;v0 = x0_triple_point(model))
     if isnothing(model.solid) || isnothing(model.fluid)
         throw(ArgumentError("triple_point: model must have solid, liquid and gas phases"))
