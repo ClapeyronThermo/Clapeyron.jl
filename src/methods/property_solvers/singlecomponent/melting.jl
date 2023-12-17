b06abeae0537fb24c9be7075a06e14aaa44c3862
longemen3000 <longemen3000@gmail.com>
2023-12-17 19:31:08 -0300

    initial point for melting pressure

--- src/methods/property_solvers/singlecomponent/melting.jl
+++ src/methods/property_solvers/singlecomponent/melting.jl
@@ -1,10 +1,15 @@
 function obj_melting_pressure(model::CompositeModel,F,T,vs,vl,p̄,T̄)
-    μs = VT_chemical_potential(model.solid, vs, T)[1]
-    μl = VT_chemical_potential(model.fluid, vl, T)[1]
-    ps = pressure(model.solid, vs, T)
-    pl = pressure(model.fluid, vl, T)
+    z = SA[1.0]
+    eos_solid(V) = eos(model.solid,V,T,z)
+    eos_fluid(V) = eos(model.fluid,V,T,z)
+    A_l,Av_l = Solvers.f∂f(eos_fluid,vl)
+    A_s,Av_s =Solvers.f∂f(eos_solid,vs)
+    μl = muladd(-vl,Av_l,A_l)
+    μs = muladd(-vs,Av_s,A_s)
+    ps = - Av_s
+    pl = - Av_l
     F[1] = (μs - μl)/R̄/T̄
-    F[2] = log((ps - pl)/p̄)
+    F[2] = (ps - pl)/p̄
     return F
 end
 
@@ -55,13 +60,13 @@ function melting_pressure_impl(model::CompositeModel,T,method::ChemPotMeltingPre
     T̄ = T_scale(model.fluid)
     p̄ = p_scale(model.fluid)
     if method.v0 == nothing
-        v0 = x0_sublimation_pressure(model,T)
+        v0 = x0_melting_pressure(model,T)
     else
         v0 = method.v0
     end
     V0 = vec2(log(v0[1]),log(v0[2]),T)
     f!(F,x) = obj_melting_pressure(model,F,T,exp(x[1]),exp(x[2]),p̄,T̄)
-    results = Solvers.nlsolve(f!,V0)
+    results = Solvers.nlsolve(f!,V0,LineSearch(Newton()))
     x = Solvers.x_sol(results)
     vs = exp(x[1])
     vl = exp(x[2])
@@ -70,7 +75,40 @@ function melting_pressure_impl(model::CompositeModel,T,method::ChemPotMeltingPre
 end
 
 function x0_melting_pressure(model::CompositeModel,T)
-    vs0 = x0_volume_solid(solid_model(model),T,SA[1.0])
-    vl0 = x0_volume_liquid(fluid_model(model),T,SA[1.0])
+    solid = solid_model(model)
+    liquid = fluid_model(model)
+    z = SA[1.0]
+    vs00 = x0_volume_solid(solid,T,z)
+    vl00 = x0_volume_liquid(liquid,T,z)
+    fs(_V) = eos(solid,_V,T,z)
+    fl(_V) = eos(liquid,_V,T,z)
+    as,das,d2as = Solvers.f∂f∂2f(fs,vs00)
+    al,dal,d2al = Solvers.f∂f∂2f(fs,vs00)
+    ps = -das
+    pl = -dal
+    #=
+    strategy:
+    quadratic taylor expansion for helmholtz energy
+    isothermal compressibility aproximation for pressure
+    =#
+    p_scale,μ_scale =  scale_sat_pure(liquid)
+    As(_V) = as + das*(_V - vs00) + 0.5*d2as*(_V - vs00)^2
+    Al(_V) = al + dal*(_V - vl00) + 0.5*d2al*(_V - vl00)^2
+    Ps(_V) = log(_V/vs00)*(-_V*d2as) + ps
+    Pl(_V) = log(_V/vl00)*(-_V*d2al) + pl
+    function f0(F,x)
+        vs = exp(x[1])
+        vl = exp(x[2])
+        asi,ali = As(vs),Al(vl)
+        psi,pli = Ps(vs),Pl(vl)
+        μsi,μli = asi + psi*vs, ali + pli*vl
+        F[1] = (μsi - μli)*μ_scale
+        F[2] = (psi - pli)*p_scale
+    end
+    x0 = vec2(log(vs00),log(vl00),T)
+    res = Solvers.nlsolve(f0,x0,LineSearch(Newton()))
+    v = Solvers.x_sol(res)
+    vs0 = exp(v[1])
+    vl0 = exp(v[2])
     return (vs0,vl0)
 end
