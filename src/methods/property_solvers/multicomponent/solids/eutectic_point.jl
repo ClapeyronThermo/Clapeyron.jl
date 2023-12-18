756cbdcfdf0c20f2cfa1cf5139dd6d183fc9c3bb
longemen3000 <longemen3000@gmail.com>
2023-12-18 08:42:24 -0300

    typos

--- src/methods/property_solvers/multicomponent/solids/eutectic_point.jl
+++ src/methods/property_solvers/multicomponent/solids/eutectic_point.jl
@@ -13,8 +13,8 @@ function eutectic_point(model::CompositeModel,p=1e5)
     end
     solid = solid_model(model)
     fluid = fluid_model(model)
-    f!(F,x) = obj_eutectic_point(F,model.solid,model.fluid,p,x[1]*200.,FractionVector(x[2]))
-    x0 = x0_eutectic_point(model)
+    f!(F,x) = obj_eutectic_point(F,solid,fluid,p,x[1]*200.,FractionVector(x[2]))
+    x0 = x0_eutectic_point(model,p)
     # println(x0)
     results = Solvers.nlsolve(f!,x0)
     T = Solvers.x_sol(results)[1]*200
@@ -46,5 +46,5 @@ function x0_eutectic_point(model::EoSModel,p)
     T0inv = Roots.solve(prob)
     T0 = 1/T0inv
     x0 = exp(-Hfus[1]/Rgas()*(1/T0-1/Tm[1]))
-    return (T0/200.,x0)
+    return [T0/200.,x0]
 end
