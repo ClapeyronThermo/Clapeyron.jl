7c11eb1cba0d357c088837310de027494b5ffd52
longemen3000 <longemen3000@gmail.com>
2023-12-20 17:16:09 -0300

    CPA: default unit for epsilon_assoc changed to K

--- src/models/SAFT/CPA/variants/sCPA.jl
+++ src/models/SAFT/CPA/variants/sCPA.jl
@@ -25,8 +25,7 @@
 - `b`: Single Parameter (`Float64`) - Covolume `[m^3/mol]`
 - `c1`: Single Parameter (`Float64`) - α-function constant Parameter (no units)
 - `k`: Pair Parameter (`Float64`) (optional) - Binary Interaction Paramater (no units)
-- `epsilon_assoc`: Association Parameter (`Float64`) - Reduced association energy `[J]`
-- `epsilon_assoc_over_R`: Association Parameter (`Float64`) (alternative to `epsilon_assoc`) - Reduced association energy `[K]`
+- `epsilon_assoc`: Association Parameter (`Float64`) - Reduced association energy `[K]`
 - `bondvol`: Association Parameter (`Float64`) - Association Volume `[m^3]`
 
 ## Model Parameters
