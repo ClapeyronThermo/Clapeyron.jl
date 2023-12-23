e487443cd9e7982992541665c10c7b7f01912e57
longemen3000 <longemen3000@gmail.com>
2023-12-23 04:13:19 -0300

    start CPCA

--- src/models/SAFT/CPA/variants/CPCA.jl
+++ src/models/SAFT/CPA/variants/CPCA.jl
@@ -1,14 +1,69 @@
-struct CPAParam <: EoSParam
-    Mw::SingleParam{Float64}
-    Tc::SingleParam{Float64}
-    a::PairParam{Float64}
-    b::PairParam{Float64}
-    c1::SingleParam{Float64}
-    epsilon_assoc::AssocParam{Float64}
-    bondvol::AssocParam{Float64}
+abstract type CPCAMixingModel <: vdW1fRuleModel end
+
+struct CPCAMixingParam <:EoSParam
+    kb1::SingleParam{Float64}
+    kb2::SingleParam{Float64}
+    segment::SingleParam{Float64}
 end
 
-struct CPA{T <: IdealModel,c <: CubicModel} <: CPAModel
+struct CPCAMixing <: CPCAMixingModel
+    components::Array{String,1}
+    params::CPCAMixingParam
+    references::Array{String,1}
+end
+
+function CPCAMixing(components; activity = nothing, userlocations=String[],activity_userlocations=String[], verbose::Bool=false)
+    params = getparams(components,String[], userlocations=userlocations, verbose=verbose)
+    k1 = params["kb1"]
+    k2 = params["kb2"]
+    segment = params["segment"]
+    param = CPCAMixingParam(kb1,kb2,segment)
+    return CPCAMixing(format_components(components), param, ["10.1021/acs.iecr.2c00902"])
+end
+
+function mixing_rule(model::CPCAMixingModel, V, T, z, model_params)
+    kb1 = model_params.kb1.values
+    kb2 = model_params.kb2.values
+    segment = model_params.segment.values
+end
+
+const CPCACubic = RK{I,CPAAlpha,T,CPCAMixing} where {I,T}
+
+function cubic_ab(model::CPCACubic,V,T,z=SA[1.0],n=sum(z))
+    a = model.params.a.values
+    b = model.params.b.values
+    T = T*float(one(T))
+    α = @f(α_function,model.alpha)
+    c = @f(translation,model.translation)
+    if length(z) > 1
+    ā,b̄,c̄ = @f(mixing_rule,model.mixing,α,a,b,c)
+    else
+        ā = a[1,1]*α[1]
+        kb1 = model.mixing.params.kb1.values[1]
+        kb2 = model.mixing.params.kb2.values[1]
+        m = model.mixing.params.segment.values[1]
+        Tc = model.params.Tc.values[1]
+        β = kb1*exp(-kb2*T/Tc)
+        b̄ = b[1,1]*β
+        c̄ = c[1]
+    end
+    return ā ,b̄, c̄
+end
+
+
+abstract type CPCAModel <: CPAModel end
+
+struct CPCAParam <: EoSParam
+    Mw::SingleParam{Float64}
+    Tc::SingleParam{Float64}
+    segment::SingleParam{Float64}
+    a::PairParam{Float64}
+    b::PairParam{Float64}
+    epsilon_assoc::AssocParam{Float64}
+    bondvol::AssocParam{Float64} 
+end
+
+struct CPCA{T <: IdealModel,c <: CubicModel} <: CPAModel
     components::Array{String,1}
     radial_dist::Symbol
     cubicmodel::c
@@ -20,21 +75,13 @@ struct CPA{T <: IdealModel,c <: CubicModel} <: CPAModel
 end
 
 """
-    CPAModel <: EoSModel
+    CPCAModel <: CPAModel
 
-    function CPA(components;
-        radial_dist::Symbol = :CS,
+    function CPCA(components;
         idealmodel=BasicIdeal,
-        cubicmodel=RK,
-        alpha=sCPAAlpha,
-        mixing=vdW1fRule,
-        activity=nothing,
-        translation=NoTranslation,
+        translation = NoTranslation
         userlocations=String[],
         ideal_userlocations=String[],
-        alpha_userlocations=String[],
-        activity_userlocations=String[],
-        mixing_userlocations=String[],
         translation_userlocations=String[],
         verbose=false,
         assoc_options = AssocOptions())
@@ -44,7 +91,7 @@ end
 - `Tc`: Single Parameter (`Float64`) - Critical Temperature `[K]`
 - `a`: Single Parameter (`Float64`) - Atraction parameter `[m^6*Pa/mol]`
 - `b`: Single Parameter (`Float64`) - Covolume `[m^3/mol]`
-- `c1`: Single Parameter (`Float64`) - α-function constant Parameter (no units)
+- `segment`: Single Parameter (`Float64`) - Number of segments (no units)
 - `k`: Pair Parameter (`Float64`) (optional) - Binary Interaction Paramater (no units)
 - `l`: Pair Parameter (`Float64`) (optional) - Binary Interaction Paramater (no units)
 - `epsilon_assoc`: Association Parameter (`Float64`) - Reduced association energy `[K]`
@@ -54,54 +101,33 @@ end
 - `Mw`: Single Parameter (`Float64`) - Molecular Weight `[g/mol]`
 - `a`: Pair Parameter (`Float64`) - Mixed Atraction Parameter `[m^6*Pa/mol]`
 - `b`: Pair Parameter (`Float64`) - Mixed Covolume `[m^3/mol]`
-- `c1`: Single Parameter (`Float64`) - α-function constant Parameter (no units)
+- `segment`: Single Parameter (`Float64`) - Number of segments (no units)
 - `epsilon_assoc`: Association Parameter (`Float64`) - Reduced association energy `[J]`
 - `bondvol`: Association Parameter (`Float64`) - Association Volume `[m^3]`
 
 ## Input models
 - `idealmodel`: Ideal Model
-- `cubicmodel`: Cubic Model
+- `translation`: Translation model
 
 ## Description
 
-Cubic Plus Association (CPA) EoS. Consists in the addition of a cubic part and an association part:
+Cubic + Chain + Association (CPCA) EoS. Consists in the addition of a cubic part, a chain part and an association part:
 ```
-a_res(model::CPA) = a_res(model::Cubic) + a_assoc(model)
+a_res(model::CPA) = a_res(model::Cubic) + a_chain(model) + a_assoc(model)
 ```
-
-The `radial_dist` argument can be used to choose between a Carnahan-Starling form (`CS`, default) or the Kontogeorgis (`KG`) term, more widely known as s-CPA.
 
 ## References
 1. Kontogeorgis, G. M., Michelsen, M. L., Folas, G. K., Derawi, S., von Solms, N., & Stenby, E. H. (2006). Ten years with the CPA (cubic-plus-association) equation of state. Part 1. Pure compounds and self-associating systems. Industrial & Engineering Chemistry Research, 45(14), 4855–4868. [doi:10.1021/ie051305v](https://doi.org/10.1021/ie051305v)
 """
-CPA
-
-export CPA
-function CPA(components;
+function CPCA(components;
     idealmodel=BasicIdeal,
-    radial_dist::Symbol = :CS,
-    cubicmodel=RK,
-    alpha=CPAAlpha,
-    mixing=vdW1fRule,
-    activity=nothing,
-    translation=NoTranslation,
+    translation = NoTranslation
     userlocations=String[],
     ideal_userlocations=String[],
-    alpha_userlocations=String[],
-    activity_userlocations=String[],
-    mixing_userlocations=String[],
     translation_userlocations=String[],
     verbose=false,
     assoc_options = AssocOptions())
-
-    locs = if radial_dist == :CS
-        ["SAFT/CPA", "properties/molarmass.csv","properties/critical.csv"]
-    elseif radial_dist == :KG
-        ["SAFT/CPA/sCPA/", "properties/molarmass.csv","properties/critical.csv"]
-    else
-        throw(error("CPA: incorrect specification of radial_dist, try using `:CS` (original CPA) or `:KG` (simplified CPA)"))
-    end
-
+    locs = ["SAFT/CPA/CPCA/", "properties/molarmass.csv","properties/critical.csv"]
     params = getparams(components, locs; userlocations=userlocations, verbose=verbose)
     
     sites = get!(params,"sites") do
@@ -113,10 +139,11 @@ function CPA(components;
     end
 
     Mw  = params["Mw"]
+    alpha
+    
+    
     k = get(params,"k",nothing)
-    l = get(params,"k",nothing)
     Tc = params["Tc"]
-    c1 = params["c1"]
     a  = epsilon_LorentzBerthelot(params["a"], k)
     b  = sigma_LorentzBerthelot(params["b"], l)
 
@@ -129,19 +156,18 @@ function CPA(components;
     end
 
     bondvol,epsilon_assoc = assoc_mix(bondvol,epsilon_assoc,cbrt.(b),assoc_options)
-    packagedparams = CPAParam(Mw, Tc, a, b, c1, epsilon_assoc, bondvol)
+    packagedparams = CPCAParam(a, b, Tc, epsilon_assoc, bondvol, Mw)
     
     #init cubic model
     init_idealmodel = init_model(idealmodel,components,ideal_userlocations,verbose)
-    init_alpha = init_model(alpha,components,alpha_userlocations,verbose)
-    init_mixing = init_model(mixing,components,activity,mixing_userlocations,activity_userlocations,verbose)
+    init_alpha = CPAAlpha(components,userlocations = (;c1 = 10.2425 .* Mw .^ 0.2829)) 
+    init_mixing = CPCAMixing(components,userlocations = (;c1 = 0.0, c2 = 0.0))
     init_translation = init_model(translation,components,translation_userlocations,verbose)
     cubicparams = ABCubicParam(a, b, params["Tc"],Pc,Mw) #PR, RK, vdW
     init_cubicmodel = cubicmodel(components,init_alpha,init_mixing,init_translation,cubicparams,init_idealmodel,String[])
 
     references = ["10.1021/ie051305v"]
-
-    model = CPA(components, radial_dist, init_cubicmodel, packagedparams, sites, init_idealmodel, assoc_options, references)
+    model = CPCA(components, radial_dist, init_cubicmodel, packagedparams, sites, init_idealmodel, assoc_options, references)
     return model
 end
 
@@ -189,30 +215,7 @@ function x0_crit_pure(model::CPAModel)
     return (1.0, log10(lb_v/0.3))
 end
 
-data(model::CPAModel, V, T, z) = data(model.cubicmodel,V,T,z)
-
 function a_res(model::CPAModel, V, T, z, _data = @f(data))
     n,ā,b̄,c̄ = _data
     return a_res(model.cubicmodel,V,T,z,_data) + a_assoc(model,V+c̄*n,T,z,_data)
-end
-
-ab_consts(model::CPAModel) = ab_consts(model.cubicmodel)
-
-function Δ(model::CPAModel, V, T, z, i, j, a, b, _data = data(model.cubicmodel,V,T,z))
-    n,ā,b̄,c̄ = _data
-    ϵ_associjab = model.params.epsilon_assoc.values[i,j][a,b]
-    βijab = model.params.bondvol.values[i,j][a,b]
-    b = model.params.b.values
-    η = n*b̄/(4*V)
-
-    rdf = model.radial_dist
-    g = if rdf == :CS #CPA original
-        (1-0.5*η)/(1-η)^3
-    elseif rdf == :KG #sCPA
-        1/(1-1.9η)
-    else
-        zero(η)/zero(η)
-    end
-
-    return g*expm1(ϵ_associjab/T)*βijab*b[i,j]/N_A
 end
