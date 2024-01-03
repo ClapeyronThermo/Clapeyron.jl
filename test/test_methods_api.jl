87dbc60c39b1d283b7de38a42b3ca768d48a878b
longemen3000 <longemen3000@gmail.com>
2024-01-02 23:56:39 -0300

    typo in test

--- test/test_methods_api.jl
+++ test/test_methods_api.jl
@@ -520,9 +520,9 @@ end
 
     @testset "Tr = 0.995" begin
         model1 = PCSAFT("water")
-        Tc1,_,_ = crit_pure(model)
+        Tc1,_,_ = crit_pure(model1)
         T1 = 0.995Tc1
-        @test Clapeyron.saturation_pressure(model,T1,crit_retry = false)[1] ≈ 3.542008160105954e7 rtol = 1e-6
+        @test Clapeyron.saturation_pressure(model1,T1,crit_retry = false)[1] ≈ 3.542008160105954e7 rtol = 1e-6
         
         model2 = PCSAFT("eicosane")
         Tc2,_,_ = crit_pure(model2)
