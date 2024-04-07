89115aa67fc6ba5e4c19b37e4f4c55a4c81c537c
longemen3000 <longemen3000@gmail.com>
2024-04-07 02:28:55 -0400

    add normorder to fixpoint

--- src/modules/solvers/fixpoint/fixpoint.jl
+++ src/modules/solvers/fixpoint/fixpoint.jl
@@ -18,12 +18,13 @@ function fixpoint end
 struct SSFixPoint{T<:Real} <: AbstractFixPoint 
     dampingfactor::T
     lognorm::Bool
+    normorder::Float64
 end
 
-SSFixPoint(;dampingfactor=1.0,lognorm = false) = SSFixPoint(dampingfactor,lognorm)
-SSFixPoint(dampingfactor) = SSFixPoint(dampingfactor,false)
+SSFixPoint(;dampingfactor=1.0,lognorm = false,normorder = Inf) = SSFixPoint(dampingfactor,lognorm,Float64(normorder))
+SSFixPoint(dampingfactor) = SSFixPoint(dampingfactor,false,Inf)
 function promote_method(method::SSFixPoint,T)
-    return SSFixPoint(T(method.dampingfactor),method.lognorm)
+    return SSFixPoint(T(method.dampingfactor),method.lognorm,method.normorder)
 end
 
 struct AitkenFixPoint <: AbstractFixPoint end
@@ -32,7 +33,7 @@ function promote_method(method::AitkenFixPoint,T)
     return method
 end
 
-function convergence(xold,xi,atol,rtol,damping = 1.0,lognorm = false)
+function convergence(xold,xi,atol,rtol,damping = 1.0,lognorm = false,normorder = Inf)
     not_finite = false
     for xii in xi
         if !isfinite(xii)
@@ -44,18 +45,23 @@ function convergence(xold,xi,atol,rtol,damping = 1.0,lognorm = false)
     xi == xold && return (true,true) #terminate, with current number
     if xi isa Number
         if lognorm
-            Δx = abs(xi-xold)#/damping
+            Δx = abs(xi-xold)
         else
-            Δx = abs(xi-xold)#/damping
+            Δx = abs(xi/xold - 1)
         end
     else
         if lognorm
-            Δx = norm(((xi[i]/xold[i] - 1) for i in eachindex(xold,xi)),Inf)#/damping
+            Δx = norm(((xi[i]/xold[i] - 1) for i in eachindex(xold,xi)),normorder)
         else
-            Δx = norm((xi[i] - xold[i] for i in eachindex(xold,xi)),Inf)#/damping
+            Δx = norm((xi[i] - xold[i] for i in eachindex(xold,xi)),normorder)
         end
     end
-    normxi = norm(xi,Inf)
+    if lognorm
+        normxi = norm(xi,normorder)
+    else
+        normxi = zero(eltype(xi))/one(eltype(xi))
+    end
+
     if abs(Δx) < max(atol,normxi*rtol)
         return (true,true) #terminate, with current number
     end
@@ -84,14 +90,15 @@ function _fixpoint(f::F,
     nan = (0*atol)/(0*atol)
     xi = f(x0)
     α = method.dampingfactor
+    ℕ = method.normorder
     lognorm = method.lognorm
-    converged,finite = convergence(x0,xi,atol,rtol,α,lognorm)
+    converged,finite = convergence(x0,xi,atol,rtol,α,lognorm,ℕ)
     converged && return ifelse(finite,xi,nan)
     itercount = 1
     xold = x0
     while itercount < max_iters
         xi = α*f(xi) + (1-α)*xi  
-        converged,finite = convergence(xold,xi,atol,rtol,α,lognorm)
+        converged,finite = convergence(xold,xi,atol,rtol,α,lognorm,ℕ)
         converged && return ifelse(finite,xi,nan)    
         itercount +=1
         xold = xi
@@ -150,8 +157,9 @@ function _fixpoint(f!::F,
     xi = copy(x0)
     xi = f!(xi,x0)
     α = method.dampingfactor
+    ℕ = method.normorder
     lognorm = method.lognorm
-    converged,finite = convergence(x0,xi,atol,rtol,α,lognorm)
+    converged,finite = convergence(x0,xi,atol,rtol,α,lognorm,ℕ)
     if converged
         if finite
             return xi
@@ -166,7 +174,7 @@ function _fixpoint(f!::F,
         xi = f!(xi,xold)
         xi .*= α
         xi .+= (1 .- α) .* xold
-        converged,finite = convergence(xold,xi,atol,rtol,α,lognorm)
+        converged,finite = convergence(xold,xi,atol,rtol,α,lognorm,ℕ)
         if converged
             if finite
                 return xi
