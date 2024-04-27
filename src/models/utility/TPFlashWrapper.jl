4093e5726d9bd6d26f9b26da7d97390b40a41fea
longemen3000 <longemen3000@gmail.com>
2024-04-27 01:18:37 -0400

    add generic method for PTFlashWrapper

--- src/models/utility/TPFlashWrapper.jl
+++ src/models/utility/TPFlashWrapper.jl
@@ -1,10 +1,10 @@
 #wrapper used to cache results in case of activity models and CompositeModel
-struct PTFlashWrapper{T,S,F,M} <: EoSModel
+struct PTFlashWrapper{T,S} <: EoSModel
     components::Vector{String}
     model::T
     sat::Vector{S}
-    fug::Vector{F}
-    μ::Vector{M}
+    fug::Vector{S}
+    μ::Vector{S}
 end
 
 Base.length(model::PTFlashWrapper) = length(model.model)
@@ -15,4 +15,16 @@ end
 
 function tp_flash_K0!(K,wrapper::PTFlashWrapper,p,T)
     K .=  first.(wrapper.sat) ./ p 
+end
+
+function PTFlashWrapper(model::EoSModel,p,T::Number,equilibrium::Symbol)
+    pures = split_model(model)
+    RT = R̄*T
+    sats = saturation_pressure.(pures,T)
+    vv_pure = last.(sats)
+    p_pure = first.(sats)
+    μpure = only.(VT_chemical_potential_res.(__gas_model.(pures),vv_pure,T))
+    ϕpure = exp.(μpure ./ RT .- log.(p_pure .* vv_pure ./ RT))
+    g_pure = [VT_gibbs_free_energy(__gas_model(pures[i]),vv_pure[i],T) for i in 1:length(model)]
+    return PTFlashWrapper(model.components,model,sats,ϕpure,g_pure)
 end
