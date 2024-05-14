368a429f31415fc78e9bc1c3997f8cde27823435
longemen3000 <longemen3000@gmail.com>
2024-05-14 19:37:06 -0400

    remove no_comb_sparse

--- test/test_methods_api.jl
+++ test/test_methods_api.jl
@@ -54,13 +54,13 @@
 end
 
 @testset "association" begin
-    no_comb_sparse = Clapeyron.AssocOptions(combining = :nocombining, dense = false)
+    #no_comb_sparse = Clapeyron.AssocOptions(combining = :nocombining, dense = false)
     no_comb_dense = Clapeyron.AssocOptions(combining = :nocombining, dense = true)
     esd = Clapeyron.AssocOptions(combining = :esd)
     esd_r = Clapeyron.AssocOptions(combining = :elliott_runtime)
     cr1 = Clapeyron.AssocOptions(combining = :cr1)
 
-    model_no_comb_sparse = PCSAFT(["methanol","ethanol"],assoc_options = no_comb_sparse)
+    #model_no_comb_sparse = PCSAFT(["methanol","ethanol"],assoc_options = no_comb_sparse)
     model_no_comb_dense = PCSAFT(["methanol","ethanol"],assoc_options = no_comb_dense)
     model_cr1 = PCSAFT(["methanol","ethanol"],assoc_options = cr1)
     model_esd = PCSAFT(["methanol","ethanol"],assoc_options = esd)
@@ -70,8 +70,8 @@ end
     T = 298.15
     z = [0.5,0.5]
     @test Clapeyron.nonzero_extrema(0:3) == (1, 3)
-    @test Clapeyron.a_assoc(model_no_comb_sparse,V,T,z) ≈ -4.667036481159167  rtol = 1E-6
-    @test Clapeyron.a_assoc(model_no_comb_sparse,V,T,z) ≈ Clapeyron.a_assoc(model_no_comb_dense,V,T,z)  rtol = 1E-6
+    @test Clapeyron.a_assoc(model_no_comb_dense,V,T,z) ≈ -4.667036481159167  rtol = 1E-6
+    #@test Clapeyron.a_assoc(model_no_comb_sparse,V,T,z) ≈ Clapeyron.a_assoc(model_no_comb_dense,V,T,z)  rtol = 1E-6
     @test Clapeyron.a_assoc(model_cr1,V,T,z) ≈ -5.323469194263458  rtol = 1E-6
     @test Clapeyron.a_assoc(model_esd,V,T,z) ≈ -5.323420343872591  rtol = 1E-6
     @test Clapeyron.a_assoc(model_esd_r,V,T,z) ≈ -5.323430326406561  rtol = 1E-6
