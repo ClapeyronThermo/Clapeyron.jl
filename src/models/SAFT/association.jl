70a06c19d89c8aeee77b5941502f61df10ef0cbf
longemen3000 <longemen3000@gmail.com>
2024-05-18 18:56:32 -0400

    association: only use COSMOSAC update when dx_proposed < min(xin-xout)

--- src/models/SAFT/association.jl
+++ src/models/SAFT/association.jl
@@ -341,13 +341,19 @@ function assoc_matrix_solve(K, α, atol ,rtol, max_iters)
     #by around 50%
     =#
     function fX(out,in)
-        
+        xnorm = zero(eltype(out))
+        @inbounds for i in 1:length(out)
+            xnorm = min(xnorm,abs(in[i]-out[i]))
+        end
         i_solved = 0
         len = length(in)
         for ii in 1:len
             Kxi = zero(eltype(out))
             @inbounds for vv in 1:i_solved
-                Kxi += out[vv]*K[ii,vv]
+                xin = in[vv]
+                xout = out[vv]
+                xi = ifelse(abs(xin - xout) < xnorm, xout, xin)
+                Kxi += xi*K[ii,vv]
             end
             @inbounds for vv in (i_solved+1):len
                 Kxi += in[vv]*K[ii,vv]
