fc194696beb8c57cf882ad5f693bd2b9f2d46123
longemen3000 <longemen3000@gmail.com>
2024-06-06 01:10:36 -0400

    update docs on SolidKs and SolidHfus

--- src/models/CompositeModel/SolidModel/SolidHfus.jl
+++ src/models/CompositeModel/SolidModel/SolidHfus.jl
@@ -19,13 +19,13 @@ end
 
 - `Hfus`: Single Parameter (`Float64`) - Enthalpy of Fusion at 1 bar `[J/mol]`
 - `Tm`: Single Parameter (`Float64`) - Melting Temperature `[K]`
-- `CpSL`: Single Parameter (`Float64`) - Heat Capacity of the Solid-Liquid Phase Transition `[J/mol/K]`
+- `CpSL`: Single Parameter (`Float64`) (optional) - Heat Capacity of the Solid-Liquid Phase Transition `[J/mol/K]`
 
 ## Description
 
 Approximation of the excess chemical potential in the solid phase (`CpSL` is not necessary by default):
 ```
-ln(x_iγ_i) = Hfus*T*(1/Tm-1/T)-CpSL/R̄*(Tm/T-1-log(Tm/T))
+ln(xᵢγᵢ) = Hfusᵢ*T*(1/Tmᵢ-1/T)-CpSLᵢ/R̄*(Tmᵢ/T-1-log(Tmᵢ/T))
 ```
 """
 SolidHfus
