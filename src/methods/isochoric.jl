57feed264b1209daee528be798380c54ba0221ca
longemen3000 <longemen3000@gmail.com>
2024-09-03 03:00:41 -0400

    more functions in isochoric formalism

--- src/methods/isochoric.jl
+++ src/methods/isochoric.jl
@@ -27,6 +27,12 @@ function Psi(model,T,ρᵢ)
     return Rgas(model)*T*ρ*a_eos(model,V,T,ρᵢ) + reference_state_eval(model,V,T,ρᵢ)
 end
 
+function Psi(model::IdealModel,T,ρᵢ)
+    ρ = sum(ρᵢ)
+    V = one(ρ)
+    return Rgas(model)*T*ρ*a_ideal(model,V,T,ρᵢ) + reference_state_eval(model,V,T,ρᵢ)
+end
+
 const Ψ_eos = Psi
 
 function Psi_res(model,T,ρᵢ)
@@ -43,6 +49,7 @@ function Ψ_grad_and_hessian_res(model,T,ρᵢ)
 end
 
 function Ψ_grad_and_hessian_res!(result,model,T,ρᵢ)
+    Ψ(ρ) = Psi_res(model,T,ρ)
     result = ForwardDiff.hessian!(result,Ψ,ρᵢ)
     ∇Ψ = DiffResults.gradient(result)
     HΨ = DiffResults.hessian(result)
@@ -55,35 +62,33 @@ function Ψ_grad_and_hessian(model,T,ρᵢ)
 end
 
 function Ψ_grad_and_hessian!(result,model,T,ρᵢ)
+    Ψ(ρ) = Psi(model,T,ρ)
     ∇Ψ, HΨ = Ψ_grad_and_hessian_res!(result,model,T,ρᵢ)
-    RT = Rgas(model)*T
-    ∇Ψ .+= RT .* log.(ρᵢ)
-    for i in diagind(HΨ)
-        HΨ[i] += RT/ρᵢ[i]
-    end
     return ∇Ψ, HΨ
 end
 
-function Ψ__hessian_res(model,T,ρᵢ)
+function Ψ_hessian_res(model,T,ρᵢ)
     Ψ(ρ) = Psi_res(model,T,ρ)
     HΨ = ForwardDiff.hessian(Ψ,ρᵢ)
     return HΨ
 end
 
 function Ψ_hessian(model,T,ρᵢ)
-    HΨ = Ψ_hessian_res(model,T,ρᵢ)
-    for i in diagind(HΨ)
-        HΨ[i] += RT/ρᵢ[i]
-    end
+    Ψ(ρ) = Psi(model,T,ρ)
+    HΨ = ForwardDiff.hessian(Ψ,ρᵢ)
     return HΨ
 end
 
-function Ψ_grad_res(model,T,ρᵢ)
-    return VT_chemical_potential_res(model,one(eltype(ρᵢ)),T,ρᵢ)
+function Ψ_grad(model,T,ρᵢ)
+    TT = gradient_type(model,T,ρᵢ)
+    similar(TT,length(ρᵢ))
+    return Ψ_grad!(similar(TT,length(ρᵢ)),model,T,ρᵢ)
 end
 
-function Ψ_grad(model,T,ρᵢ)
-    return VT_chemical_potential(model,one(eltype(ρᵢ)),T,ρᵢ)
+function Ψ_grad!(∇Ψ,model,T,ρᵢ)
+    Ψ(ρ) = Psi(model,T,ρ)
+    ForwardDiff.gradient!(∇Ψ,Ψ,dΨrdT)
+    return ∇Ψ
 end
 
 function dΨdT_grad_res(model,T,ρᵢ)
@@ -109,4 +114,29 @@ function d2ΨdT2_grad_res!(∇Ψ,model,T,ρᵢ)
     ForwardDiff.gradient!(∇Ψ,Ψ,d2ΨrdT2)
     return ∇Ψ
 end 
+
+function dΨdT_grad(model,T,ρᵢ)
+    TT = gradient_type(model,T,ρᵢ)
+    similar(TT,length(ρᵢ))
+    return dΨdT_grad!(similar(TT,length(ρᵢ)),model,T,ρᵢ)
+end
+
+function dΨdT_grad!(∇Ψ,model,T,ρᵢ)
+    dΨrdT(ρ) = last(Solvers.f∂f(_T -> Psi(model,_T,ρ),T))
+    ForwardDiff.gradient!(∇Ψ,Ψ,dΨrdT)
+    return ∇Ψ
+end
+
+function d2ΨdT2_grad(model,T,ρᵢ)
+    TT = gradient_type(model,T,ρᵢ)
+    similar(TT,length(ρᵢ))
+    return d2ΨdT2_grad!(similar(TT,length(ρᵢ)),model,T,ρᵢ)
+end
+
+function d2ΨdT2_grad!(∇Ψ,model,T,ρᵢ)
+    d2ΨrdT2(ρ) = last(Solvers.f∂f∂2f(_T -> Psi(model,_T,ρ),T))
+    ForwardDiff.gradient!(∇Ψ,Ψ,d2ΨrdT2)
+    return ∇Ψ
+end 
+
 
