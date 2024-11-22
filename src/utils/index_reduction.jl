8bc91ce6947b6272e3bab3b3871909350d53b4dc
longemen3000 <longemen3000@gmail.com>
2024-11-22 02:15:01 -0300

    speed up splitting

--- src/utils/index_reduction.jl
+++ src/utils/index_reduction.jl
@@ -16,6 +16,10 @@ you can pass an arbitrary boolean vector (`bools`) to perform the reduction.
 
 """
 function index_reduction(model::EoSModel,z::AbstractVector,zmin = sum(z)*4*eps(eltype(z)))
+    #skip splitting if possible
+    if all(>(zmin),z)
+        return model,trues(length(model))
+    end
     idx = z .> zmin
     return index_reduction(model,idx)
 end
