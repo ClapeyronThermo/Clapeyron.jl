680df12cc2ddde324ff630e23d12a97c6a5d36d4
longemen3000 <longemen3000@gmail.com>
2024-11-24 01:26:20 -0300

    fix test

--- test/test_methods_api.jl
+++ test/test_methods_api.jl
@@ -206,7 +206,8 @@ end
         #test for initialization when K suggests single phase but it could be solved supposing bubble or dew conditions.
         substances = ["water", "methanol", "propyleneglycol","methyloxirane"]
         pcp_system = PCPSAFT(substances)
-        @test Clapeyron.tp_flash2(pcp_system, 25_000.0, 300.15, [1.0, 1.0, 1.0, 1.0], RRTPFlash())[end] ≈ -8.900576759774916 rtol = 1e-6
+        res = Clapeyron.tp_flash2(pcp_system, 25_000.0, 300.15, [1.0, 1.0, 1.0, 1.0], RRTPFlash())
+        @test res.data.g ≈ -8.900576759774916 rtol = 1e-6
     end
 
     if isdefined(Base,:get_extension)
