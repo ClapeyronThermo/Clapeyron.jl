bb4e1de511ce61a623e63a1cc88f2163a96acfa4
longemen3000 <longemen3000@gmail.com>
2024-11-28 01:42:28 -0300

    substitutute lazy alpha vector for static array

--- src/models/cubic/alphas/alphas.jl
+++ src/models/cubic/alphas/alphas.jl
@@ -21,11 +21,19 @@ function α_function end
 #In the future, we could use the optional allocation argument to stop allocating alphas
 α_function(model,V,T,z,alphamodel) = _α_function(model,V,T,z,alphamodel)
 
-function _α_function(model,V,T,z,alphamodel)
-    return LazyAlphaVector(model,alphamodel,T)
-    α = zeros(Base.promote_eltype(model,T),length(model))
-    α_function!(α,model,alphamodel,T)
-    return α
+function α_function(model,V,T,z,alphamodel)
+    _0 = zero(Base.promote_eltype(model,T))
+    nc = length(model)
+    if nc <= 8
+        #view of a static array of constant length
+        #this allows non-allocating alpha functions.
+        αtup = ntuple(i -> i <= nc ? α_function(model,alphamodel,T,i) : _0,Val{8}())
+        return view(SVector(αtup),1:nc)
+    else
+        αvec = fill(_0,nc)
+        α_function!(αvec,model,alphamodel,T)
+        return αvec
+    end
 end
 
 #all alphas at the moment don't have any need for recombine!
@@ -66,47 +74,11 @@ function init_alphamodel(alpha,components,w = nothing,userlocations = String[],v
     return init_model(alpha,components,userlocations,verbose)
 end
 
-mutable struct LazyAlphaVector{M,α,TT} <: AbstractVector{TT}
-    @const model::M
-    @const alphamodel::α
-    @const T::TT
-    @const allocated::Bool
-    @const nc::Int
-    vector::Vector{TT}
-    function LazyAlphaVector(model::M,alpha::α,T::T1) where {M,α,T1}
-        TT = Base.promote_eltype(model,T)
-        nmax = alpha_lazy_limit(model)
-        nc = length(model)
-        needs_alloc = nc > nmax
-        αvec = new{M,α,TT}(model,alpha,convert(TT,T),needs_alloc,nc)
-        if needs_alloc
-            vec = Vector{TT}(undef,nc)
-            α_function!(vec,model,alpha,T)
-            αvec.vector = vec
-        end
-        return αvec
-    end
-end
-
-alpha_lazy_limit(model::CubicModel) = min(alpha_lazy_limit(model.mixing),alpha_lazy_limit(model.alpha))
-alpha_lazy_limit(model) = 5
-
 function α_function!(vec,model,alphamodel,T)
-    for i in 1:length(model)
+    @inbounds for i in 1:length(model)
         vec[i] = α_function(model,alphamodel,T,i)
     end
     return vec
-end
-
-Base.size(x::LazyAlphaVector) = (x.nc,)
-Base.length(x::LazyAlphaVector) = x.nc
-Base.eltype(x::LazyAlphaVector{M,α,TT}) where {M,α,TT} = TT
-Base.@propagate_inbounds function Base.getindex(x::LazyAlphaVector{M,α,TT},i::Int)::TT where {M,α,TT}
-    if x.allocated
-        @inbounds return x.vector[i]::TT
-    else
-        return α_function(x.model,x.alphamodel,x.T,i)::TT
-    end
 end
 
 include("NoAlpha.jl")
