f27d7608121cc7169bedf0509eb5c52771673498
longemen3000 <longemen3000@gmail.com>
2024-12-31 02:57:04 -0300

    fix initial K-value in case volume calculation failed for one phase (#325)

--- src/methods/tpd.jl
+++ src/methods/tpd.jl
@@ -624,6 +624,14 @@ function suggest_K(model,p,T,z,pure = split_model(model),volatiles = FillArrays.
         lnϕl = VT_lnϕ_pure(pure[i],vl,T,p)
         tpd_v = lnϕv - di[i]
         tpd_l = lnϕl - di[i]
+        
+        if vl == vv
+            if is_liquid(VT_identify_phase(model,vl,T,SA[1.0]))
+                tpd_v = Inf*abs(tpd_v)
+            else
+                tpd_l = Inf*abs(tpd_l)
+            end
+        end
         if tpd_l < 0 && tpd_v < 0
             K[i] = exp(lnϕl[1])/exp(lnϕv[1])
         elseif tpd_l < 0 && tpd_v >= 0
