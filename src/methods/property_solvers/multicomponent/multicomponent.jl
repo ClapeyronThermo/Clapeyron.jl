39aba355cc8fe207ecc6e22ddc9aaea086dd074f
longemen3000 <longemen3000@gmail.com>
2025-01-28 18:49:47 -0300

    μp_equality: use log(a) + log(b) - log(c) - log(d) instead of log(a*b/(c*d))

--- src/methods/property_solvers/multicomponent/multicomponent.jl
+++ src/methods/property_solvers/multicomponent/multicomponent.jl
@@ -118,7 +118,7 @@ function μp_equality(model::EoSModel, F, PT::TPspec, Base.@specialize(v), Base.
             μ1i = Fj[i]
             μji = μj[i]
             Δuᵣ = μ1i - μji
-            Δu = Δuᵣ*RTinv + log(vj*w1[i]/(v1*wj[i]))
+            Δu = Δuᵣ*RTinv + log(vj) + log(w1[i]) -log(v1) - log(wj[i])
             Fj[i] = Δu
         end
     end
@@ -161,7 +161,7 @@ function μp_equality2(models::NTuple{2,M}, F, PT::TPspec, v, w, short_view) whe
         μ_long_i = F[i]
         μ_short_i = μ_short[i]
         Δuᵣ = (μ_long_i - μ_short_i)
-        Δμ = Δuᵣ*RTinv + log(v_short*x_long_view[i]/(v_long*x_short[i]))
+        Δμ = Δuᵣ*RTinv + log(v_short) + log(x_long_view[i])  - log(v_long) - log(x_short[i])
         F[i] = Δμ*RT⁻¹
     end
     F[n_short+1] = (p_long-p_short)*p⁻¹
