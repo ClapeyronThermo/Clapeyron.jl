23fd76a779bd2e193b9c5dfc4c1af545f591135b
longemen3000 <longemen3000@gmail.com>
2025-02-05 04:05:59 -0300

    fix error on fugacity coefficient when passing a dlnphidT-enabled cache

--- src/methods/fugacity_coefficient.jl
+++ src/methods/fugacity_coefficient.jl
@@ -3,20 +3,22 @@ function lnϕ(model::EoSModel, p, T, z=SA[1.],cache = nothing; phase=:unknown, v
     vol = volume(model, p, T, z, phase=phase, vol0=vol0, threaded=threaded)
     RT = Rgas(model)*T
     logZ = log(p*vol/RT/sum(z))
+    nc = length(model)
     if cache != nothing
         result,aux,lnϕ,∂lnϕ∂n,∂lnϕ∂P,∂P∂n,∂lnϕ∂T,hconfig = cache    
+        aux .= 0
         aux[1] = vol
-        aux[2:end] = z
+        aux[2:nc+1] = z
         gconf,jconf = hconfig.gradient_config,hconfig.jacobian_config
         seeds = jconf.seeds
         duals = jconf.duals[1]
         gconfig = ForwardDiff.GradientConfig{Nothing,eltype(aux),length(seeds),typeof(duals)}(seeds,duals)
         gresult = ForwardDiff.MutableDiffResult(result.value,(result.derivs[1],))
         F_res(model, V, T, z) = eos_res(model, V, T, z)
-        fun(aux) = F_res(model, aux[1], T, @view(aux[2:end]))
+        fun(aux) = F_res(model, aux[1], T, @view(aux[2:nc+1]))
         _result = ForwardDiff.gradient!(gresult, fun, aux, gconfig, Val{false}())
         dresult = _result.derivs[1]
-        μ_res = @view dresult[2:end]
+        μ_res = @view dresult[2:nc+1]
         lnϕ .= μ_res ./ RT .- logZ
     else
         μ_res = VT_chemical_potential_res(model, vol, T, z)
