8b7c8364554959f9ad9799b89e010976fe77c187
longemen3000 <longemen3000@gmail.com>
2025-02-14 14:28:19 -0300

    fix wilson_k_values tests

--- test/test_methods_eos.jl
+++ test/test_methods_eos.jl
@@ -283,7 +283,7 @@ end
         @test psat ≈ 1.409820798879772e6 rtol = 1E-6
         @test Clapeyron.saturation_pressure(system, T,SuperAncSaturation())[1]  ≈ psat rtol = 1E-6
         @test Clapeyron.crit_pure(system)[1] ≈ 305.31999999999994 rtol = 1E-6
-        @test Clapeyron.wilson_k_values(system,p,T) ≈ [0.13839117786853375]  rtol = 1E-6
+        @test Clapeyron.wilson_k_values(system,p,T) ≈ [0.13840091523637849]  rtol = 1E-6
     end
 end
 GC.gc()
@@ -367,7 +367,7 @@ end
         @test Clapeyron.bubble_pressure(system, T, z)[1] ≈ 1.5760730143760687e6 rtol = 1E-6
         @test Clapeyron.crit_mix(system, z)[1] ≈ 575.622237585033 rtol = 1E-6
         srksystem  = SRK(["ethane","undecane"])
-        @test Clapeyron.wilson_k_values(srksystem,p,T) ≈ [0.420849235562207, 1.6163027384311e-5] rtol = 1E-6
+        @test Clapeyron.wilson_k_values(srksystem,p,T) ≈ [0.4208525854463047, 1.6171551943938252e-5] rtol = 1E-6
         #test scaling of crit_mix
         cm1 = crit_mix(system3,[0.5,0.5])
         cm2 = crit_mix(system3,[1.0,1.0])
