c42078d5b54e0598ed25af29152f5b1a58bf084a
longemen3000 <longemen3000@gmail.com>
2025-04-10 19:32:03 -0400

    fix tests

--- test/test_issues.jl
+++ test/test_issues.jl
@@ -340,13 +340,13 @@
         bubT1 = bubble_temperature(model1, 101325, x1, FugBubbleTemperature(nonvolatiles = ["calcium", "chloride"]))[1]
         @test bubT1 ≈ bub_test rtol = 1e-6
 
-        bubT1_chempot = bubble_temperature(model1, 101325, x, ChemPotBubbleTemperature(T0=373.0, nonvolatiles=["calcium","chloride"]))[1]
+        bubT1_chempot = bubble_temperature(model1, 101325, x1, ChemPotBubbleTemperature(T0=373.0, nonvolatiles=["calcium","chloride"]))[1]
         @test_broken bubT1_chempot ≈ bub_test rtol = 1e-6
 
-        bubT1_chempot2 = bubble_temperature(model1, 101325, x, ChemPotBubbleTemperature(nonvolatiles=["calcium","chloride"]))[1]
+        bubT1_chempot2 = bubble_temperature(model1, 101325, x1, ChemPotBubbleTemperature(nonvolatiles=["calcium","chloride"]))[1]
         @test_broken bubT1_chempot2 ≈ bub_test rtol = 1e-6
 
-        bubT1_2 = bubble_temperature(model1, 101325, x, FugBubbleTemperature(nonvolatiles = ["calcium", "chloride"],y0=[1.,0.,0.],vol0=(1.8e-5,1.),T0=373.15))[1]
+        bubT1_2 = bubble_temperature(model1, 101325, x1, FugBubbleTemperature(nonvolatiles = ["calcium", "chloride"],y0=[1.,0.,0.],vol0=(1.8e-5,1.),T0=373.15))[1]
         @test bubT1_2 ≈ bub_test rtol = 1e-6
 
         bubP1 = bubble_pressure(model1,298.15,x1,FugBubblePressure(nonvolatiles=["calcium","chloride"],y0=[1.,0.,0.],vol0=(1e-5,1.)))
