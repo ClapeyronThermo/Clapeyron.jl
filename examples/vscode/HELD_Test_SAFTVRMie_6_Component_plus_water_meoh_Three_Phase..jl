f042fccb19482dff050dce63231ee7eb158a25d7
branch171 <craig.branch@virginmedia.com>
2025-04-30 20:50:58 +0100

    HELDTPFlash updated vscode examples using tp_flash_impl

--- examples/vscode/HELD_Test_SAFTVRMie_6_Component_plus_water_meoh_Three_Phase..jl
+++ examples/vscode/HELD_Test_SAFTVRMie_6_Component_plus_water_meoh_Three_Phase..jl
@@ -12,9 +12,21 @@ xwater = 0.05
 z = append!(zdry*(1.0-xmeoh-xwater),xmeoh)
 z = append!(z,xwater)
 
-add_near_pure_guess = true
-add_pure_component = fill(true,length(z))
-add_random_guess = false
-add_all_guess = false
 verbose = true
-Clapeyron.HELD_derivatives(model,p,T,z,add_near_pure_guess,add_pure_component,add_random_guess,add_all_guess,verbose)
+
+beta,xp,vp,Gsol = Clapeyron.tp_flash_impl(model,p,T,z, HELDTPFlash(verbose = verbose))
+
+if !verbose
+    for ip = 1:length(beta)
+        println("Phase beta($(ip)) = $(beta[ip])")
+    end
+    println("Phase mole fraction:")
+    for ip = 1:length(beta)
+        println("Phase x($(ip)) = $(xp[ip])")
+    end
+    println("Phase volumes:")
+    for ip = 1:length(beta)
+        println("Phase volume($(ip)) = $(vp[ip])")
+    end
+    println("Minimum Gibbs Energy = $(Gsol)")
+end
