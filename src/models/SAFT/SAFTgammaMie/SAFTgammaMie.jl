3b2af6c17ecbc5c9f40903c20c27d26692639d44
Garren-H <garren382@gmail.com>
2025-05-02 12:29:35 +0200

    Update src/models/SAFT/SAFTgammaMie/SAFTgammaMie.jl
    Co-authored-by: Andrés Riedemann <38795484+longemen3000@users.noreply.github.com>

--- src/models/SAFT/SAFTgammaMie/SAFTgammaMie.jl
+++ src/models/SAFT/SAFTgammaMie/SAFTgammaMie.jl
@@ -258,7 +258,9 @@ function SAFTVRMie(groups::GroupParam,param::SAFTgammaMieParam,sites::SiteParam 
     comp_sites = gc_to_comp_sites(sites,groups)
     comp_bondvol = gc_to_comp_sites(gc_bondvol,comp_sites)
     comp_epsilon_assoc = gc_to_comp_sites(gc_epsilon_assoc,comp_sites)
-    Mw = SingleParam("Mw",components, zeros(eltype(shapefactor), length(groups.components)))
+    Mw = get(params,"Mw") do
+      SingleParam("Mw",components, zeros(eltype(shapefactor), length(groups.components)))
+    end
     vrparams = SAFTVRMieParam(Mw,segment,sigma,lambda_a,lambda_r,epsilon,comp_epsilon_assoc,comp_bondvol)
     return SAFTVRMie(components,comp_sites,vrparams,idealmodel,assoc_options,default_references(SAFTVRMie))
 end
