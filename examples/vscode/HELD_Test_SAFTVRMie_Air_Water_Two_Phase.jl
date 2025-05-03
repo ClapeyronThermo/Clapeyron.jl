22d18913060f8dafefa62386760c43cd465da53a
branch171 <craig.branch@virginmedia.com>
2025-05-04 00:30:47 +0100

    HELDTPFlash added more Mset on initialisation - bug fixes

--- examples/vscode/HELD_Test_SAFTVRMie_Air_Water_Two_Phase.jl
+++ examples/vscode/HELD_Test_SAFTVRMie_Air_Water_Two_Phase.jl
@@ -1,20 +1,20 @@
 using Clapeyron
 
-#components = ["nitrogen","oxygen","argon","carbon dioxide","water"]
-components = ["water","carbon dioxide","argon","oxygen","nitrogen"]
+components = ["nitrogen","oxygen","argon","carbon dioxide","water"]
+#components = ["water","carbon dioxide","argon","oxygen","nitrogen"]
 model = SAFTVRMie(components; assoc_options=AssocOptions(combining=:elliott))
 
 p = 1.5e5
 T = 20+273.15
 
-#zdry=[0.7808,0.2095,0.0093,0.0004]
-zdry=[0.0004,0.0093,0.2095,0.7808]
+zdry=[0.7808,0.2095,0.0093,0.0004]
+#zdry=[0.0004,0.0093,0.2095,0.7808]
 zwater=0.05
-z=prepend!(zdry*(1-zwater),zwater)
-#z=append!(zdry*(1-zwater),zwater)
+#z=prepend!(zdry*(1-zwater),zwater)
+z=append!(zdry*(1-zwater),zwater)
 
 verbose = true
-add_all_guess = false
+add_all_guess = true
 beta,xp,vp,Gsol = Clapeyron.tp_flash_impl(model,p,T,z, HELDTPFlash(add_all_guess = add_all_guess,verbose = verbose))
 
 if !verbose
