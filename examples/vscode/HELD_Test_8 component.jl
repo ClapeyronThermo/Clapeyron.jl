63ee0a1de015794a5556d38e4cd8cd09a73d5395
branch171 <craig.branch@virginmedia.com>
2025-05-23 22:46:26 +0100

    HELDTP_Flash added rho solution to all phases and worked on lambda max as this wasn't working for 8 comp water meoh

--- examples/vscode/HELD_Test_8 component.jl
+++ examples/vscode/HELD_Test_8 component.jl
@@ -30,8 +30,6 @@ println("Density Clapeyron Volume method = $(round(rhom,sigdigits=6)) kg/m3")
 
 npoint = 1000
 density = zeros(npoint)
-#p1 = zeros(npoint)
-#dp1 = zeros(npoint)
 G1 = zeros(npoint)
 dG1 = zeros(npoint)
 ddG1 = zeros(npoint)
@@ -39,8 +37,6 @@ let rho = 0.2
     for i = 1:npoint
         global density[i] = rho/vref*mw 
         V = vref/rho
-        #p1,dpdV1 = Clapeyron.p∂p∂V(model,V,T,z)
-        #global dp1[i] = dpdV1
         Gi1, dGi1, ddGi1 = Clapeyron.HELD_volume2(model,p,T,z,vref,rho)
         global G1[i] = Gi1
         global dG1[i] = dGi1
@@ -53,14 +49,11 @@ l = @layout [a b c d]
 p1 = plot([density], [G1], xlabel = "rho", ylabel = "G",left_margin = 10Plots.mm,bottom_margin = 10Plots.mm,grid = :on,linewidth=3,size=(1600,1200))
 p2 = plot([density], [dG1], xlabel = "rho", ylabel = "dG",left_margin = 10Plots.mm,bottom_margin = 10Plots.mm,grid = :on,linewidth=3,size=(1600,1200))
 p3 = plot([density], [ddG1], xlabel = "rho", ylabel = "ddG",left_margin = 10Plots.mm,bottom_margin = 10Plots.mm,grid = :on,linewidth=3,size=(1600,1200))
-#p4 = plot([density], [dp1], xlabel = "rho", ylabel = "dpdV",left_margin = 10Plots.mm,bottom_margin = 10Plots.mm,grid = :on,linewidth=3,size=(1600,1200))
 display(p1)
 display(p2)
 display(p3)
-#display(p4)
 
 rho = Clapeyron.HELD_density(model,p,T,z,vref)
-#mw = Clapeyron.molecular_weight(model,z)
 println("rho  = $(rho)")
 vol = vref/rho
 rhom = mw/vol
