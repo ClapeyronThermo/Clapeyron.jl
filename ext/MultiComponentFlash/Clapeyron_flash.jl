757b7493c77a78890cd0999539c6eac9f9f929e8
longemen3000 <longemen3000@gmail.com>
2025-07-29 00:45:18 -0400

    MultiComponentFlashExt: use flashed_mixture_2ph! instead of flash_2ph!

--- ext/MultiComponentFlash/Clapeyron_flash.jl
+++ ext/MultiComponentFlash/Clapeyron_flash.jl
@@ -25,13 +25,27 @@ end
 function C.tp_flash_impl(model,p,T,z,method::C.MCFlashJL)
     S = method.storage == nothing ? M.flash_storage(model,p,T,z,method) : method.storage
     K = method.K === nothing ? C.wilson_k_values!(zeros(typeof(p+T+one(eltype(model))),length(model)),model,p,T,S.crit) : method.K
-    conditions = (p = p, T = T,z = z)
-    β = M.flash_2ph!(S,K,model,conditions;method = method.method,method.kwarg...)
-    x = M.liquid_mole_fraction!(S.x, z, K, β)
-    y = M.vapor_mole_fraction!(S.y, x, K)
-    volumes = [C.volume(model,p,T,x,phase = :l),C.volume(model,p,T,y,phase = :v)]
-    g = C.__tpflash_gibbs_reduced(model,p,T,x,y,β,:vle,volumes)
+    conditions = (p = p, T = T, z = z)
+    res = M.flashed_mixture_2ph!(S, model, conditions, K; method.kwarg...)
+    #β = M.flash_2ph!(S,K,model,conditions;method = method.method,method.kwarg...)
+    M.flashed_mixture_2ph
+    KK = res.K
+    if res.state == M.single_phase_v
+        β = one(eltype(KK))
+    elseif res.state == M.single_phase_l
+        β = zero(eltype(KK))
+    elseif res.state == M.two_phase_lv
+        β = res.V
+    else
+    end
+    n = sum(z)
+    x,y = res.liquid.mole_fractions,res.vapor.mole_fractions
+    Zl,Zv = res.liquid.Z,res.vapor.Z
+    nRT = n*C.Rgas(model)*T
+    vl,vv = Zl*nRT/p,Zv*nRT/p
+    volumes = [vl,vv]
+    data = C.FlashData(p,T)
     comps = [x,y]
-    βi = [1-β,β]
-    return C.FlashResult(p,T,comps,βi,volumes,g,sort = false)
+    βi = [n*(1-β),n*β]
+    return C.FlashResult(comps,βi,volumes,data)
 end
