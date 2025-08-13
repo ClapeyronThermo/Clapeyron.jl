c3156d848cc445158b9fb1a5c0e99f7cb0bde08c
longemen3000 <longemen3000@gmail.com>
2025-08-13 01:42:38 -0400

    file reorganization

--- src/methods/property_solvers/property_solvers.jl
+++ src/methods/property_solvers/property_solvers.jl
@@ -1,3 +1,23 @@
+#volume calculations
 include("volume.jl")
+
+#lnϕ calculations
+include("fugacity_coefficient.jl")
+
+#single component properties and solvers
 include("singlecomponent/singlecomponent.jl")
+
+#multiple component properties and solvers
 include("multicomponent/multicomponent.jl")
+
+#stability calculations
+include("stability/tpd.jl")
+include("stability/stability.jl")
+
+#T-X and P-X solvers
+include("Tproperty.jl")
+include("Pproperty.jl")
+
+#spinodal solvers
+include("spinodal.jl")
+
