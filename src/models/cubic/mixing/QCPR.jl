ec1e889088448254ae10bbd9467c021d76fc8e55
longemen3000 <longemen3000@gmail.com>
2026-01-22 01:07:19 -0300

    fix QCPR

--- src/models/cubic/mixing/QCPR.jl
+++ src/models/cubic/mixing/QCPR.jl
@@ -108,7 +108,8 @@ function mixing_rule1(model,V,T,z,mixing_model::QCPRRuleModel,α,a,b)
     Tc = model.params.Tc.values[1]
     β = (1 + A/(T + B))^3 / (1 + A/(Tc + B))^3
     b̄ = b[1,1]*β*_1
-    c̄ = translation2(model,V,T,z,model.translation,a,b,α)*invn
+    v = V/sum(z)
+    c̄ = translation2(model,v,T,SA[1.0],model.translation,a,b,α)
     return ā,b̄,c̄
 end
 
