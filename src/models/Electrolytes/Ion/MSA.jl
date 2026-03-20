92d01a6182827ffb9b7fc27b377a8f99d85985e0
longemen3000 <longemen3000@gmail.com>
2026-03-19 23:46:31 -0300

    typo in MSA iter

--- src/models/Electrolytes/Ion/MSA.jl
+++ src/models/Electrolytes/Ion/MSA.jl
@@ -81,10 +81,12 @@ function screening_length(V, T, z, Z, σ, ϵ_r)
     Δ = 1-π*ρ/6*sum(z[i]*σ[i]^3 for i ∈ 1:nc)
     κ = debye_length(V,T,z,ϵ_r,Z)
     k1 = sqrt(π*e_c^2*ρ/(4π*ϵ_0*ϵ_r*k_B*T))
-    Γold = κ*oneunit(k1)
-    iszero(primalval(Γold)) && return _0
+    Γnew = κ*oneunit(k1)
+    Γold = Γnew
+    iszero(primalval(Γnew)) && return _0
     Γnew = Γold
     for _ in 1:100
+        Γold = Γnew
         #Ω = 1+π*ρ/(2*Δ)*sum(z[i]*σ[i]^3/(1+Γold*σ[i]) for i ∈ iions)
         Ω1 = oneunit(Γold)
         for i in 1:nc
@@ -114,9 +116,42 @@ function screening_length(V, T, z, Z, σ, ϵ_r)
             end
         end
 
-        Γold = Γnew
+        
         Γnew = k1*sqrt(∑Q2x)
         abs(1-Γnew/Γold) <= 1e-12 && break
+    end
+    return Γnew
+end
+
+function screening_length2(V, T, z, Z, σ, ϵ_r)
+    iions = @iions
+    ρ = N_A/V
+    nc = length(Z)
+    Δ = 1-π*ρ/6*sum(z[i]*σ[i]^3 for i ∈ 1:nc)
+    κ = debye_length(V,T,z,ϵ_r,Z)
+    k1 = sqrt(π*e_c^2*ρ/(4π*ϵ_0*ϵ_r*k_B*T))
+    Γold = κ*oneunit(k1)
+    _0 = zero(Γold)
+    iszero(primalval(Γold)) && return _0
+    
+    iter = 1
+    tol = oneunit(_0)
+    
+    Γnew = _0*k1
+    #step 1: bounded SS
+    while tol>1e-12 && iter < 100
+        Ω = 1+π*ρ/(2*Δ)*sum(z[i]*σ[i]^3/(1+Γold*σ[i]) for i ∈ iions)
+        Pn = ρ/Ω*sum(z[i]*σ[i]*Z[i]/(1+Γold*σ[i]) for i ∈ iions)
+        #Q = @. (Z-σ^2*Pn*(π/(2Δ)))./(1+Γold*σ)
+        ∑Q2x = _0
+        for i ∈ iions
+            Qi = (Z[i]-σ[i]^2*Pn*(π/(2Δ)))/(1+Γold*σ[i])
+            ∑Q2x += z[i]*Qi^2
+        end
+        Γnew = k1*sqrt(∑Q2x)
+        tol = abs(1-Γnew/Γold)
+        
+        Γold = Γnew
     end
     return Γnew
 end
