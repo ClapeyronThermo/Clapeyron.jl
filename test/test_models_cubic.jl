751e24156f2e072bf60a950b6d0730fa758e4c74
longemen3000 <longemen3000@gmail.com>
2026-05-26 20:29:53 -0400

    typo

--- test/test_models_cubic.jl
+++ test/test_models_cubic.jl
@@ -264,10 +264,9 @@
         end
 
         @testset "PTH" begin
-            zz = [0.95,0.05]
             system = PatelTejaHeyen(["ethane","undecane"])
-            @test Clapeyron.a_res(system, V, T, zz) ≈ -0.66377850749776 rtol = 1e-6
-            @test Clapeyron.cubic_p(system, V, T, zz) ≈ Clapeyron.pressure(system, V, T, zz) rtol = 1e-6
+            @test Clapeyron.a_res(system, V, T, z) ≈ -0.66377850749776 rtol = 1e-6
+            @test Clapeyron.cubic_p(system, V, T, z) ≈ Clapeyron.pressure(system, V, T, zz) rtol = 1e-6
         end
 
     end
