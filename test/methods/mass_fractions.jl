66421933c76a4b5fc2b9b07f2e99bebca8825814
w3lld1 <42353747+w3lld1@users.noreply.github.com>
2026-09-02 18:37:49 +0200

    feat: support mass fractions as composition input

---
+++ test/methods/mass_fractions.jl
@@ -0,0 +1,8 @@
+@testset "Mass fraction inputs" begin
+    model = PR(["methane", "ethane"])
+    mass_fractions = MassFractions([0.25, 0.75])
+    mass_amounts = [0.25u"kg", 0.75u"kg"]
+
+    @test Clapeyron.uzstrip(model, mass_fractions) ≈ Clapeyron.uzstrip(model, mass_amounts)
+    @test volume(model, 1u"bar", 300u"K", mass_fractions) ≈ volume(model, 1u"bar", 300u"K", mass_amounts)
+end
