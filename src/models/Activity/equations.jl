803d96b0d0298a773150775095d66963e449f41f
Sebastian Schmitt <62754211+se-schmitt@users.noreply.github.com>
2026-09-08 17:02:45 +0200

    Fix ∂lnγ∂T for models with lnγ_impl! (#630)
    * fix dlngdT
    
    * small changes

--- src/models/Activity/equations.jl
+++ src/models/Activity/equations.jl
@@ -310,10 +310,12 @@ end
 function ∂lnγ∂T(model,p,T,z,cache = nothing)
     nc = length(z)
     dgEdt(w) = dG_EdT(model,p,T,@view(w[1:nc]))
+    _ft_lnγ_impl!(_lnγ, _T) = lnγ_impl!(_lnγ, model, p, _T, z)
     if cache === nothing
         if has_lnγ_impl(model)
-            out = zeros(Base.promote_eltype(model,p,T,z))
-            ∂lnγ∂T = ForwardDiff.derivative!(out,lnγ_impl!,T)
+            ∂lnγ∂T = zeros(Base.promote_eltype(model,p,T,z),nc)
+            lnγ = similar(∂lnγ∂T)
+            ForwardDiff.derivative!(∂lnγ∂T,_ft_lnγ_impl!,lnγ,T)
             return ∂lnγ∂T
         else
             ∂lnγ∂T = ForwardDiff.gradient(dgEdt,z)
@@ -321,16 +323,14 @@ function ∂lnγ∂T(model,p,T,z,cache = nothing)
         end
     else
         result,aux,lnγ,∂lnγ∂ni,∂lnγ∂T,_,_,hconfig,jcache,∂lnγ∂T_out = cache
-        aux .= 0
-        aux[1:nc] .= z
-        aux[nc+1] = T
         if has_lnγ_impl(model)
             Dconfig = Solvers._DerivativeConfig(∂lnγ∂T_out)
-            ForwardDiff.derivative!(∂lnγ∂T,lnγ_impl!,lnγ,T,Dconfig,Val{false}())
+            ForwardDiff.derivative!(∂lnγ∂T,_ft_lnγ_impl!,lnγ,T,Dconfig,Val{false}())
             return ∂lnγ∂T
         else
             aux .= 0
             aux[1:nc] = z
+            aux[nc+1] = T
             gconfig = Solvers._GradientConfig(hconfig)
             _result = ForwardDiff.gradient!(result, dgEdt, aux, gconfig, Val{false}())
             dresult = DiffResults.gradient(_result)
